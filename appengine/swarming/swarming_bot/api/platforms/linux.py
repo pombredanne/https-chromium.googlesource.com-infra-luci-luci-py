eb8cba053db38de240321434aa92c0d831cc1422
Petr Hosek <phosek@google.com>
2017-08-29 18:16:05 -0700

    Don't check /dev/kvm access, only check existence
    This avoid race condition between swarming_bot and udev which is responsible
    for setting the correct ACL of /dev/kvm.
    
    Change-Id: I356bb08478bfb9a3ae730bb4c7781f0554dd7cbc

--- appengine/swarming/swarming_bot/api/platforms/linux.py
+++ appengine/swarming/swarming_bot/api/platforms/linux.py
@@ -215,9 +215,10 @@ def get_ssd():
     return ()
 
 
+@tools.cached
 def get_kvm():
-  """Check whether KVM is supported."""
-  return os.path.exists('/dev/kvm') and os.access("/dev/kvm", os.R_OK|os.W_OK)
+  """Check whether KVM is available."""
+  return os.path.exists('/dev/kvm')
 
 
 ## Mutating code.
