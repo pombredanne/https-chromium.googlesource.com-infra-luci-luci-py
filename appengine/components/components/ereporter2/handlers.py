380df084869f58ea2abfb561f79b9a6fe597f91a
Sadaf Matinkhoo <sadafm@google.com>
2017-09-27 14:12:50 -0400

    Disable GAE UI elements
    Security issues regarding LUCI stack XSRF protection were reported during
    AlphaSource Apato hackit. Disabling the UI elements will eliminate
    those. By setting an environment variable in app.yaml before deployment
    and wrapping the handlers with a function that checks the value of the
    environment variable, we can turn off the UI endpoints if required.
    This method does not affect any deployment that requires the UI to be
    enabled.
    
    Change-Id: I0835b786e2a597cd163c760d8b416621ad759e45

--- appengine/components/components/ereporter2/handlers.py
+++ appengine/components/components/ereporter2/handlers.py
@@ -271,27 +271,31 @@ class OnErrorHandler(auth.AuthenticatingHandler):
 
 
 def get_frontend_routes():
-  return [
-    webapp2.Route(
-        r'/restricted/ereporter2/errors',
-        RestrictedEreporter2ErrorsList),
-    webapp2.Route(
-        r'/restricted/ereporter2/errors/<error_id:\d+>',
-        RestrictedEreporter2Error),
-    webapp2.Route(
-        r'/restricted/ereporter2/report',
-        RestrictedEreporter2Report),
-    webapp2.Route(
-        r'/restricted/ereporter2/request/<request_id:[0-9a-fA-F]+>',
-        RestrictedEreporter2Request),
-    webapp2.Route(
-        r'/restricted/ereporter2/silence',
-        RestrictedEreporter2Silence),
-
+  routes = [
     # Public API.
     webapp2.Route(
       '/ereporter2/api/v1/on_error', OnErrorHandler),
   ]
+  if os.environ.get('LUCI_DISABLE_UI_ROUTES', '0') == '0':
+    routes.extend([
+      webapp2.Route(
+        r'/restricted/ereporter2/errors',
+        RestrictedEreporter2ErrorsList),
+      webapp2.Route(
+        r'/restricted/ereporter2/errors/<error_id:\d+>',
+        RestrictedEreporter2Error),
+      webapp2.Route(
+        r'/restricted/ereporter2/report',
+        RestrictedEreporter2Report),
+      webapp2.Route(
+        r'/restricted/ereporter2/request/<request_id:[0-9a-fA-F]+>',
+        RestrictedEreporter2Request),
+      webapp2.Route(
+        r'/restricted/ereporter2/silence',
+        RestrictedEreporter2Silence),
+    ])
+
+  return routes
 
 
 def get_backend_routes():
