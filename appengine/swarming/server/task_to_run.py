f17e4939dfb16e452f4aa205a67111d58bd22e25
Alexander Thomas <athom@google.com>
2017-10-12 12:11:45 +0200

    swarming: respect priorities when dimensions differ
    Add a new test to verify that the highest priority task 
    (lowest queue number) is returned first.
    
    Change-Id: Iaf9535c8ed966aea9fc6910ae963bc8f0529f159

--- appengine/swarming/server/task_to_run.py
+++ appengine/swarming/server/task_to_run.py
@@ -132,7 +132,7 @@ def _gen_queue_number(dimensions_hash, timestamp, priority):
 def _queue_number_priority(q):
   """Returns the number to be used as a comparision for priority.
 
-  The higher the more important.
+  The lower the more important.
   """
   return q & 0x7FFFFFFF
 
@@ -350,7 +350,7 @@ def _yield_potential_tasks(bot_id):
     if f and f.done():
       items.extend(f.get_result())
       futures[i] = next(yielders[i], None)
-  items.sort(key=lambda k: _queue_number_priority(k.id()), reverse=True)
+  items.sort(key=lambda k: _queue_number_priority(k.id()))
 
   # It is possible that there is no items yet, in case all futures are taking
   # more than 1 second.
@@ -372,7 +372,7 @@ def _yield_potential_tasks(bot_id):
         futures[i] = next(yielders[i], None)
         changed = True
     if changed:
-      items.sort(key=lambda k: _queue_number_priority(k.id()), reverse=True)
+      items.sort(key=lambda k: _queue_number_priority(k.id()))
 
 
 ### Public API.
