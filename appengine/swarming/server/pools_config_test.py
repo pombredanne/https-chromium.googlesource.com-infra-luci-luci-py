7acafdebe3803c09a330ac280c45fe09da20c773
Vadim Shtayura <vadimsh@chromium.org>
2017-10-30 22:27:11 -0700

    swarming: Add pools.cfg with new pool ACL checks.
    Execute them, but just log the results, without enforcing it yet. Once all
    relevant Swarming instances are updated with pools.cfg file, the new ACL check
    will become the main one and 'dimension_acls' in Settings will be deleted.
    
    R=maruel@chromium.org
    BUG=777022
    
    Change-Id: I9419b10901cfd9946149099aeb8997a438dc9e3c

---
+++ appengine/swarming/server/pools_config_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright 2017 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import logging
+import sys
+import unittest
+
+import test_env
+test_env.setup_test_env()
+
+
+if __name__ == '__main__':
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  logging.basicConfig(
+      level=logging.DEBUG if '-v' in sys.argv else logging.CRITICAL)
+  unittest.main()
