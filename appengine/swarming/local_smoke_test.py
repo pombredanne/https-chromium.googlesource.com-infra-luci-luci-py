752ac08997d5b7a78d8c47d7a3d07173da5a3fb7
Marc-Antoine Ruel <maruel@chromium.org>
2017-11-06 16:18:31 -0500

    swarming: add smoke test to confirm priorities work.
    Bug: 781487
    Change-Id: I2157d0b6b9f40c44b4c733bfd2bfc55b83c469cb

--- appengine/swarming/local_smoke_test.py
+++ appengine/swarming/local_smoke_test.py
@@ -206,6 +206,10 @@ class SwarmingClient(object):
     finally:
       os.remove(tmp)
 
+  def task_query(self, task_id):
+    """Query a task result without waiting for it to complete."""
+    return json.loads(self._capture('query', ['task/%s/result' % task_id]))
+
   def terminate(self, bot_id):
     task_id = self._capture('terminate', [bot_id]).strip()
     logging.info('swarming.py terminate returned %r', task_id)
@@ -797,6 +801,67 @@ class Test(unittest.TestCase):
     dimensions = {
         i['key']: i['value'] for i in self.client.query_bot()['dimensions']}
     self.assertEqual(expected, set(dimensions))
+
+  def test_priority(self):
+    # Make a test that keeps the bot busy, while all the other tasks are being
+    # created with priorities that are out of order.
+    h, tmp = tempfile.mkstemp(prefix='swarming_smoke_test')
+    os.close(h)
+    task_ids = []
+    priorities = [9, 8, 6, 7]
+    tags = [u'pool:default', u'service_account:none', u'user:joe@localhost']
+    try:
+      args = [
+        '-T', 'wait', '--priority', '20', '--',
+        'python', '-u', '-c',
+        # Cheezy wait.
+        ('import os,time;'
+         '[time.sleep(0.1) for _ in xrange(100000) if os.path.exists(\'%s\')];'
+        'print(\'hi\')') % tmp,
+      ]
+      task_id = self.client.task_trigger_raw(args)
+      # Assert that the 'wait' task has started but not completed, otherwise
+      # this defeats the purpose.
+      for i in xrange(20):
+        stats = self.client.task_query(task_id)
+        state = stats[u'state']
+        if state == u'RUNNING':
+          break
+        self.assertEqual(u'PENDING', state, stats)
+
+      for p in priorities:
+        args = [
+          '-T', 'p%d' % p, '--priority', str(p), '--',
+          'python', '-u', '-c', 'print(\'%d\')' % p,
+        ]
+        task_ids.append(self.client.task_trigger_raw(args))
+      # And the wait task is still running, so that all tasks above are pending,
+      # thus are given a chance to run in priority order.
+      stats = self.client.task_query(task_id)
+      self.assertEqual(u'RUNNING', stats[u'state'], stats)
+    finally:
+      # Unblock the bot.
+      os.remove(tmp)
+    # Ensure the initial wait task is completed.
+    actual_summary, actual_files = self.client.task_collect(task_id)
+    t = tags[:] + [u'priority:20']
+    self.assertResults(
+        self.gen_expected(name=u'wait', tags=sorted(t)), actual_summary)
+    actual_files.pop('summary.json')
+    self.assertEqual({}, actual_files)
+
+    # Look at tasks that were triggered, so that they ran in the expected order.
+    self.assertEqual(len(priorities), len(task_ids))
+    for priority, task_id in zip(priorities, task_ids):
+      actual_summary, actual_files = self.client.task_collect(task_id)
+      t = tags[:] + [u'priority:%d' % priority]
+      expected_summary = self.gen_expected(
+          name=u'p%d' % priority, tags=sorted(t),
+          outputs=[u'%d\n' % priority])
+      self.assertResults(expected_summary, actual_summary)
+      bot_version = self.assertResults(expected_summary, actual_summary)
+      actual_files.pop('summary.json')
+      self.assertEqual({}, actual_files)
 
   def _run_isolated(self, hello_world, name, args, expected_summary,
       expected_files, deduped=False, isolated_content=None):
