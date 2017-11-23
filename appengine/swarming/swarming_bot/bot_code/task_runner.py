luci/luci-py

commit bfab799cddab135075b4e548e81091d38c010b03

[swarming] Add env_prefixes TaskProperty.
This will allow swarming task triggerers to populate e.g. PATH with
CIPD binaries, and/or PYTHONPATH with subdirectories of the isolate,
etc.

This would immediately be used to replace the similar functionality in
kitchen, and to allow the 'swarming' recipe module in build.git to
include standard infra binaries (e.g. logdog, vpython) in $PATH for
triggered tasks.

R=maruel@chromium.org, nodir@chromium.org, tandrii@chromium.org, vadimsh@chromium.org

Bug: 786503
Change-Id: If50875a9cd32afd96fb59a24a0f0eb254dd98ed0
Reviewed-on: https://chromium-review.googlesource.com/777484
Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>

appengine/swarming/swarming_bot/bot_code/task_runner.py
@@ -157,6 +157,10 @@ def get_isolated_args(work_dir, task_details, isolated_result,
 
   cmd.extend(run_isolated_flags)
 
+  for key, values in task_details.env_prefixes.iteritems():
+    for v in values:
+      cmd.extend(('--env-prefix', '%s=%s' % (key, v)))
+
   # TODO(nodir): Pass the command line arguments via a response file.
   if task_details.command:
     cmd.append('--raw-cmd')
@@ -190,6 +194,10 @@ class TaskDetails(object):
 
     self.env = {
       k.encode('utf-8'): v.encode('utf-8') for k, v in data['env'].iteritems()
+    }
+    self.env_prefixes = {
+      k.encode('utf-8'): [path.encode('utf-8') for path in v]
+      for k, v in data['env_prefixes'].iteritems()
     }
     self.grace_period = data['grace_period']
     self.hard_timeout = data['hard_timeout']

appengine/swarming/handlers_bot.py
@@ -694,6 +694,7 @@ class BotPollHandler(_BotBaseHandler):
         'command': request.properties.command,
         'dimensions': request.properties.dimensions,
         'env': request.properties.env,
+        'env_prefixes': request.properties.env_prefixes,
         'extra_args': request.properties.extra_args,
         'grace_period': request.properties.grace_period_secs,
         'hard_timeout': request.properties.execution_timeout_secs,

appengine/swarming/handlers_bot_test.py
@@ -309,6 +309,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
           u'pool': u'default',
         },
         u'env': {},
+        u'env_prefixes': {},
         u'extra_args': [],
         u'grace_period': 30,
         u'hard_timeout': 3600,
@@ -382,6 +383,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
           u'pool': u'default',
         },
         u'env': {},
+        u'env_prefixes': {},
         u'extra_args': [],
         u'grace_period': 30,
         u'hard_timeout': 3600,
@@ -440,6 +442,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
           u'pool': u'default',
         },
         u'env': {},
+        u'env_prefixes': {},
         u'extra_args': [],
         u'grace_period': 30,
         u'hard_timeout': 3600,
@@ -535,6 +538,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
           u'pool': u'default',
         },
         u'env': {},
+        u'env_prefixes': {},
         u'extra_args': [],
         u'hard_timeout': 3600,
         u'grace_period': 30,
@@ -698,6 +702,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
           u'pool': u'default',
         },
         u'env': {},
+        u'env_prefixes': {},
         u'extra_args': [],
         u'hard_timeout': 3600,
         u'grace_period': 30,

appengine/swarming/local_smoke_test.py
@@ -81,21 +81,12 @@ RESULT_HEY_OUTPUTS_REF = {
 }
 
 RESULT_HEY2_ISOLATED_OUT = {
-  u'isolated': u'0616f86b24065a0595e58088925567ae54a8157c',
+  u'isolated': u'7f89142465cba8d465b1f0a3f6e6f95ad28cc56b',
   u'isolatedserver': u'http://localhost:10050',
   u'namespace': u'default-gzip',
   u'view_url':
     u'http://localhost:10050/browse?namespace=default-gzip'
-    '&hash=0616f86b24065a0595e58088925567ae54a8157c',
-}
-
-RESULT_HEY2_OUTPUTS_REF = {
-  u'isolated': u'0616f86b24065a0595e58088925567ae54a8157c',
-  u'isolatedserver': u'http://localhost:10050',
-  u'namespace': u'default-gzip',
-  u'view_url':
-    u'http://localhost:10050/browse?namespace=default-gzip'
-    '&hash=0616f86b24065a0595e58088925567ae54a8157c',
+    '&hash=7f89142465cba8d465b1f0a3f6e6f95ad28cc56b',
 }
 
 RESULT_SECRET_OUTPUT = {
@@ -491,8 +482,16 @@ class Test(unittest.TestCase):
         'import os',
         'import sys',
         'print(\'hi\')',
+        'cwd = os.path.realpath(os.getcwd()).rstrip(os.sep)',
+        'path = os.environ["PATH"].split(os.pathsep)',
+        'print(os.path.realpath(path[0]).replace(cwd, "$CWD"))',
+        'with open(os.path.join(sys.argv[1], \'FOO.txt\'), \'wb\') as f:',
+        '  f.write(os.environ["FOO"])',
         'with open(os.path.join(sys.argv[1], \'result.txt\'), \'wb\') as f:',
-        '  f.write(\'hey2\')'))
+        '  f.write(\'hey2\')',
+    ))
+    computed_PATH = os.sep.join(['$CWD', 'local', 'path'])
+
     # Hard code the size of the isolated file.
     isolated_size = 138
     expected_summary = self.gen_expected(
@@ -509,18 +508,24 @@ class Test(unittest.TestCase):
                 len(hello_world) + isolated_size),
           },
           u'isolated_upload': {
-            u'items_cold': [4, 118],
+            u'items_cold': [3, 4, 191],
             u'items_hot': [],
-            u'num_items_cold': u'2',
-            u'total_bytes_items_cold': u'122',
+            u'num_items_cold': u'3',
+            u'total_bytes_items_cold': u'198',
           },
         },
-        outputs=[u'hi\n'],
-        outputs_ref=RESULT_HEY2_OUTPUTS_REF)
-    expected_files = {os.path.join('0', 'result.txt'): 'hey2'}
+        outputs=[u'hi\n%s\n' % computed_PATH],
+        outputs_ref=RESULT_HEY2_ISOLATED_OUT)
+    expected_files = {
+      os.path.join('0', 'result.txt'): 'hey2',
+      os.path.join('0', 'FOO.txt'): 'bar',
+    }
     self._run_isolated(
         hello_world, 'separate_cmd',
-        ['--raw-cmd', '--', 'python', 'hello_world.py', '${ISOLATED_OUTDIR}'],
+        ['--raw-cmd',
+         '--env', 'FOO', 'bar',
+         '--env-prefix', 'PATH', 'local/path',
+         '--', 'python', 'hello_world.py', '${ISOLATED_OUTDIR}'],
         expected_summary, expected_files,
         isolated_content={'variables': {'files': ['hello_world.py']}})
 
@@ -635,7 +640,7 @@ class Test(unittest.TestCase):
         },
       },
       properties_hash =
-          u'082928de84d0a65839d227dcea2f5a947898929c77c1602b68c46d7d4588c1f5',
+          u'3d6dcab82b1c098b27c7a7cbb6ddcb3d41689c42ca7fe5fafd53f9105af6efea',
     )
     task_id = self._run_isolated(
         hello_world, 'idempotent_reuse', ['--idempotent'], expected_summary, {})

appengine/swarming/message_conversion.py
@@ -127,6 +127,8 @@ def task_request_to_rpc(entity):
       secret_bytes='<REDACTED>' if props.has_secret_bytes else None,
       dimensions=_string_pairs_from_dict(props.dimensions),
       env=_string_pairs_from_dict(props.env),
+      env_prefixes=_string_list_pairs_from_dict(
+          entity.properties.env_prefixes or {}),
       inputs_ref=inputs_ref)
 
   return _ndb_to_rpc(
@@ -175,6 +177,8 @@ def new_task_request_from_rpc(msg, now):
     raise ValueError('same dimension key cannot be specified twice')
   if len(set(i.key for i in props.env)) != len(props.env):
     raise ValueError('same environment variable key cannot be specified twice')
+  if len(set(i.key for i in props.env_prefixes)) != len(props.env_prefixes):
+    raise ValueError('same environment prefix key cannot be specified twice')
 
   properties = _rpc_to_ndb(
       task_request.TaskProperties,
@@ -187,6 +191,7 @@ def new_task_request_from_rpc(msg, now):
       secret_bytes=None, # ignore this, it's handled out of band
       dimensions={i.key: i.value for i in props.dimensions},
       env={i.key: i.value for i in props.env},
+      env_prefixes={i.key: i.value for i in props.env_prefixes},
       inputs_ref=inputs_ref)
 
   req = _rpc_to_ndb(

appengine/swarming/server/task_request.py
@@ -175,22 +175,44 @@ def _validate_dimensions(prop, value):
         '%s can have up to 64 keys' % prop._name)
 
 
+def _validate_env_key(prop, key):
+  maxlen = 1024
+  if not isinstance(key, unicode):
+    raise TypeError(
+        '%s must have string key, not %r' % (prop._name, key))
+  if not key:
+    raise datastore_errors.BadValueError(
+        'valid key are required in %s' % prop._name)
+  if len(key) > maxlen:
+    raise datastore_errors.BadValueError(
+        'key in %s is too long: %d > %d' % (prop._name, len(key), maxlen))
+  if not _ENV_KEY_RE.match(key):
+    raise datastore_errors.BadValueError(
+        'key in %s is invalid: %r' % (prop._name, key))
+
+
 def _validate_env(prop, value):
   _validate_dict_of_strings(prop, value)
   maxlen = 1024
   for k, v in value.iteritems():
-    if not k:
-      raise datastore_errors.BadValueError(
-          'valid key are required in %s' % prop._name)
-    if len(k) > maxlen:
-      raise datastore_errors.BadValueError(
-          'key in %s is too long: %d > %d' % (prop._name, len(k), maxlen))
-    if not _ENV_KEY_RE.match(k):
-      raise datastore_errors.BadValueError(
-          'key in %s is invalid: %r' % (prop._name, k))
+    _validate_env_key(prop, k)
     if len(v) > maxlen:
       raise datastore_errors.BadValueError(
           'key in %s is too long: %d > %d' % (prop._name, len(v), maxlen))
+  if len(value) > 64:
+    raise datastore_errors.BadValueError(
+        '%s can have up to 64 keys' % prop._name)
+
+
+def _validate_env_prefixes(prop, value):
+  for k, v in value.iteritems():
+    if not isinstance(v, list):
+      raise TypeError(
+          '%s must have list value, not %r' % (prop._name, v))
+    _validate_env_key(prop, k)
+    for path in v:
+      _validate_rel_path('Env Prefix', path)
+
   if len(value) > 64:
     raise datastore_errors.BadValueError(
         '%s can have up to 64 keys' % prop._name)
@@ -502,6 +524,12 @@ class TaskProperties(ndb.Model):
   env = datastore_utils.DeterministicJsonProperty(
       validator=_validate_env, json_type=dict, indexed=False)
 
+  # Environment path prefix variables. Encoded as json. Optional.
+  #
+  # Env key -> [list, of, rel, paths, to, prepend]
+  env_prefixes = datastore_utils.DeterministicJsonProperty(
+      validator=_validate_env_prefixes, json_type=dict, indexed=False)
+
   # Maximum duration the bot can take to run this task. It's named hard_timeout
   # in the bot.
   execution_timeout_secs = ndb.IntegerProperty(
@@ -547,6 +575,7 @@ class TaskProperties(ndb.Model):
         not (self.inputs_ref and self.inputs_ref.isolated) and
         not self.cipd_input and
         not self.env and
+        not self.env_prefixes and
         not self.execution_timeout_secs and
         not self.extra_args and
         not self.grace_period_secs and

appengine/swarming/server/task_request_test.py
@@ -81,6 +81,7 @@ def _gen_request(properties=None, **kwargs):
       u'pool': u'default',
     },
     'env': {u'foo': u'bar', u'joe': u'2'},
+    'env_prefixes': {u'PATH': [u'local/path']},
     'execution_timeout_secs': 30,
     'grace_period_secs': 30,
     'idempotent': False,
@@ -256,6 +257,7 @@ class TaskRequestApiTest(TestCase):
         u'pool': u'default',
       },
       'env': {u'foo': u'bar', u'joe': u'2'},
+      'env_prefixes': {u'PATH': [u'local/path']},
       'extra_args': [],
       'execution_timeout_secs': 30,
       'grace_period_secs': 30,
@@ -278,7 +280,7 @@ class TaskRequestApiTest(TestCase):
       # Intentionally hard code the hash value since it has to be deterministic.
       # Other unit tests should use the calculated value.
       'properties_hash':
-          '258e0894f2589a7bc2b46bd563d01f64300ae08938fb14b3d750baff94ba714e',
+          '0063761c9b2e47eabeb4bc1cc07f9f8613edf04731947a1e0d5d7afe49ef3ac7',
       'pubsub_topic': None,
       'pubsub_userdata': None,
       'service_account': u'none',
@@ -335,6 +337,7 @@ class TaskRequestApiTest(TestCase):
         u'pool': u'default',
       },
       'env': {u'foo': u'bar', u'joe': u'2'},
+      'env_prefixes': {u'PATH': [u'local/path']},
       'extra_args': [],
       'execution_timeout_secs': 30,
       'grace_period_secs': 30,
@@ -357,7 +360,7 @@ class TaskRequestApiTest(TestCase):
       # Intentionally hard code the hash value since it has to be deterministic.
       # Other unit tests should use the calculated value.
       'properties_hash':
-          '693715e9539552e2ece434e25a665d437cfe4685504a3c7bad6356f9feffee2a',
+          'bf688f31ecb8f0c8f029b6afa4d6da037af63bdae180f466109c43af5809874b',
       'pubsub_topic': None,
       'pubsub_userdata': None,
       'service_account': u'none',
@@ -399,7 +402,7 @@ class TaskRequestApiTest(TestCase):
     # Other unit tests should use the calculated value.
     # Ensure the algorithm is deterministic.
     self.assertEqual(
-        'c7445927612637c589b72dd47c4657b166a53ac6ecdfd08c167539331a7890d3',
+        '75825e335ed719b464e5dd9e116380fcec1d7f04406dafbc9dc3b1889b9338ed',
         as_dict['properties_hash'])
 
   def test_init_new_request_bot_service_account(self):

appengine/swarming/swarming_bot/bot_code/task_runner_test.py
@@ -47,6 +47,7 @@ def get_manifest(script=None, isolated=None, **kwargs):
     'command':
         [sys.executable, '-u', '-c', script] if not isolated_input else None,
     'env': {},
+    'env_prefixes': {},
     'extra_args': [],
     'grace_period': 30.,
     'hard_timeout': 10.,
@@ -245,6 +246,7 @@ class TestTaskRunner(TestTaskRunnerBase):
         'bot_id': 'localhost',
         'command': ['a'],
         'env': {'d': 'e'},
+        'env_prefixes': {},
         'extra_args': [],
         'grace_period': 30.,
         'hard_timeout': 10,
@@ -304,6 +306,7 @@ class TestTaskRunner(TestTaskRunnerBase):
         'bot_id': 'localhost',
         'command': None,
         'env': {'d': 'e'},
+        'env_prefixes': {},
         'extra_args': ['foo', 'bar'],
         'grace_period': 30.,
         'hard_timeout': 10,
@@ -336,6 +339,56 @@ class TestTaskRunner(TestTaskRunnerBase):
     # This runs the command for real.
     self.requests(cost_usd=1, exit_code=0)
     task_details = self.get_task_details('print(\'hi\')')
+    expected = {
+      u'exit_code': 0,
+      u'hard_timeout': False,
+      u'io_timeout': False,
+      u'must_signal_internal_failure': None,
+      u'version': task_runner.OUT_VERSION,
+    }
+    self.assertEqual(expected, self._run_command(task_details))
+
+  def test_run_command_env_prefix(self):
+    # This runs the command for real.
+    self.requests(cost_usd=1, exit_code=0,
+                  output_re='.*%slocal%ssmurf\n$' % (os.sep, os.sep))
+    task_details = self.get_task_details(
+      'import os\nprint os.getenv("PATH").split(os.pathsep)[0]',
+      env_prefixes={
+        'PATH': ['./local/smurf', './other/thing'],
+      }
+    )
+    expected = {
+      u'exit_code': 0,
+      u'hard_timeout': False,
+      u'io_timeout': False,
+      u'must_signal_internal_failure': None,
+      u'version': task_runner.OUT_VERSION,
+    }
+    self.assertEqual(expected, self._run_command(task_details))
+
+  def test_run_command_env_prefix(self):
+    # This runs the command for real.
+    self.requests(
+        cost_usd=1, exit_code=0,
+        output_re=(
+          r'^'
+          r'(?P<cwd>[^\n]*)\n'
+          r'(?P=cwd)%slocal%ssmurf\n'
+          r'(?P=cwd)%sother%sthing\n'
+          r'$'
+        ) % (os.sep, os.sep, os.sep, os.sep))
+    task_details = self.get_task_details(
+        '\n'.join([
+          'import os',
+          'print os.path.realpath(os.getcwd())',
+          'path = os.getenv("PATH").split(os.pathsep)',
+          'print os.path.realpath(path[0])',
+          'print os.path.realpath(path[1])',
+        ]),
+        env_prefixes={
+          'PATH': ['./local/smurf', './other/thing'],
+        })
     expected = {
       u'exit_code': 0,
       u'hard_timeout': False,
@@ -445,6 +498,7 @@ class TestTaskRunner(TestTaskRunnerBase):
             'thus_raising_OSError',
           ],
           'env': {},
+          'env_prefixes': {},
           'extra_args': [],
           'grace_period': 30.,
           'hard_timeout': 6,
@@ -563,6 +617,7 @@ class TestTaskRunner(TestTaskRunnerBase):
           'bot_id': 'localhost',
           'command': ['large', 'executable'],
           'env': {'foo': 'bar'},
+          'env_prefixes': {},
           'extra_args': [],
           'grace_period': 30.,
           'hard_timeout': 60,

pombredanne/https-chromium.googlesource.com-infra-luci-luci-py

commit 06fe0a7a47140ced6220d7e2a39408489ee8fc4f

swarming_bot: make task_runner more resilient to partial manifest
This was broken in bfab799cddab135075b

R=aludwin@google.com

Bug:
Change-Id: I0558300991212eaa43d90300c7e44688516c50f4

appengine/swarming/swarming_bot/bot_code/task_runner.py
@@ -197,7 +197,7 @@ class TaskDetails(object):
     }
     self.env_prefixes = {
       k.encode('utf-8'): [path.encode('utf-8') for path in v]
-      for k, v in data['env_prefixes'].iteritems()
+      for k, v in (data.get('env_prefixes') or {}).iteritems()
     }
     self.grace_period = data['grace_period']
     self.hard_timeout = data['hard_timeout']