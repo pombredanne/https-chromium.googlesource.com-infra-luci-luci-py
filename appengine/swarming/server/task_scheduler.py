luci/luci-py

commit 945d5d7da6b9f32a9523d12e9cfc8eecbb87c79a

[swarming] Add TaskRequest.manual_tags and prepare for TaskRequest.task_slices
TaskRequest.manual_tags is to differentiate between tags specified by the user
and tags that are automatically added. Tags on a TaskSlice will have the manual
tags plus the tags relevant for this specific TaskSlice. E.g. if someone
triggers first on 'gpu:foo:bar' then fallbacks on 'gpu:foo:baz', the
TaskResultSummary shall have the automatic tag from the dimension of the
TaskSlice that actually ran. Said in a different way: this means that the tags
for a TaskResultSummary shall have only the relevant tags, hence the manual tags
must be kept in a separate property in TaskRequest.

Stop using TaskRequest.properties and use TaskRequest.task_slice(index) to
prepare for TaskRequest.task_slices. Right now index must be 0, as there's only
one TaskSlice (the one stored as TaskRequest.properties). This makes later CL
much simpler.

Redo task_request_test.py TaskRequest entity creation flow as this was based on
a flow that was used in 2015.

No behavior change from the user's PoV.

Bug: 781021
Change-Id: If4e49e577ef9086cb1560e0b19c54070174c738e
Reviewed-on: https://chromium-review.googlesource.com/995621
Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

appengine/swarming/server/task_scheduler.py
@@ -47,8 +47,14 @@ def _secs_to_ms(value):
 def _expire_task(to_run_key, request):
   """Expires a TaskResultSummary and unschedules the TaskToRun.
 
+  This function is only meant to process PENDING tasks.
+
+  If a follow up TaskSlice is available, reenqueue a new TaskToRun instead of
+  expiring the TaskResultSummary.
+
   Returns:
-    TaskResultSummary on success.
+    TaskResultSummary on success, bool if reenqueued (due to following
+    TaskSlice).
   """
   # Look if the TaskToRun is reapable once before doing the check inside the
   # transaction. This reduces the likelihood of failing this check inside the
@@ -66,11 +72,21 @@ def _expire_task(to_run_key, request):
     result_summary_future = result_summary_key.get_async()
     to_run = to_run_future.get_result()
     if not to_run or not to_run.is_reapable:
-      result_summary_future.wait()
-      return None
+      result_summary_future.get_result()
+      return None, None
 
+    # In any case, dequeue the TaskToRun.
     to_run.queue_number = None
     result_summary = result_summary_future.get_result()
+    to_put = [to_run, result_summary]
+    new_to_run = None
+    if result_summary.current_task_slice < request.num_task_slices-1:
+      # Enqueue a new TasktoRun for the next TaskSlice.
+      new_to_run = task_to_run.new_task_to_run(
+          request, 1, result_summary.current_task_slice+1)
+      result_summary.current_task_slice += 1
+      to_put.append(new_to_run)
+
     if result_summary.try_number:
       # It's a retry that is being expired. Keep the old state. That requires an
       # additional pipelined GET but that shouldn't be the common case.
@@ -81,12 +97,12 @@ def _expire_task(to_run_key, request):
     result_summary.abandoned_ts = now
     result_summary.modified_ts = now
 
-    futures = ndb.put_multi_async((to_run, result_summary))
+    futures = ndb.put_multi_async(to_put)
     _maybe_pubsub_notify_via_tq(result_summary, request)
     for f in futures:
       f.check_success()
 
-    return result_summary
+    return result_summary, new_to_run
 
   # Add it to the negative cache *before* running the transaction. Either way
   # the task was already reaped or the task is correctly expired and not
@@ -95,14 +111,15 @@ def _expire_task(to_run_key, request):
 
   # It'll be caught by next cron job execution in case of failure.
   try:
-    res = datastore_utils.transaction(run)
+    res, r = datastore_utils.transaction(run)
   except datastore_utils.CommitError:
     res = None
+    r = None
   if res:
     logging.info(
         'Expired %s', task_pack.pack_result_summary_key(result_summary_key))
     ts_mon_metrics.on_task_completed(res)
-  return res
+  return res, r
 
 
 def _reap_task(bot_dimensions, bot_version, to_run_key, request):
@@ -127,13 +144,14 @@ def _reap_task(bot_dimensions, bot_version, to_run_key, request):
     # 3 GET, 1 PUT at the end.
     to_run_future = to_run_key.get_async()
     result_summary_future = result_summary_key.get_async()
-    if request.properties.has_secret_bytes:
-      secret_bytes_future = request.secret_bytes_key.get_async()
     to_run = to_run_future.get_result()
+    t = request.task_slice(to_run.task_slice_index)
+    if t.properties.has_secret_bytes:
+      secret_bytes_future = request.secret_bytes_key.get_async()
     result_summary = result_summary_future.get_result()
     orig_summary_state = result_summary.state
     secret_bytes = None
-    if request.properties.has_secret_bytes:
+    if t.properties.has_secret_bytes:
       secret_bytes = secret_bytes_future.get_result()
     if not to_run:
       logging.error('Missing TaskToRun?\n%s', result_summary.task_id)
@@ -217,15 +235,13 @@ def _handle_dead_bot(run_result_key):
   server_version = utils.get_app_version()
   packed = task_pack.pack_run_result_key(run_result_key)
   request = request_future.get_result()
-  # The TaskRunResult key id is the try number.
-  try_number = run_result_key.integer_id()
-  to_run_key = task_to_run.request_to_task_to_run_key(request, try_number, 0)
 
   def run():
-    """Returns tuple(task_is_retried or None, bot_id)."""
-    # Do one GET, one PUT at the end.
-    run_result, result_summary, to_run = ndb.get_multi(
-        (run_result_key, result_summary_key, to_run_key))
+    """Returns tuple(task_is_retried or None, bot_id).
+
+    1x GET, 1x GETs 2~3x PUT.
+    """
+    run_result = run_result_key.get()
     if run_result.state != task_result.State.RUNNING:
       # It was updated already or not updating last. Likely DB index was stale.
       return None, run_result.bot_id
@@ -233,10 +249,12 @@ def _handle_dead_bot(run_result_key):
       # The query index IS stale.
       return None, run_result.bot_id
 
+    current_task_slice = run_result.current_task_slice
     run_result.signal_server_version(server_version)
     old_modified = run_result.modified_ts
     run_result.modified_ts = now
 
+    result_summary = result_summary_key.get()
     orig_summary_state = result_summary.state
     if result_summary.try_number != run_result.try_number:
       # Not updating correct run_result, cancel it without touching
@@ -247,7 +265,7 @@ def _handle_dead_bot(run_result_key):
       run_result.abandoned_ts = now
       task_is_retried = None
     elif (result_summary.try_number == 1 and now < request.expiration_ts and
-          (request.properties.idempotent or
+          (request.task_slice(current_task_slice).properties.idempotent or
             run_result.started_ts == old_modified)):
       # Retry it. It fits:
       # - first try
@@ -256,8 +274,13 @@ def _handle_dead_bot(run_result_key):
       #   - idempotent
       #   - task hadn't got any ping at all from task_runner.run_command()
       # TODO(maruel): Allow retry for bot locked task using 'id' dimension.
+      # TODO(maruel): Allow increasing the current_task_slice value.
       # Create a second TaskToRun.
-      to_run = task_to_run.new_task_to_run(request, 2, 0)
+      to_run = task_to_run.new_task_to_run(request, 2, current_task_slice)
+
+      # Remove it from the negative cache.
+      task_to_run.set_lookup_cache(to_run.key, True)
+
       to_put = (run_result, result_summary, to_run)
       run_result.state = task_result.State.BOT_DIED
       run_result.internal_failure = True
@@ -285,11 +308,6 @@ def _handle_dead_bot(run_result_key):
       f.check_success()
 
     return task_is_retried
-
-  # Remove it from the negative cache *before* running the transaction. Either
-  # way the TaskToRun.queue_number was not set so there was no contention on
-  # this entity. At best the task is reenqueued for a retry.
-  task_to_run.set_lookup_cache(to_run_key, True)
 
   try:
     task_is_retried = datastore_utils.transaction(run)
@@ -640,8 +658,9 @@ def check_schedule_request_acl(request):
   # hitting this function, and so we can assume there's a pool set (this is
   # checked in TaskProperties's pre put hook).
   #
-  # It is guaranteed that at most one item can be specified in 'pool'.
-  pool = request.properties.dimensions[u'pool'][0]
+  # It is guaranteed that at most one item can be specified in 'pool' and that
+  # every TaskSlice property dimensions use the same pool and id dimensions.
+  pool = request.task_slice(0).properties.dimensions[u'pool'][0]
   pool_cfg = pools_config.get_pool_config(pool)
 
   # request.service_account can be 'bot' or 'none'. We don't care about these,
@@ -713,7 +732,7 @@ def schedule_request(request, secret_bytes):
 
   now = utils.utcnow()
   request.key = task_request.new_request_key()
-  # This is the first try.
+  # First try, first task slice.
   to_run = task_to_run.new_task_to_run(request, 1, 0)
   result_summary = task_result.new_result_summary(request)
   result_summary.modified_ts = now
@@ -734,8 +753,9 @@ def schedule_request(request, secret_bytes):
     return key
 
   deduped = False
-  if request.properties.idempotent:
-    dupe_summary = _find_dupe_task(now, request.properties_hash())
+  t = request.task_slice(0)
+  if t.properties.idempotent:
+    dupe_summary = _find_dupe_task(now, t.properties_hash())
     if dupe_summary:
       _copy_summary(
           dupe_summary, result_summary,
@@ -817,6 +837,7 @@ def bot_reap_task(bot_dimensions, bot_version, deadline):
   start = time.time()
   bot_id = bot_dimensions[u'id'][0]
   iterated = 0
+  reenqueueds = 0
   expired = 0
   failures = 0
   stale_index = 0
@@ -826,7 +847,12 @@ def bot_reap_task(bot_dimensions, bot_version, deadline):
     for request, to_run in q:
       iterated += 1
       if request.expiration_ts < utils.utcnow():
-        if _expire_task(to_run.key, request):
+        s, r = _expire_task(to_run.key, request)
+        if r:
+          # Expiring a TaskToRun for TaskSlice may reenqueue a new TaskToRun.
+          # It'll be processed accordingly but not handled here.
+          reenqueueds += 1
+        elif s:
           expired += 1
         else:
           stale_index += 1
@@ -846,9 +872,10 @@ def bot_reap_task(bot_dimensions, bot_version, deadline):
     return None, None, None
   finally:
     logging.debug(
-        'bot_reap_task(%s) in %.3fs: %d iterated, %d expired, %d stale_index, '
-        '%d failured',
-        bot_id, time.time()-start, iterated, expired, stale_index, failures)
+        'bot_reap_task(%s) in %.3fs: %d iterated, %d reenqueued, %d expired, '
+        '%d stale_index, %d failured',
+        bot_id, time.time()-start, iterated, reenqueueds, expired, stale_index,
+        failures)
 
 
 def bot_update_task(
@@ -1015,7 +1042,8 @@ def cancel_task(request, result_key, kill_running):
   now = utils.utcnow()
 
   def run():
-    """1x DB GET, 1x DB GET, 2x DB PUTs, 1x task queue."""
+    """1 DB GET, 1 memcache write, 2x DB PUTs, 1x task queue."""
+    # Need to get the current try number to know which TaskToRun to fetch.
     result_summary = result_key.get()
     was_running = result_summary.state == task_result.State.RUNNING
     if not result_summary.can_be_canceled:
@@ -1025,7 +1053,16 @@ def cancel_task(request, result_key, kill_running):
     if not was_running:
       # PENDING.
       result_summary.state = task_result.State.CANCELED
-      to_run = task_to_run.request_to_task_to_run_key(request, 1, 0).get()
+      to_run_key = task_to_run.request_to_task_to_run_key(
+          request,
+          result_summary.try_number or 1,
+          result_summary.current_task_slice or 0)
+      to_run_future = to_run_key.get_async()
+
+      # Add it to the negative cache.
+      task_to_run.set_lookup_cache(to_run_key, False)
+
+      to_run = to_run_future.get_result()
       entities.append(to_run)
       to_run.queue_number = None
     else:
@@ -1052,23 +1089,12 @@ def cancel_task(request, result_key, kill_running):
       f.check_success()
     return True, was_running
 
-  # Add it to the negative cache *before* running the transaction. Either way
-  # the task was already reaped or the task is correctly canceled thus not
-  # reapable.
-  for i in (1, 2):
-    # We don't know in advance if it's try number 1 or 2, so do both. Since it's
-    # an memcache RPC, it's not too costly.
-    # TODO(maruel): Refactor to do both simultaneously to save 2ms.
-    to_run_key = task_to_run.request_to_task_to_run_key(request, i, 0)
-    task_to_run.set_lookup_cache(to_run_key, False)
-
   try:
     ok, was_running = datastore_utils.transaction(run)
   except datastore_utils.CommitError as e:
     packed = task_pack.pack_result_summary_key(result_key)
     return 'Failed killing task %s: %s' % (packed, e)
 
-  # TODO(maruel): Add paper trail.
   return ok, was_running
 
 
@@ -1094,12 +1120,16 @@ def cron_abort_expired_task_to_run(host):
     Packed tasks ids of aborted tasks.
   """
   killed = []
+  reenqueueds = 0
   skipped = 0
   try:
     for to_run in task_to_run.yield_expired_task_to_run():
       request = to_run.request_key.get()
-      summary = _expire_task(to_run.key, request)
-      if summary:
+      summary, reenqueued = _expire_task(to_run.key, request)
+      if reenqueued:
+        # Expiring a TaskToRun for TaskSlice may reenqueue a new TaskToRun.
+        reenqueueds += 1
+      elif summary:
         # TODO(maruel): Know which try it is.
         killed.append(request)
       else:
@@ -1111,9 +1141,12 @@ def cron_abort_expired_task_to_run(host):
           'EXPIRED!\n%d tasks:\n%s',
           len(killed),
           '\n'.join(
-            '  %s/user/task/%s  %s' % (host, i.task_id, i.properties.dimensions)
+            '  %s/user/task/%s  %s' % (
+              host, i.task_id, i.task_slice(0).properties.dimensions)
             for i in killed))
-    logging.info('Killed %d task, skipped %d', len(killed), skipped)
+    logging.info(
+        'Reenqueued %d tasks, killed %d, skipped %d',
+        reenqueueds, len(killed), skipped)
   return [i.task_id for i in killed]
 
 

appengine/swarming/event_mon_metrics.py
@@ -74,7 +74,8 @@ def _task_summary_to_proto(summary, event):
   if summary.request.pubsub_topic:
     request_proto.pubsub_topic = summary.request.pubsub_topic
 
-  task_properties = summary.request.properties
+  task_properties = summary.request.task_slice(
+      summary.current_task_slice).properties
   properties_proto = request_proto.properties
 
   if task_properties.inputs_ref:

appengine/swarming/handlers_backend.py
@@ -155,10 +155,13 @@ class CronCountTaskBotDistributionHandler(webapp2.RequestHandler):
             task_result.State.STATES_RUNNING))
     for result in q:
       # Make dimensions immutable so they can be used to index a key.
-      dimensions = tuple(sorted(
-            (k, tuple(sorted(v)))
-            for k, v in result.request.properties.dimensions.iteritems()))
-      n_tasks_by_dimensions[dimensions] += 1
+      req = result.request
+      for i in xrange(req.num_task_slices):
+        t = req.task_slice(i)
+        dimensions = tuple(sorted(
+              (k, tuple(sorted(v)))
+              for k, v in t.properties.dimensions.iteritems()))
+        n_tasks_by_dimensions[dimensions] += 1
 
     # Count how many bots have those dimensions for each set.
     n_bots_by_dimensions = {}

appengine/swarming/handlers_bot.py
@@ -665,7 +665,8 @@ class BotPollHandler(_BotBaseHandler):
       try:
         # This part is tricky since it intentionally runs a transaction after
         # another one.
-        if request.properties.is_terminate:
+        if request.task_slice(
+            run_result.current_task_slice).properties.is_terminate:
           bot_event('bot_terminate', task_id=run_result.task_id)
           self._cmd_terminate(run_result.task_id)
         else:
@@ -673,8 +674,7 @@ class BotPollHandler(_BotBaseHandler):
               'request_task', task_id=run_result.task_id,
               task_name=request.name)
           self._cmd_run(
-              request, secret_bytes, run_result.key,
-              res.bot_id, res.bot_group_cfg)
+              request, secret_bytes, run_result, res.bot_id, res.bot_group_cfg)
       except:
         logging.exception('Dang, exception after reaping')
         raise
@@ -688,10 +688,9 @@ class BotPollHandler(_BotBaseHandler):
       # https://code.google.com/p/swarming/issues/detail?id=130
       self.abort(500, 'Deadline')
 
-  def _cmd_run(
-      self, request, secret_bytes, run_result_key, bot_id, bot_group_cfg):
+  def _cmd_run(self, request, secret_bytes, run_result, bot_id, bot_group_cfg):
     logging.info('Run: %s', request.task_id)
-    props = request.properties
+    props = request.task_slice(run_result.current_task_slice).properties
     out = {
       'cmd': 'run',
       'manifest': {
@@ -733,7 +732,7 @@ class BotPollHandler(_BotBaseHandler):
             'service_account': request.service_account,
           },
         },
-        'task_id': task_pack.pack_run_result_key(run_result_key),
+        'task_id': task_pack.pack_run_result_key(run_result.key),
       },
     }
     self.send_response(utils.to_json_encodable(out))

appengine/swarming/handlers_endpoints.py
@@ -393,10 +393,12 @@ class SwarmingTasksService(remote.Service):
     try:
       request_obj, secret_bytes = message_conversion.new_task_request_from_rpc(
           request, utils.utcnow())
-      apply_server_property_defaults(request_obj.properties)
+      for index in xrange(request_obj.num_task_slices):
+        apply_server_property_defaults(request_obj.task_slice(index).properties)
       task_request.init_new_request(
           request_obj, acl.can_schedule_high_priority_tasks())
     except (datastore_errors.BadValueError, TypeError, ValueError) as e:
+      logging.exception('Here\'s what was wrong in the user new task request:')
       raise endpoints.BadRequestException(e.message)
 
     # Make sure the caller is actually allowed to schedule the task before
@@ -413,10 +415,7 @@ class SwarmingTasksService(remote.Service):
         raise endpoints.BadRequestException(
             'This Swarming server doesn\'t support task service accounts '
             'because Token Server URL is not configured')
-      max_lifetime_secs = (
-          request_obj.expiration_secs +
-          request_obj.properties.execution_timeout_secs +
-          request_obj.properties.grace_period_secs)
+      max_lifetime_secs = request_obj.max_lifetime_secs
       try:
         # Note: this raises AuthorizationError if the user is not allowed to use
         # the requested account or service_accounts.InternalError if something

appengine/swarming/handlers_endpoints_test.py
@@ -599,6 +599,91 @@ class TasksApiTest(BaseTest):
     response = self.call_api('new', body=message_to_dict(request))
     self.assertEqual(expected, response.json)
 
+  def test_new_task_slices_one(self):
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+    now = datetime.datetime(2010, 1, 2, 3, 4, 5)
+    self.mock_now(now)
+
+    task_slices = [
+      {
+        u'properties': self.create_props(command=['python', 'run_test.py']),
+        u'expiration_secs': 180,
+      },
+    ]
+    response, _ = self.client_create_task(
+        expiration_secs=None, task_slices=task_slices)
+    expected_props = self.gen_props(command=[u'python', u'run_test.py'])
+    expected = {
+      u'request': self.gen_request(
+          created_ts=fmtdate(self.now),
+          expiration_secs=u'180',
+          properties=expected_props,
+          task_slices=[
+            {
+              u'expiration_secs': u'180',
+              u'properties': expected_props,
+            },
+          ]),
+      u'task_id': u'5cee488008810',
+    }
+    self.assertEqual(expected, response)
+
+  def test_new_task_slices_two(self):
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+    now = datetime.datetime(2010, 1, 2, 3, 4, 5)
+    self.mock_now(now)
+
+    task_slices = [
+      {
+        u'properties': self.create_props(command=['python', 'run_test.py']),
+        u'expiration_secs': 180,
+      },
+      {
+        u'properties': self.create_props(command=['python', 'run_test.py']),
+        u'expiration_secs': 180,
+      },
+    ]
+    # This will soon work.
+    with self.assertRaises(webtest.AppError):
+      self.client_create_task(
+          expiration_secs=None, task_slices=task_slices)
+
+  def test_new_task_slices_two_denied(self):
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+    now = datetime.datetime(2010, 1, 2, 3, 4, 5)
+    self.mock_now(now)
+
+    cfg = config.settings()
+    cfg.isolate.default_server = 'https://isolateserver.appspot.com'
+    cfg.isolate.default_namespace = 'default-gzip'
+    self.mock(config, 'settings', lambda: cfg)
+
+    task_slices = [
+      {
+        u'properties': self.create_props(command=['python', 'run_test.py']),
+        u'expiration_secs': 180,
+      },
+      {
+        u'properties': self.create_props(command=['python', 'run_test.py']),
+        # That's incorrect:
+        u'expiration_secs': 0,
+      },
+    ]
+    request = swarming_rpcs.NewTaskRequest(
+        expiration_secs=None,
+        name='hi',
+        priority=10,
+        tags=[],
+        task_slices=task_slices,
+        user='joe@localhost')
+    resp = self.call_api('new', body=message_to_dict(request), status=400)
+    expected = {
+      u'state': u'APPLICATION_ERROR',
+      # This will change for an error message about expiration_secs.
+      u'error_message': u'Only one task_slices entry is supported for now',
+    }
+    self.assertEqual(expected, resp.json)
+
   def test_mass_cancel(self):
     # Create two tasks.
     self.mock(random, 'getrandbits', lambda _: 0x88)

appengine/swarming/message_conversion.py
@@ -245,6 +245,9 @@ def new_task_request_from_rpc(msg, now):
       # It is set in task_request.init_new_request().
       authenticated=None,
       properties=props,
+      # 'tags' is now generated from manual_tags plus automatic tags.
+      tags=None,
+      manual_tags=msg.tags,
       # This is internal field not settable via RPC.
       service_account_token=None)
 

appengine/swarming/server/service_accounts_test.py
@@ -164,7 +164,7 @@ class TaskAccountTokenTest(TestBase):
         'io_timeout_secs': None,
       }),
       'expiration_ts': now + datetime.timedelta(seconds=60),
-      'tags': [u'tag:1'],
+      'manual_tags': [u'tag:1'],
       'user': 'Someone',
     }
     req = task_request.TaskRequest(**args)

pombredanne/https-chromium.googlesource.com-infra-luci-luci-py

commit 734b7068c363b95c68f1c4ea0a24740c98d00272

[swarming] Fix bug introduced in 945d5d7da6b9f32a952.
This very rarely occurs, only during a very short window where the negative
cache was not caught, and the item was not detected as reapeable even in the
index.

TBR=iannucci@chromium.org

Bug: 781021
Change-Id: I1f4977fff59ac95ca81c6a2053da77d3d872ff52

appengine/swarming/server/task_scheduler.py
@@ -61,7 +61,7 @@ def _expire_task(to_run_key, request):
   # transaction, which is an order of magnitude more costly.
   if not to_run_key.get().is_reapable:
     logging.info('Not reapable anymore')
-    return None
+    return None, None
 
   result_summary_key = task_pack.request_key_to_result_summary_key(request.key)
   now = utils.utcnow()