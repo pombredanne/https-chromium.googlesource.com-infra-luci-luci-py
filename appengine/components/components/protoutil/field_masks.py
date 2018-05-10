e570061dab95d5ada0d801b40a9914b4d59246ab
Nodir Turakulov <nodir@google.com>
2018-05-10 11:01:18 -0700

    [protoutil] Add field-mask-based msg trimming
    Add trim_message(msg, field_mask) function. Useful for partial responses.
    Will be used in Buildbucket API v2.
    
    Bug: 832733
    Change-Id: Ie07a00cfa28d5f09313f5d771efa30c4c911abaf

---
+++ appengine/components/components/protoutil/field_masks.py
@@ -0,0 +1,33 @@
+# Copyright 2018 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+"""Utility functions for google.protobuf.field_mask_pb2.FieldMask."""
+
+
+def trim_message(msg, field_mask):
+  """Clears msg fields that are not in the field_mask.
+
+  Args:
+    msg: a google.protobuf.message.Message instance.
+    field_mask: a google.protobuf.field_mask_pb2.FieldMask instance.
+  """
+  _trim(msg, _field_mask_to_dict(field_mask))
+
+
+def _trim(msg, mask):
+  for f, v in msg.ListFields():
+    fm = mask.get(f.name)
+    if fm is None:
+      msg.ClearField(f.name)
+    elif f.message_type and len(fm) > 0:
+      _trim(v, fm)
+
+
+def _field_mask_to_dict(field_mask):
+  ret = {}
+  for path in field_mask.paths:
+    cur = ret
+    for comp in path.split('.'):
+      cur = cur.setdefault(str(comp), {})
+  return ret
