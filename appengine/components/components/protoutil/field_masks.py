fc7ef63ece542214c8b855a5ad51d090d86aac0e
Nodir Turakulov <nodir@google.com>
2018-05-16 16:58:35 -0700

    [protoutil] Add field-mask-based msg trimming
    Add trim_message(msg, field_mask) function. Useful for partial responses.
    Supports advanced field mask semantics.
    Will be used in Buildbucket API v2.
    
    Bug: 832733
    Change-Id: Ie07a00cfa28d5f09313f5d771efa30c4c911abaf

---
+++ appengine/components/components/protoutil/field_masks.py
@@ -0,0 +1,453 @@
+# Copyright 2018 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+"""Utility functions for google.protobuf.field_mask_pb2.FieldMask.
+
+Supports advanced field mask semantics:
+- Refer to fields and map keys using . literals:
+  - Supported map key types: string, integer types, bool.
+  - Floating point (including double and float), enum, and bytes keys are not
+    supported by protobuf or this implementation.
+  - Fields: 'publisher.name' means field name of field publisher
+  - string map keys: 'metadata.year' means string key 'year' of map field
+    metadata
+  - integer map keys (e.g. int32): 'year_ratings.0' means integer key 0 of a map
+    field year_ratings
+  - bool map keys: 'access_text.true' means boolean key true of a map field
+    access_text
+- String map keys that cannot be represented as an unquoted string literal,
+  must be quoted using backticks: metadata.`year.published`, metadata.`17`,
+  metadata.``. Backtick can be escaped with ``: a.`b``c` means map key "b`c"
+  of map field a.
+- Refer to all map keys using a * literal: topics.*.archived
+- Refer to all elements of a repeated field using a * literal: authors.*.name
+- Refer to all fields of a message using * literal: publisher.*.
+- Prohibit addressing a single element in repeated fields: authors.0.name
+
+TODO(nodir): replace spec above with a link to a spec when it is avialble.
+"""
+
+import contextlib
+
+from google import protobuf
+from google.protobuf import descriptor
+
+
+__all__ = [
+    'parse_field_tree',
+    'trim_message',
+]
+
+
+def parse_field_tree(field_mask, descriptor):
+  """Parses a field mask to a tree of fields.
+
+  Each node represents a field and in turn is represented by a dict where each
+  dict key is a child key and dict value is a child node. For example, parses
+  ['a', 'b.c'] to {'a': {}, 'b': {'c': {}}}.
+
+  Removes trailing stars, e.g. parses ['a.*'] to {'a': {}}.
+  Removes redundant paths, e.g. parses ['a', 'a.b'] as {'a': {}}.
+
+  Args:
+    field_mask: a google.protobuf.field_mask_pb2.FieldMask instance.
+    descriptor: a google.protobuf.descriptor.Descriptor for the target message.
+
+  Raises:
+    ValueError if a field path is invalid.
+  """
+  parsed_paths = []
+  for p in field_mask.paths:
+    try:
+      parsed_paths.append(_parse_path(p, descriptor))
+    except ValueError as ex:
+      raise ValueError('invalid path "%s": %s' % (p, ex))
+
+  parsed_paths = _normalize_paths(parsed_paths)
+
+  root = {}
+  for p in parsed_paths:
+    node = root
+    for seg in p:
+      node = node.setdefault(seg, {})
+  return root
+
+
+def trim_message(message, field_tree):
+  """Clears msg fields that are not in the field_mask.
+
+  If field_mask is empty, this is a noop.
+
+  Args:
+    message: a google.protobuf.message.Message instance.
+    field_tree: a field tree parsed using parse_field_tree.
+      The desciptor used in parse_field_tree must match message.DESCRIPTOR.
+
+  Raises:
+    ValueError if a field mask path is invalid.
+  """
+
+  # Path to the current field value.
+  path = []
+
+  @contextlib.contextmanager
+  def with_seg(seg):
+    path.append(seg)
+    try:
+      yield _must_include(field_tree, tuple(path))
+    finally:
+      path.pop()
+
+  def trim_msg(msg):
+    for f, v in msg.ListFields():
+      with with_seg(f.name) as incl:
+        if incl == _INCLUDE_ENTIRELY:
+          continue
+
+        if incl == _EXCLUDE:
+          msg.ClearField(f.name)
+          continue
+
+        assert incl == _INCLUDE_PARTIALLY
+
+        if not f.message_type:
+          # The field is scalar, but the field mask does not specify to include
+          # it entirely. Skip it because scalars do not have subfields.
+          # Note that parse_field_tree would fail on such a mask because
+          # a scalar field cannot be followed by other fields.
+          msg.ClearField(f.name)
+          continue
+
+        # Trim the field value.
+        if f.message_type.GetOptions().map_entry:
+          for mk, mv in v.items():
+            with with_seg(mk) as incl:
+              if incl == _INCLUDE_ENTIRELY:
+                pass
+              elif incl == _EXCLUDE:
+                v.pop(mk)
+              elif isinstance(mv, protobuf.message.Message):
+                trim_msg(mv)
+              else:
+                # Same reasoning as above.
+                v.pop(mk)
+        elif f.label == descriptor.FieldDescriptor.LABEL_REPEATED:
+          with with_seg(_STAR_SEG):
+            for rv in v:
+              trim_msg(rv)
+        else:
+          trim_msg(v)
+
+  trim_msg(message)
+
+
+# Used in a parsed path to represent a star literal.
+# See _parse_path.
+_STAR_SEG = object()
+
+# Token types.
+_STAR, _PERIOD, _LITERAL, _QUOTED_STRING = xrange(4)
+
+
+def _tokenize(path):
+  """Transforms path to an iterator of (token_type, string) tuples.
+
+  Raises:
+    ValueError if a quoted string is not closed.
+  """
+  assert isinstance(path, basestring), path
+
+  i = 0
+  while i < len(path):
+    if path[i] == '`':
+      # This is a quoted string.
+
+      i += 1  # Swallow backtick.
+
+      quoted_string = []  # Parsed quoted string as list of string parts.
+      while True:
+        next_backtick = path.find('`', i)
+        if next_backtick == -1:
+          raise ValueError('a quoted string is not closed')
+
+        quoted_string.append(path[i:next_backtick])
+        i = next_backtick + 1  # Swallow the discovered backtick.
+
+        escaped_backtick = i < len(path) and path[i] == '`'
+        if not escaped_backtick:
+          break
+        quoted_string.append('`')
+        i += 1  # Swallow second backtick.
+
+      yield (_QUOTED_STRING, ''.join(quoted_string))
+    elif path[i] == '*':
+      i += 1  # Swallow star.
+      yield (_STAR, '*')
+    elif path[i] == '.':
+      i += 1  # Swallow period.
+      yield (_PERIOD, '.')
+    else:
+      start = i
+      # Scan the string until a delimiter or EOF.
+      while i < len(path) and path[i] not in ('.', '*', '`'):
+        i += 1  # Swallow character.
+      assert i > start
+      yield (_LITERAL, path[start:i])
+
+
+_INTEGER_FIELD_TYPES = {
+    descriptor.FieldDescriptor.TYPE_INT64,
+    descriptor.FieldDescriptor.TYPE_INT32,
+    descriptor.FieldDescriptor.TYPE_UINT32,
+    descriptor.FieldDescriptor.TYPE_UINT64,
+    descriptor.FieldDescriptor.TYPE_FIXED64,
+    descriptor.FieldDescriptor.TYPE_FIXED32,
+    descriptor.FieldDescriptor.TYPE_SFIXED64,
+    descriptor.FieldDescriptor.TYPE_SFIXED32,
+}
+_SUPPORTED_MAP_KEY_TYPES = _INTEGER_FIELD_TYPES | {
+    descriptor.FieldDescriptor.TYPE_STRING,
+    descriptor.FieldDescriptor.TYPE_BOOL,
+}
+
+
+class _ParseContext(object):
+  def __init__(self, desc):
+    self.i = 0
+    self.desc = desc
+    self.expect_star = False
+    self._field_path = []  # full path of the urrent field
+
+  def advance_to_field(self, field):
+    self.desc = field.message_type
+    self.expect_star = (
+        field.label == descriptor.FieldDescriptor.LABEL_REPEATED
+        and not (self.desc and self.desc.GetOptions().map_entry))
+    self._field_path.append(field.name)
+
+  @property
+  def field_path(self):
+    return '.'.join(self._field_path)
+
+
+def _parse_path(path, desc):
+  """Parses a field path to a tuple of segments.
+
+  Grammar:
+    path = segment {'.' segment}
+    segment = literal | '*' | quoted_string;
+    literal = string | integer | bool
+    string = (letter | '_') {letter | '_' | digit}
+    integer = ['-'] digit {digit};
+    bool = 'true' | 'false';
+    quoted_string = '`' { utf8-no-backtick | '``' } '`'
+
+  Args:
+    path: a field path.
+    desc: a google.protobuf.descriptor.Descriptor of the target message.
+
+  Returns:
+    A tuple of segments. A star is returned as _STAR_SEG object.
+
+  Raises:
+    ValueError if path is invalid.
+  """
+  tokens = list(_tokenize(path))
+  ctx = _ParseContext(desc)
+  eof = lambda: ctx.i >= len(tokens)
+
+  def peek():
+    if eof():
+      raise ValueError('unexpected end')
+    return tokens[ctx.i]
+
+  def read():
+    tok = peek()
+    ctx.i += 1
+    return tok
+
+  def read_path():
+    segs = []
+    while True:
+      seg, must_be_last = read_segment()
+      segs.append(seg)
+
+      if eof():
+        break
+
+      tok_type, tok = read()
+      if must_be_last:
+        raise ValueError('unexpected token "%s"; expected end of string' % tok)
+      if tok_type != _PERIOD:
+        raise ValueError('unexpected token "%s"; expected a period' % tok)
+    return tuple(segs)
+
+  def read_segment():
+    """Returns (segment, must_be_last) tuple."""
+    tok_type, tok = peek()
+    assert tok
+    if tok_type == _PERIOD:
+      raise ValueError('a segment cannot start with a period')
+
+    if ctx.expect_star:
+      if tok_type != _STAR:
+        raise ValueError('unexpected token "%s", expected a star' % tok)
+      read()  # Swallow star.
+      ctx.expect_star = False
+      return _STAR_SEG, False
+
+    if ctx.desc is None:
+      raise ValueError(
+          'scalar field "%s" cannot have subfields' % ctx.field_path)
+
+    if ctx.desc.GetOptions().map_entry:
+      key_type = ctx.desc.fields_by_name['key'].type
+      if key_type not in _SUPPORTED_MAP_KEY_TYPES:
+        raise ValueError(
+            'unsupported key type of field "%s"' % ctx.field_path)
+      if tok_type == _STAR:
+        read()  # Swallow star.
+        seg = _STAR_SEG
+      elif key_type == descriptor.FieldDescriptor.TYPE_BOOL:
+        seg = read_bool()
+      elif key_type in _INTEGER_FIELD_TYPES:
+        seg = read_integer()
+      else:
+        assert key_type == descriptor.FieldDescriptor.TYPE_STRING
+        if tok_type == _QUOTED_STRING:
+          read()  # Swallow quoted string.
+          seg = tok
+        elif tok_type == _LITERAL:
+          seg = read_string()
+        else:
+          raise ValueError('unexpected token "%s"; expected a string' % tok)
+
+      ctx.advance_to_field(ctx.desc.fields_by_name['value'])
+      return seg, False
+
+    if tok_type == _STAR:
+      # Include all fields.
+      read()  # Swallow star.
+       # A _STAR_SEG field cannot be followed by subfields.
+      return _STAR_SEG, True
+
+    field_name = read_string()
+    field = ctx.desc.fields_by_name.get(field_name)
+    if field is None:
+      prefix = ctx.field_path
+      full_name = '%s.%s' % (prefix, field_name) if prefix else field_name
+      raise ValueError('field "%s" does not exist' % full_name)
+    ctx.advance_to_field(field)
+    return field_name, False
+
+  def read_bool():
+    tok_type, tok = read()
+    if tok_type == _LITERAL and tok in ('true', 'false'):
+      return tok == 'true'
+    raise ValueError(
+        'unexpected token "%s", expected true or false' % tok)
+
+  def read_integer():
+    tok_type, tok = read()
+    if tok_type == _LITERAL:
+      try:
+        return int(tok)
+      except ValueError:
+        pass
+    raise ValueError('unexpected token "%s"; expected an integer' % tok)
+
+  def read_string():
+    tok_type, tok = read()
+    if tok_type != _LITERAL:
+      raise ValueError(
+          'unexpected token "%s"; expected an unquoted string literal' % tok)
+
+    first, rem = tok[0], tok[1:]
+    if first == '_' or first.isalpha():
+      if not rem or all(c == '_' or c.isalnum() for c in rem):
+        return tok
+    raise ValueError('invalid unquoted string "%s"' % tok)
+
+  return read_path()
+
+
+def _normalize_paths(paths):
+  """Normalizes field paths. Returns a new set of paths.
+
+  paths must be parsed, see _parse_path.
+
+  Removes trailing stars, e.g. convertes ('a', _STAR_SEG) to ('a',).
+
+  Removes paths that have a segment prefix already present in paths,
+  e.g. removes ('a', 'b') from [('a', 'b'), ('a',)].
+  """
+  paths = _remove_trailing_stars(paths)
+
+  present = set(paths)
+  ret = set()
+  for p in paths:
+    redundant = False
+    for i in xrange(len(p)):
+      if p[:i] in present:
+        redundant = True
+        break
+    if not redundant:
+      ret.add(p)
+
+  return ret
+
+
+def _remove_trailing_stars(paths):
+  ret = set()
+  for p in paths:
+    assert isinstance(p, tuple), p
+    if p[-1] == _STAR_SEG:
+      p = p[:-1]
+    ret.add(p)
+  return ret
+
+
+_EXCLUDE = 0
+_INCLUDE_PARTIALLY = 1
+_INCLUDE_ENTIRELY = 2
+
+
+def _must_include(field_tree, path):
+  """Tells if a field value at the given path must be included in the response.
+
+  Args:
+    field_tree: a dict of fields, see parse_field_tree.
+    path: a tuple of path segments.
+
+  Returns:
+    _EXCLUDE if the field value must be excluded.
+    _INCLUDE_PARTIALLY if some subfields of the field value must be included.
+    _INCLUDE_ENTIRELY if the field value must be included entirely.
+  """
+  assert isinstance(path, tuple), path
+  assert path
+
+  def include(n, i):
+    if len(n) == 0:
+      # n is a leaf.
+      return _INCLUDE_ENTIRELY
+
+    if i == len(path):
+      # n is an intermediate node and we've exhausted path.
+      # Some of the value's subfields are included, so include this value
+      # partially.
+      return _INCLUDE_PARTIALLY
+
+    # Find children that match current segment.
+    seg = path[i]
+    children = [n.get(seg)]
+    if seg != _STAR_SEG:
+      children.append(n.get(_STAR_SEG))
+    children = [c for c in children if c is not None]
+    if not children:
+      # Nothing matched.
+      return _EXCLUDE
+    return max(include(c, i + 1) for c in children)
+
+  return include(field_tree, 0)
---
+++ appengine/components/components/protoutil/field_masks_test.py
@@ -0,0 +1,436 @@
+#!/usr/bin/env python
+# Copyright 2018 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import sys
+import unittest
+
+from test_support import test_env
+test_env.setup_test_env()
+
+# google.protobuf package requires some python package magic hacking.
+from components import utils
+utils.fix_protobuf_package()
+
+from google.protobuf import field_mask_pb2
+
+import field_masks
+import test_proto_pb2
+
+
+class ParsePathTests(unittest.TestCase):
+  def parse(self, path):
+    return field_masks._parse_path(path, test_proto_pb2.Msg.DESCRIPTOR)
+
+  def test_str(self):
+    actual = self.parse('str')
+    expected = ('str',)
+    self.assertEqual(actual, expected)
+
+  def test_str_str(self):
+    err_pattern = r'scalar field "str" cannot have subfields'
+    with self.assertRaisesRegexp(ValueError, err_pattern):
+      self.parse('str.str')
+
+  def test_str_invalid_char(self):
+    with self.assertRaisesRegexp(ValueError, r'invalid unquoted string "str@"'):
+      self.parse('str@')
+
+  def test_str_repeated(self):
+    actual = self.parse('strs')
+    expected = ('strs',)
+    self.assertEqual(actual, expected)
+
+  def test_str_repeated_trailing_star(self):
+    actual = self.parse('strs.*')
+    expected = ('strs', field_masks._STAR_SEG)
+    self.assertEqual(actual, expected)
+
+  def test_str_repeated_index(self):
+    err_pattern = r'unexpected token "1", expected a star'
+    with self.assertRaisesRegexp(ValueError, err_pattern):
+      self.parse('strs.1')
+
+  def test_map_num_key(self):
+    actual = self.parse('map_num_str.1')
+    expected = ('map_num_str', 1)
+    self.assertEqual(actual, expected)
+
+  def test_map_num_key_negative(self):
+    actual = self.parse('map_num_str.-1')
+    expected = ('map_num_str', -1)
+    self.assertEqual(actual, expected)
+
+  def test_map_num_key_invalid(self):
+    with self.assertRaisesRegexp(ValueError, r'expected an integer'):
+      self.parse('map_num_str.a')
+
+  def test_map_num_key_invalid_with_correct_prefix(self):
+    with self.assertRaisesRegexp(ValueError, r'expected an integer'):
+      self.parse('map_num_str.1a')
+
+  def test_map_str_key_unquoted(self):
+    actual = self.parse('map_str_num.a')
+    expected = ('map_str_num', 'a')
+    self.assertEqual(actual, expected)
+
+  def test_map_str_key_unquoted_longer(self):
+    actual = self.parse('map_str_num.ab')
+    expected = ('map_str_num', 'ab')
+    self.assertEqual(actual, expected)
+
+  def test_map_str_key_quoted(self):
+    actual = self.parse('map_str_num.`a`')
+    expected = ('map_str_num', 'a')
+    self.assertEqual(actual, expected)
+
+  def test_map_str_key_quoted_with_period(self):
+    actual = self.parse('map_str_num.`a.b`')
+    expected = ('map_str_num', 'a.b')
+    self.assertEqual(actual, expected)
+
+  def test_map_str_key_star(self):
+    actual = self.parse('map_str_num.*')
+    expected = ('map_str_num', field_masks._STAR_SEG)
+    self.assertEqual(actual, expected)
+
+  def test_map_bool_key_true(self):
+    actual = self.parse('map_bool_str.true')
+    expected = ('map_bool_str', True)
+    self.assertEqual(actual, expected)
+
+  def test_map_bool_key_false(self):
+    actual = self.parse('map_bool_str.false')
+    expected = ('map_bool_str', False)
+    self.assertEqual(actual, expected)
+
+  def test_map_bool_key_invalid(self):
+    with self.assertRaisesRegexp(ValueError, r'expected true or false'):
+      self.parse('map_bool_str.not-a-bool')
+
+  def test_map_bool_key_star(self):
+    actual = self.parse('map_bool_str.*')
+    expected = ('map_bool_str', field_masks._STAR_SEG)
+    self.assertEqual(actual, expected)
+
+  def test_msg_str(self):
+    actual = self.parse('msg.str')
+    expected = ('msg', 'str')
+    self.assertEqual(actual, expected)
+
+  def test_msg_star(self):
+    actual = self.parse('msg.*')
+    expected = ('msg', field_masks._STAR_SEG)
+    self.assertEqual(actual, expected)
+
+  def test_msg_star_str(self):
+    with self.assertRaisesRegexp(ValueError, r'expected end of string'):
+      self.parse('msg.*.str')
+
+  def test_msg_unexpected_field(self):
+    with self.assertRaisesRegexp(ValueError, r'field "msg.x" does not exist'):
+      self.parse('msg.x')
+
+  def test_msg_unexpected_subfield(self):
+    with self.assertRaisesRegexp(ValueError, r'"msg.msg.x" does not exist'):
+      self.parse('msg.msg.x')
+
+  def test_msg_msgs_str(self):
+    actual = self.parse('msgs.*.str')
+    expected = ('msgs', field_masks._STAR_SEG, 'str')
+    self.assertEqual(actual, expected)
+
+  def test_msg_map_num_str(self):
+    actual = self.parse('msg.map_num_str.1')
+    expected = ('msg', 'map_num_str', 1)
+    self.assertEqual(actual, expected)
+
+  def test_map_str_map_num(self):
+    actual = self.parse('map_str_msg.num')
+    expected = ('map_str_msg', 'num')
+    self.assertEqual(actual, expected)
+
+  def test_map_str_map_num_star(self):
+    actual = self.parse('map_str_msg.*')
+    expected = ('map_str_msg', field_masks._STAR_SEG)
+    self.assertEqual(actual, expected)
+
+  def test_map_str_map_num_star_str(self):
+    actual = self.parse('map_str_msg.*.str')
+    expected = ('map_str_msg', field_masks._STAR_SEG, 'str')
+    self.assertEqual(actual, expected)
+
+  def test_trailing_period(self):
+    with self.assertRaisesRegexp(ValueError, r'unexpected end'):
+      self.parse('str.')
+
+  def test_trailing_period_period(self):
+    with self.assertRaisesRegexp(ValueError, r'cannot start with a period'):
+      self.parse('str..')
+
+
+class NormalizePathsTests(unittest.TestCase):
+
+  def test_empty(self):
+    actual = field_masks._normalize_paths([])
+    expected = set()
+    self.assertEqual(actual, expected)
+
+  def test_normal(self):
+    actual = field_masks._normalize_paths([
+        ('a',),
+        ('b',),
+    ])
+    expected = {('a',), ('b',)}
+    self.assertEqual(actual, expected)
+
+  def test_redundancy_one_level(self):
+    actual = field_masks._normalize_paths([
+        ('a',),
+        ('a', 'b'),
+    ])
+    expected = {('a',)}
+    self.assertEqual(actual, expected)
+
+  def test_redundancy_second_level(self):
+    actual = field_masks._normalize_paths([
+        ('a',),
+        ('a', 'b', 'c'),
+    ])
+    expected = {('a',)}
+    self.assertEqual(actual, expected)
+
+
+class ParseFieldTreeTests(unittest.TestCase):
+  def parse(self, paths):
+    return field_masks.parse_field_tree(
+        field_mask_pb2.FieldMask(paths=paths),
+        test_proto_pb2.Msg.DESCRIPTOR)
+
+  def test_empty(self):
+    actual = self.parse([])
+    expected = {}
+    self.assertEqual(actual, expected)
+
+  def test_str(self):
+    actual = self.parse(['str'])
+    expected = {'str': {}}
+    self.assertEqual(actual, expected)
+
+  def test_str_num(self):
+    actual = self.parse(['str', 'num'])
+    expected = {'str': {}, 'num': {}}
+    self.assertEqual(actual, expected)
+
+  def test_str_msg_num(self):
+    actual = self.parse(['str', 'msg.num'])
+    expected = {'str': {}, 'msg': {'num': {}}}
+    self.assertEqual(actual, expected)
+
+  def test_redunant(self):
+    actual = self.parse(['msg', 'msg.num'])
+    expected = {'msg': {}}
+    self.assertEqual(actual, expected)
+
+  def test_redunant_star(self):
+    actual = self.parse(['msg.*', 'msg.msg.num'])
+    expected = {'msg': {}}
+    self.assertEqual(actual, expected)
+
+
+class MustIncludeTests(unittest.TestCase):
+  def test_all(self):
+    tree = {}
+    path = ('a', )
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_ENTIRELY)
+
+  def test_include_recursively(self):
+    tree = {'a': {}}
+    path = ('a', )
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_ENTIRELY)
+
+  def test_include_recursively_second_level(self):
+    tree = {'a': {'b': {}}}
+    path = ('a', 'b')
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_ENTIRELY)
+
+  def test_include_recursively_star(self):
+    tree = {'a': {field_masks._STAR_SEG: {'b': {}}}}
+    path = ('a', 'x', 'b')
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_ENTIRELY)
+
+  def test_include_partially(self):
+    tree = {'a': {'b': {}}}
+    path = ('a', )
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_PARTIALLY)
+
+  def test_include_partially_second_level(self):
+    tree = {'a': {'b': {'c': {}}}}
+    path = ('a', 'b')
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_PARTIALLY)
+
+  def test_include_partially_star(self):
+    tree = {'a': {field_masks._STAR_SEG: {'b': {}}}}
+    path = ('a', 'x')
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._INCLUDE_PARTIALLY)
+
+  def test_exclude(self):
+    tree = {'a': {}}
+    path = ('b',)
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._EXCLUDE)
+
+  def test_exclude_second_level(self):
+    tree = {'a': {'b': {}}}
+    path = ('a', 'c')
+    self.assertEqual(
+        field_masks._must_include(tree, path),
+        field_masks._EXCLUDE)
+
+
+class TrimTests(unittest.TestCase):
+
+  def trim(self, msg, *mask_paths):
+    field_tree = field_masks.parse_field_tree(
+        field_mask_pb2.FieldMask(paths=mask_paths), msg.DESCRIPTOR)
+    field_masks.trim_message(msg, field_tree)
+
+  def test_scalar_trim(self):
+    msg = test_proto_pb2.Msg(num=1)
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_scalar_leave(self):
+    msg = test_proto_pb2.Msg(num=1)
+    self.trim(msg, 'num')
+    self.assertEqual(msg, test_proto_pb2.Msg(num=1))
+
+  def test_scalar_repeated_trim(self):
+    msg = test_proto_pb2.Msg(nums=[1, 2])
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_scalar_repeated_leave(self):
+    msg = test_proto_pb2.Msg(nums=[1, 2])
+    self.trim(msg, 'nums')
+    self.assertEqual(msg, test_proto_pb2.Msg(nums=[1, 2]))
+
+  def test_submessage_trim(self):
+    msg = test_proto_pb2.Msg(msg=test_proto_pb2.Msg(num=1))
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_submessage_leave_entirely(self):
+    msg = test_proto_pb2.Msg(msg=test_proto_pb2.Msg(num=1))
+    self.trim(msg, 'msg')
+    self.assertEqual(msg, test_proto_pb2.Msg(msg=test_proto_pb2.Msg(num=1)))
+
+  def test_submessage_leave_partially(self):
+    msg = test_proto_pb2.Msg(msg=test_proto_pb2.Msg(num=1, str='x'))
+    self.trim(msg, 'msg.num')
+    self.assertEqual(msg, test_proto_pb2.Msg(msg=test_proto_pb2.Msg(num=1)))
+
+  def test_submessage_repeated_trim(self):
+    msg = test_proto_pb2.Msg(
+        msgs=[test_proto_pb2.Msg(num=1), test_proto_pb2.Msg(num=2)])
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_submessage_repeated_leave_entirely(self):
+    msg = test_proto_pb2.Msg(
+        msgs=[test_proto_pb2.Msg(num=1), test_proto_pb2.Msg(num=2)])
+    expected = test_proto_pb2.Msg(
+        msgs=[test_proto_pb2.Msg(num=1), test_proto_pb2.Msg(num=2)])
+    self.trim(msg, 'msgs')
+    self.assertEqual(msg, expected)
+
+  def test_submessage_repeated_leave_entirely_trailing_star(self):
+    msg = test_proto_pb2.Msg(
+        msgs=[test_proto_pb2.Msg(num=1), test_proto_pb2.Msg(num=2)])
+    expected = test_proto_pb2.Msg(
+        msgs=[test_proto_pb2.Msg(num=1), test_proto_pb2.Msg(num=2)])
+    self.trim(msg, 'msgs.*')
+    self.assertEqual(msg, expected)
+
+  def test_submessage_repeated_leave_partially(self):
+    msg = test_proto_pb2.Msg(msgs=[
+        test_proto_pb2.Msg(num=1, str='x'),
+        test_proto_pb2.Msg(num=2, str='y'),
+    ])
+    expected = test_proto_pb2.Msg(msgs=[
+        test_proto_pb2.Msg(num=1),
+        test_proto_pb2.Msg(num=2),
+    ])
+    self.trim(msg, 'msgs.*.num')
+    self.assertEqual(msg, expected)
+
+  def test_map_str_num_trim(self):
+    msg = test_proto_pb2.Msg(map_str_num={'1': 1, '2': 2})
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_map_str_num_leave_key(self):
+    msg = test_proto_pb2.Msg(map_str_num={'a': 1, 'b': 2})
+    self.trim(msg, 'map_str_num.a')
+    self.assertEqual(msg, test_proto_pb2.Msg(map_str_num={'a': 1}))
+
+  def test_map_str_num_leave_key_with_int_key(self):
+    msg = test_proto_pb2.Msg(map_str_num={'1': 1, '2': 2})
+    self.trim(msg, 'map_str_num.`1`')
+    self.assertEqual(msg, test_proto_pb2.Msg(map_str_num={'1': 1}))
+
+  def test_map_str_num_leave_key_with_int_key_invalid(self):
+    msg = test_proto_pb2.Msg(map_str_num={'1': 1, '2': 2})
+    with self.assertRaisesRegexp(ValueError, 'invalid unquoted string "1"'):
+      self.trim(msg, 'map_str_num.1')
+
+  def test_map_str_msg_trim(self):
+    msg = test_proto_pb2.Msg(map_str_msg={'a': test_proto_pb2.Msg()})
+    self.trim(msg, 'str')
+    self.assertEqual(msg, test_proto_pb2.Msg())
+
+  def test_map_str_msg_leave_key_entirely(self):
+    msg = test_proto_pb2.Msg(
+        map_str_msg={
+            'a': test_proto_pb2.Msg(num=1),
+            'b': test_proto_pb2.Msg(num=2),
+        },
+        num=1)
+    self.trim(msg, 'map_str_msg.a')
+    self.assertEqual(
+        msg,
+        test_proto_pb2.Msg(map_str_msg={'a': test_proto_pb2.Msg(num=1)}))
+
+  def test_map_str_msg_leave_key_partially(self):
+    msg = test_proto_pb2.Msg(
+        map_str_msg={
+            'a': test_proto_pb2.Msg(num=1, str='a'),
+            'b': test_proto_pb2.Msg(num=2, str='b'),
+        },
+        num=1)
+    self.trim(msg, 'map_str_msg.a.num')
+    self.assertEqual(
+        msg,
+        test_proto_pb2.Msg(map_str_msg={'a': test_proto_pb2.Msg(num=1)}))
+
+
+if __name__ == '__main__':
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  unittest.main()
