25d0898683978543275eb773441f1a244fd0adcd
Kevin Lubick <kjlubick@google.com>
2018-05-30 11:03:37 -0400

    Experiment with pulito/WebComponents setup for Swarming
    Results from html-template-minifier
    (For bundle.js, 1 html template)
    Control code size ( stock pulito config, which just minifies the JS): 12.4k unzipped, 4.5k gzipped
    With uglify-es and simple html minification: 11.9k unzipped, 4.4k gzipped
    
    
    Bug:
    Change-Id: I61cffca994c41131d3defa356b6800abaf1f658e

---
+++ appengine/swarming/ui2/webpack.config.js
@@ -0,0 +1,18 @@
+const { commonBuilder } = require('pulito');
+const path = require('path');
+
+module.exports = commonBuilder(__dirname);
+
+module.exports.output.publicPath='/newres/'
+
+module.exports.module.rules.push({
+	test: /.js$/,
+	use: [
+		{
+			loader: path.resolve('html-template-minifier/loader.js'),
+			options: {
+				alpha: 'beta',
+			},
+		},
+	]
+})
