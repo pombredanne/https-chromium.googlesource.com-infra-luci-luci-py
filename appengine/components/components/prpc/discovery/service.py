c2bf567ef33677cf779e027540449077916c9004
Nodir Turakulov <nodir@google.com>
2018-05-30 17:04:02 -0700

    [prpc][discovery] Include source code info
    Current implementation of discovery service does not include source code info,
    thus comments are not available.
    
    Take cproto (dumb) approach: in each prpc_pb2.py file, include a
    FileDescriptorSet that includes the file and all of its transition
    dependenecies, with source code info.
    
    Bug: 846935
    Change-Id: Ibda31f0d0a8fae7fc6c3b9dbde914b8e32cd999a

--- appengine/components/components/prpc/discovery/service.py
+++ appengine/components/components/prpc/discovery/service.py
@@ -4,20 +4,8 @@
 
 """Discovery service implementation."""
 
-from google.protobuf import descriptor_pb2
-from google.protobuf import descriptor_pool
-
 import service_pb2
 import service_prpc_pb2
-
-
-class FileNotFound(Exception):
-  """Raised when a proto file is not found."""
-
-  def __init__(self, name):
-    super(FileNotFound, self).__init__(
-        'Proto file "%s" is not found in the default protobuf descriptor pool. '
-        'Ensure all _pb2.py are imported.' % name)
 
 
 class Discovery(object):
@@ -31,23 +19,12 @@ class Discovery(object):
     return self._response
 
   def add_service(self, service_description):
-    pool = descriptor_pool.Default()
-
-    def ensure_file(name):
-      if name in self._files:
-        return
-      desc = pool.FindFileByName(name)
-      if not desc:
-        raise FileNotFound(name)
-
-      proto = self._response.description.file.add()
-      desc.CopyToProto(proto)
-      for dep in proto.dependency:
-        ensure_file(dep)
-
     full_name = service_description['descriptor'].name
     if service_description['package']:
       full_name = '%s.%s' % (service_description['package'], full_name)
     self._response.services.append(full_name)
 
-    ensure_file(service_description['file'])
+    for f in service_description['file_descriptor_set'].file:
+      if f.name not in self._files:
+        self._response.description.file.add().CopyFrom(f)
+        self._files.add(f.name)
