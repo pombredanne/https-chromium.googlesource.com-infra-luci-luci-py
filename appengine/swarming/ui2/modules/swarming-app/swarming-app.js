d1e0d56c1a87b406239246915c36ba2030365662
Kevin Lubick <kjlubick@google.com>
2018-06-08 15:23:00 -0400

    Add more karma tests for Swarming UI
    swarming-app and oauth-login
    
    This was a bit more tricky than anticipated because of how Karma
    loads things into the browser.
    
    We no longer have a way to signal autorebuild, but perhaps someone
    could write a custom case using 'watch' or a similar third party tool
    that watches the directory and automatically runs make test.
    
    Also add the spinner to indicate things are loading, as was promised
    in a previous CL.
    
    Bug: 842181
    Change-Id: I256215f2a4333964a6a899577cb6f2e0d5731737

--- appengine/swarming/ui2/modules/swarming-app/swarming-app.js
+++ appengine/swarming/ui2/modules/swarming-app/swarming-app.js
@@ -10,8 +10,19 @@
  *   side panel. This element is largely CSS, with a smattering of
  *   JS to toggle the side panel on/off when in small screen mode.
  *   A notable addition to the top panel is an &lt;oauth-login&gt; element
- *   to handle login.
+ *   to handle login. See the demo page for an example usage.
  * </p>
+ *
+ * <p>
+ *   The swarming-app can be a central place for indicating to the user
+ *   that the app is busy (e.g. AJAX requests). Simply use the addBusyTasks()
+ *   and finishedTask() to indicate when work is starting and stopping.
+ *   The 'busy-end' event will signal any time a finishedTask() drops the
+ *   count of ongoing tasks to zero (or lower). See also the busy property.
+ * </p>
+ *
+ * @evt busy-end This event is emitted when ever the app transitions from
+ *               busy to not busy.
  *
  * @attr client_id - The Client ID for authenticating via OAuth.
  * @attr testing_offline - If true, the real OAuth flow won't be used.
@@ -30,6 +41,13 @@ button_template.innerHTML =`
 </button>
 `;
 
+const spinner_template = document.createElement('template');
+spinner_template.innerHTML =`
+<div class=spinner-spacer>
+  <spinner-sk></spinner-sk>
+</div>
+`;
+
 const login_template = (ele) => html`
 <oauth-login client_id=${ele.client_id} testing_offline=${ele.testing_offline}></oauth-login>
 `;
@@ -38,18 +56,22 @@ window.customElements.define('swarming-app', class extends HTMLElement {
   connectedCallback() {
     upgradeProperty(this, 'client_id');
     upgradeProperty(this, 'testing_offline');
+    this._busyTaskCount = 0;
+    this._spinner = null;
+    this._loginEle = null;
     this._addHTML();
     this._render();
-    this._busyTaskCount = 0;
   }
 
-  static get observedAttributes() {
-    return ['client_id', 'testing_offline'];
-  }
+  /** @prop {boolean} busy Indicates if there any on-going tasks. This also mirrors
+  *                   the status of the embedded spinner-sk. Read-only. */
+  get busy() { return !!this._busyTaskCount;}
 
+  /** @prop {string} client_id Mirrors the attribute 'client_id'. */
   get client_id() { return this.getAttribute('client_id');}
   set client_id(val) { return this.setAttribute('client_id', val);}
 
+  /** @prop {bool} testing_offline Mirrors the attribute 'testing_offline'. */
   get testing_offline() { return this.getAttribute('testing_offline');}
   set testing_offline(val) {
     // If testing_offline gets passed through multiple layers, it ends up the
@@ -58,6 +80,35 @@ window.customElements.define('swarming-app', class extends HTMLElement {
       this.setAttribute('testing_offline', true);
     } else {
       this.removeAttribute('testing_offline');
+    }
+  }
+
+  /**
+   * Indicate there are some number of events the app is waiting on
+   * and should be in the "busy" state, if it isn't already.
+   *
+   * @param {Number} count - Number of tasks to wait for. Should be positive.
+   */
+  addBusyTasks(count) {
+    this._busyTaskCount += count;
+    if (this._spinner) {
+      this._spinner.active = true;
+    }
+  }
+
+  /**
+   * Removes one task from the busy count. If there are no more tasks to wait
+   * for, the app will leave the "busy" state and emit the "busy-end" event.
+   *
+   */
+  finishedTask() {
+    this._busyTaskCount--;
+    if (this._busyTaskCount <= 0) {
+      this._busyTaskCount = 0;
+      if (this._spinner) {
+        this._spinner.active = false;
+      }
+      this.dispatchEvent(new CustomEvent('busy-end', {bubbles: true}));
     }
   }
 
@@ -74,6 +125,12 @@ window.customElements.define('swarming-app', class extends HTMLElement {
     header.insertBefore(btn, header.firstElementChild);
     btn = header.firstElementChild;
     btn.addEventListener('click', (e) => this._toggleMenu(e, sidebar));
+
+    let spinner = spinner_template.content.cloneNode(true);
+    header.insertBefore(spinner, sidebar);
+    // The real spinner is a child of the template, so we need to grab it
+    // from the header after the template has been expanded.
+    this._spinner = header.querySelector('spinner-sk');
 
     let spacer = document.createElement('span');
     spacer.classList.add('grow');
@@ -93,21 +150,11 @@ window.customElements.define('swarming-app', class extends HTMLElement {
     }
   }
 
+  static get observedAttributes() {
+    return ['client_id', 'testing_offline', 'busy'];
+  }
   attributeChangedCallback(attrName, oldVal, newVal) {
     this._render();
-  }
-
-  addBusyTasks(count) {
-    this._busyTaskCount += count;
-    // TODO(kjlubick): activate the spinner
-  }
-
-  finishedTask() {
-    this._busyTaskCount--;
-    if (this._busyTaskCount <= 0) {
-      this._busyTaskCount = 0;
-      this.dispatchEvent(new CustomEvent('busy-end', {bubbles: true}));
-    }
   }
 
 });
