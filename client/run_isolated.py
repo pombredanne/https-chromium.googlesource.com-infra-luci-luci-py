097a1dd0d5ac99679fd082e23318fe08bf167b0f
Marc-Antoine Ruel <maruel@chromium.org>
2018-06-14 20:14:03 -0400

    [client] Fix cache LRU coherency
    This CL moves a lot of code around. It is done so that the methods are grouped
    by the 'interface' they represent. The word interface is meant to be interpreted
    in its weakest meaning here.
    
    Bug: 717716
    Change-Id: I5ea53b49c97080cd68070e144b54967e28730c2d

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -1019,37 +1019,6 @@ def install_client_and_packages(
       })
 
 
-def clean_caches(isolate_cache, named_cache):
-  """Trims isolated and named caches.
-
-  The goal here is to coherently trim both caches, deleting older items
-  independent of which container they belong to.
-  """
-  # TODO(maruel): Trim CIPD cache the same way.
-  total = 0
-  oldest_isolated = isolate_cache.get_oldest()
-  oldest_named = named_cache.get_oldest()
-  trimmers = [
-    (
-      isolate_cache.trim,
-      isolate_cache.get_timestamp(oldest_isolated) if oldest_isolated else 0,
-    ),
-    (
-      named_cache.trim,
-      named_cache.get_timestamp(oldest_named) if oldest_named else 0,
-    ),
-  ]
-  trimmers.sort(key=lambda (_, ts): ts)
-  # TODO(maruel): This is incorrect, we want to trim 'items' that are strictly
-  # the oldest independent of in which cache they live in. Right now, the
-  # cache with the oldest item pays the price.
-  for trim, _ in trimmers:
-    total += trim()
-  named_cache.trim()
-  isolate_cache.cleanup()
-  return total
-
-
 def create_option_parser():
   parser = logging_utils.OptionParserWithLogging(
       usage='%prog <options> [command to run or extra args]',
@@ -1237,11 +1206,21 @@ def main(args):
       parser.error('Can\'t use --json with --clean.')
     if options.named_caches:
       parser.error('Can\t use --named-cache with --clean.')
-    clean_caches(isolate_cache, named_cache)
+    # Trim first, then clean.
+    # TODO(maruel): CIPD caches.
+    local_caching.trim_caches(
+        [isolate_cache, named_cache],
+        min_free_space=options.min_free_space,
+        max_age_secs=MAX_AGE_SECS)
+    isolate_cache.clean()
+    name_cache.clean()
     return 0
 
   if not options.no_clean:
-    clean_caches(isolate_cache, named_cache)
+    local_caching.trim_caches(
+        [isolate_cache, named_cache],
+        min_free_space=options.min_free_space,
+        max_age_secs=MAX_AGE_SECS)
 
   if not options.isolated and not args:
     parser.error('--isolated or command to run is required.')
