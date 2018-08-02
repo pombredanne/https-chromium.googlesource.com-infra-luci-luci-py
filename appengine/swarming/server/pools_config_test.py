ce2d470d4cd4156ccc6a47f7cf3e26a9e0055732
Marc-Antoine Ruel <maruel@chromium.org>
2018-08-02 16:45:18 -0400

    [swarming] add validation for bot_monitoring
    No implementation yet, but this enables users to add filling their pools.cfg.
    
    Bug: 864724
    Change-Id: I533d83a7b8e7465462c153abeeecc2d4113bc71d

--- appengine/swarming/server/pools_config_test.py
+++ appengine/swarming/server/pools_config_test.py
@@ -24,32 +24,38 @@ from server import task_request
 from google.protobuf import text_format
 
 
-TEST_CONFIG = pools_pb2.PoolsCfg(pool=[
-  pools_pb2.Pool(
-    name=['pool_name', 'another_name'],
-    schedulers=pools_pb2.Schedulers(
-      user=['user:a@example.com', 'b@example.com'],
-      group=['group1', 'group2'],
-      trusted_delegation=[
-        pools_pb2.TrustedDelegation(
-          peer_id='delegatee@example.com',
-          require_any_of=pools_pb2.TrustedDelegation.TagList(
-            tag=['k:tag1', 'k:tag2'],
-          ),
+TEST_CONFIG = pools_pb2.PoolsCfg(
+    pool=[
+      pools_pb2.Pool(
+        name=['pool_name', 'another_name'],
+        schedulers=pools_pb2.Schedulers(
+          user=['user:a@example.com', 'b@example.com'],
+          group=['group1', 'group2'],
+          trusted_delegation=[
+            pools_pb2.TrustedDelegation(
+              peer_id='delegatee@example.com',
+              require_any_of=pools_pb2.TrustedDelegation.TagList(
+                tag=['k:tag1', 'k:tag2'],
+              ),
+            ),
+          ],
         ),
-      ],
-    ),
-    allowed_service_account=[
-      'a1@example.com',
-      'a2@example.com',
+        allowed_service_account=[
+          'a1@example.com',
+          'a2@example.com',
+        ],
+        allowed_service_account_group=[
+          'accounts_group1',
+          'accounts_group2',
+        ],
+        bot_monitoring='bots',
+      ),
     ],
-    allowed_service_account_group=[
-      'accounts_group1',
-      'accounts_group2',
+    forbid_unknown_pools=True,
+    bot_monitoring=[
+      pools_pb2.BotMonitoring(name='bots', dimension_keys=['os', 'bool']),
     ],
-  ),
-], forbid_unknown_pools=True)
-
+)
 
 class PoolsConfigTest(test_case.TestCase):
   def validator_test(self, cfg, messages):
@@ -91,7 +97,8 @@ class PoolsConfigTest(test_case.TestCase):
         },
         service_accounts=frozenset([u'a2@example.com', u'a1@example.com']),
         service_accounts_groups=(u'accounts_group1', u'accounts_group2'),
-        task_template_deployment=None)
+        task_template_deployment=None,
+        bot_monitoring=None)
     expected2 = expected1._replace(name='another_name')
 
     self.assertEqual(expected1, pools_config.get_pool_config('pool_name'))
