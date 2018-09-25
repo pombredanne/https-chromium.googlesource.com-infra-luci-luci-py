5fb4c50400dce5c95261d31c4597df19a5dbd676
Kevin Lubick <kjlubick@google.com>
2018-09-25 16:05:33 -0400

    [WIP] bot-list super CL
    This CL will likely be broken up before submission.
    
    Change-Id: Id16aa060971c91673376683591ff2d719526e98c

--- appengine/swarming/ui2/modules/bot-list/index.js
+++ appengine/swarming/ui2/modules/bot-list/index.js
@@ -2,4 +2,10 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-import '../modules/bot-list'
+import 'common-sk/modules/error-toast-sk'
+
+import '../sort-toggle'
+import '../swarming-app'
+
+import './bot-list.js'
+import './bot-list.scss'
---
+++ appengine/swarming/ui2/modules/sort-toggle/sort-toggle.js
@@ -0,0 +1,75 @@
+// Copyright 2018 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import { html, render } from 'lit-html'
+import { upgradeProperty } from 'elements-sk/upgradeProperty'
+
+import 'elements-sk/icon/arrow-drop-down-icon-sk'
+import 'elements-sk/icon/arrow-drop-up-icon-sk'
+
+// Looks to properties set, then attributes to initialize ele[prop]
+// using the proper setters.  Optionally removes the attr to avoid stale data.
+function initPropertyFromAttrOrProperty(ele, prop, removeAttr=true) {
+  upgradeProperty(ele, prop);
+  if (ele[prop] === undefined && ele.hasAttribute(prop)) {
+    ele[prop] = ele.getAttribute(prop);
+    if (removeAttr) {
+      ele.removeAttribute(prop);
+    }
+  }
+}
+
+const template = (ele) => html`
+<arrow-drop-down-icon-sk ?hidden=${ele.key === ele.current && ele.direction === 'asc'}></arrow-drop-down-icon-sk>
+<arrow-drop-up-icon-sk ?hidden=${ele.key === ele.current && ele.direction === 'desc'}></arrow-drop-up-icon-sk>
+`
+
+/** TODO(kjlubick)  Don't forget event*/
+window.customElements.define('sort-toggle', class extends HTMLElement {
+
+  constructor() {
+    super()
+    // _current, _name, _direction are private members
+  }
+
+  connectedCallback() {
+    initPropertyFromAttrOrProperty(this, 'current');
+    initPropertyFromAttrOrProperty(this, 'key');
+    initPropertyFromAttrOrProperty(this, 'direction');
+
+    this.addEventListener('click', () => {
+      this.toggle();
+    });
+  }
+
+  /** TODO(kjlubick) */
+  get current() { return this._current; }
+  set current(val) { this._current = val; this.render();}
+
+  get key() { return this._key; }
+  set key(val) { this._key = val; this.render();}
+
+  get direction() { return this._direction; }
+  set direction(val) { this._direction = val; this.render();}
+
+  toggle() {
+    if (this.direction === 'asc') {
+      this.direction = 'desc';
+    } else {
+      this.direction = 'asc';
+    }
+    this.dispatchEvent(new CustomEvent('sort-change', {
+      detail: {
+        'direction': this.direction,
+        'key': this.key,
+      },
+      bubbles: true,
+    }));
+  }
+
+  render() {
+    render(template(this), this);
+  }
+
+});
