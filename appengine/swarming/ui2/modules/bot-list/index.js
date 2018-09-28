b0bf96a8007f10a125a12e9e221cbe2c7c94dc49
Kevin Lubick <kjlubick@google.com>
2018-09-28 10:13:52 -0400

    Adds the first pieces of the swarming bot-list using WebComponents.
    It sorts and dynamically can add/remove filters and columns.
    It makes all sorts of ajax requests using fetch.
    It makes use of query params.
    
    It has > 30 tests just for the bot-list.
    
    There are many more TODOs to get all the current functionality, but
    those will arrive in a later CL.
    
    Bug: 842181
    Change-Id: I1e0156a0de1ad53a2306b3d4399e8a980a089b14

---
+++ appengine/swarming/ui2/modules/bot-list/index.js
@@ -0,0 +1,11 @@
+// Copyright 2018 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'common-sk/modules/error-toast-sk'
+
+import '../sort-toggle'
+import '../swarming-app'
+
+import './bot-list.js'
+import './bot-list.scss'
