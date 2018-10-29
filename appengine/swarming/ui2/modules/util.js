844fd4aebf056a604b461322402ff1098f4b3df8
Kevin Lubick <kjlubick@google.com>
2018-10-29 09:40:35 -0400

    Add aliasing to new botlist
    This is a somewhat simpler implentation than the Polymer one.
    
    Bug: 842181
    Change-Id: I2bd382e5594bfc0469167d17d22e4b727bd687b0

--- appengine/swarming/ui2/modules/util.js
+++ appengine/swarming/ui2/modules/util.js
@@ -21,36 +21,9 @@ import * as query from 'common-sk/modules/query'
  *                   whether a is greater than, less than or equal to b.
  */
 export function stableSort(arr, comp) {
-  if (!arr || !comp) {
-    console.warn('missing arguments to stableSort', arr, comp);
-    return;
-  }
-  // We can guarantee a potential non-stable sort (like V8's
-  // Array.prototype.sort()) to be stable by first storing the index in the
-  // original sorting and using that if the original compare was 0.
-  arr.forEach((e, i) => {
-    if (e !== undefined && e !== null) {
-      e.__sortIdx = i;
-    }
-  });
-
-  arr.sort((a, b) => {
-    // undefined and null elements always go last.
-    if (a === undefined || a === null) {
-      if (b === undefined || b === null) {
-        return 0;
-      }
-      return 1;
-    }
-    if (b === undefined || b === null) {
-      return -1;
-    }
-    let c = comp(a, b);
-    if (c === 0) {
-      return a.__sortIdx - b.__sortIdx;
-    }
-    return c;
-  });
+  // All major supported browsers are now stable (or stable-ish)
+  // https://stackoverflow.com/a/3027715
+  arr.sort(comp);
 }
 
 /** sanitizeAndHumanizeTime parses a date string or ms_since_epoch into a JS
