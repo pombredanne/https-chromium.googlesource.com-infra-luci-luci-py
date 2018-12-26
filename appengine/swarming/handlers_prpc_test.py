d849f213f0db736e36e8b401bd3e71c029418709
Gary Miguel <garymm@google.com>
2018-12-26 21:53:18 +0000

    Reland "Return file path in config validation response."
    This is a reland of 20c0045691357d51e2047b8f5508fe804fc14776
    
    Original change's description:
    > Return file path in config validation response.
    >
    > This lets the user associate the error message with the config file.
    >
    > Change-Id: I85077611ffe97cf2033008ff5ca865d6ec46a07f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388106
    > Commit-Queue: Gary Miguel <garymm@google.com>
    > Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    > Auto-Submit: Gary Miguel <garymm@google.com>
    
    Change-Id: I5031f949f9c815ad5f7dbde78f3104035a818283

--- appengine/swarming/handlers_prpc_test.py
+++ appengine/swarming/handlers_prpc_test.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
+# Just a dummy line to trigger CQ. Revert this line before submit!
+
 import datetime
 import logging
 import os
