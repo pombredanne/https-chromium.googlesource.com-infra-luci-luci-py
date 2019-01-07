4ba2088d6bef3f20716c31466ec43a3452690703
Kevin Lubick <kjlubick@google.com>
2019-01-07 17:27:16 -0500

    [swarming-ui] Copy the skeleton from bot-list to task-list
    Refactor bot-list a little to share code with task-list where easy.
    Copy-paste where it isn't (more refactoring is probably still to come).
    
    Bug: 842181
    Change-Id: I033fc125af293292d6591eb7d37c6275968926a5

--- appengine/swarming/ui2/modules/task-list/task-list-demo.js
+++ appengine/swarming/ui2/modules/task-list/task-list-demo.js
@@ -5,6 +5,7 @@
 import './index.js'
 
 import { tasks_20 } from './test_data'
+import { fleetDimensions } from '../bot-list/test_data'
 import { requireLogin, mockAuthdAppGETs } from '../test_util'
 
 
@@ -17,6 +18,9 @@ mockAuthdAppGETs(fetchMock, {});
 
 fetchMock.get('glob:/_ah/api/swarming/v1/tasks/list?*',
               requireLogin(tasks_20));
+
+fetchMock.get('/_ah/api/swarming/v1/bots/dimensions',
+              requireLogin(fleetDimensions));
 
 // Everything else
 fetchMock.catch(404);
