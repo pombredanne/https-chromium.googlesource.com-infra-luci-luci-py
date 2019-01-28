1f506c305a9f84283f334b5f0f0133b34ef1117e
Marc-Antoine Ruel <maruel@chromium.org>
2019-01-28 16:01:17 -0500

    [client] Implement fs.readlink(); Fix fs.symlink()
    - fs.symlink() did not work with relative paths on Windows. This
      limitation was overlooked, causing failure at runtime on Windows.
    - Add readlink() for completeness.
    - With both supported, add unit tests to ensure they work as expected.
    - Improve ctypes typing.
    
    Ran manually fs_test.py on macOS, linux and Windows7 and 10.
    
    Bug: 853721
    Change-Id: I5acc5b3a02864fff82fd29ea9cd47783f7123d22

---
+++ client/tests/fs_test.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import logging
+import os
+import tempfile
+import unittest
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(
+    __file__.decode(sys.getfilesystemencoding())))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.insert(0, ROOT_DIR)
+sys.path.insert(0, os.path.join(ROOT_DIR, 'third_party'))
+
+FILE_PATH = os.path.abspath(__file__.decode(sys.getfilesystemencoding()))
+
+from depot_tools import fix_encoding
+from utils import file_path
+from utils import fs
+
+
+def write_content(filepath, content):
+  with fs.open(filepath, 'wb') as f:
+    f.write(content)
+
+
+class FSTest(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    if not file_path.enable_symlink():
+      raise Exception('Failed to enable symlink support')
+
+  def setUp(self):
+    super(FSTest, self).setUp()
+    self._tempdir = None
+
+  def tearDown(self):
+    try:
+      if self._tempdir:
+        file_path.rmtree(self._tempdir)
+    finally:
+      super(FSTest, self).tearDown()
+
+  @property
+  def tempdir(self):
+    if not self._tempdir:
+      self._tempdir = tempfile.mkdtemp(prefix=u'fs_test')
+    return self._tempdir
+
+  def test_symlink_relative(self):
+    # A symlink to a relative path is valid.
+    # /dir
+    # /dir/file
+    # /ld -> /dir
+    # /lf -> /ld/file
+    dirpath = os.path.join(self.tempdir, 'dir')
+    filepath = os.path.join(dirpath, 'file')
+    os.mkdir(dirpath)
+    write_content(filepath, 'hello')
+
+    linkfile = os.path.join(self.tempdir, 'lf')
+    linkdir = os.path.join(self.tempdir, 'ld')
+    dstfile = os.path.join('ld', 'file')
+    fs.symlink(dstfile, linkfile)
+    fs.symlink('dir', linkdir)
+    self.assertEqual(dstfile, fs.readlink(linkfile))
+    self.assertEqual('dir', fs.readlink(linkdir))
+    self.assertEqual(['file'], fs.listdir(linkdir))
+    # /lf resolves to /dir/file.
+    self.assertEqual('hello', fs.open(linkfile).read())
+
+  def test_symlink_absolute(self):
+    # A symlink to an absolute path is valid.
+    # /dir
+    # /dir/file
+    # /ld -> /dir
+    # /lf -> /ld/file
+    dirpath = os.path.join(self.tempdir, 'dir')
+    filepath = os.path.join(dirpath, 'file')
+    os.mkdir(dirpath)
+    write_content(filepath, 'hello')
+
+    linkfile = os.path.join(self.tempdir, 'lf')
+    linkdir = os.path.join(self.tempdir, 'ld')
+    dstfile = os.path.join(linkdir, 'file')
+    fs.symlink(dstfile, linkfile)
+    fs.symlink(dirpath, linkdir)
+    self.assertEqual(dstfile, fs.readlink(linkfile))
+    self.assertEqual(dirpath, fs.readlink(linkdir))
+    self.assertEqual(['file'], fs.listdir(linkdir))
+    # /lf resolves to /dir/file.
+    self.assertEqual('hello', fs.open(linkfile).read())
+
+  def test_symlink_missing_destination(self):
+    # A symlink to a missing destination is valid.
+    filepath = os.path.join(self.tempdir, 'file')
+    linkfile = os.path.join(self.tempdir, 'lf')
+    fs.symlink(filepath, linkfile)
+    self.assertEqual(filepath, fs.readlink(linkfile))
+
+  def test_symlink_existing(self):
+    # Creating a symlink that overrides a file fails.
+    filepath = os.path.join(self.tempdir, 'file')
+    linkfile = os.path.join(self.tempdir, 'lf')
+    write_content(linkfile, 'hello')
+    with self.assertRaises(OSError):
+      fs.symlink(filepath, linkfile)
+
+  def test_readlink_fail(self):
+    with self.assertRaises(OSError):
+      fs.readlink(os.path.join(self.tempdir, 'not_there'))
+
+
+if __name__ == '__main__':
+  fix_encoding.fix_encoding()
+  logging.basicConfig(
+      level=logging.DEBUG if '-v' in sys.argv else logging.ERROR)
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  unittest.main()
