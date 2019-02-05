2f24f0b68b49104dc0a99aef4899b133e720c162
Kevin Lubick <kjlubick@google.com>
2019-02-05 14:46:50 -0500

    [swarming-ui] Make input connected in task-page
    Change-Id: I3cc027fff94d6ed488a990f3465952639e3e96b0

--- appengine/swarming/ui2/modules/task-list/task-list.js
+++ appengine/swarming/ui2/modules/task-list/task-list.js
@@ -89,11 +89,11 @@ const col_selector = (ele) => {
 <div class=col_selector @click=${e => e.stopPropagation()}>
   <input id=column_search class=search type=text
          placeholder='Search columns to show'
-         @input=${e => ele._refilterPossibleColumns(e)}
+         @input=${ele._refilterPossibleColumns}
          <!-- Looking at the change event, but that had the behavior of firing
               any time the user clicked away, with seemingly no differentiation.
               Instead, we watch keyup and wait for the 'Enter' key. -->
-         @keyup=${e => ele._columnSearch(e)}>
+         @keyup=${ele._columnSearch}>
   </input>
   ${ele._filteredPossibleColumns.map((key) => columnOption(key, ele))}
 </div>`;
@@ -157,7 +157,7 @@ const filterChip = (filter, ele) => html`
 const filters = (ele) => html`
 <!-- primary key selector-->
 <select-sk class="selector keys"
-           @selection-changed=${(e) => ele._primaryKeyChanged(e)}>
+           @selection-changed=${ele._primaryKeyChanged}>
   ${ele._filteredPrimaryArr.map((key) => primaryOption(key, ele))}
 </select-sk>
 <!-- secondary value selector-->
