c7eec42ab15a5812bafb0160f3682db850e8f339
Aviv Keshet <akeshet@chromium.org>
2019-02-05 17:45:00 -0800

    swarming: implement task queue for es-notify-tasks
    BUG=928966
    
    Change-Id: I098dc0fbf25e3802b1efd33aec209a9d37e71857

--- appengine/swarming/server/external_scheduler.py
+++ appengine/swarming/server/external_scheduler.py
@@ -7,6 +7,7 @@
 import logging
 
 from components import utils
+from components import datastore_utils
 
 from components.prpc import client
 
@@ -150,7 +151,6 @@ def assign_task(es_cfg, bot_dimensions):
   return resp.assignments[0].task_id, resp.assignments[0].slice_number
 
 
-# TODO(akeshet): Add a blocking and non-blocking variant of this.
 def notify_request(es_cfg, request, result_summary, use_tq=False):
   """Calls external scheduler to notify it of a task state.
 
@@ -159,8 +159,8 @@ def notify_request(es_cfg, request, result_summary, use_tq=False):
         notify.
     - request: task_request.TaskRequest
     - result_summary: task_result.TaskResultSummary
-    - use_tq: If true, make this call asynchronously on a task queue (not
-              yet implemented).
+    - use_tq: If true, make this call on a task queue (within the current
+              datastore transaction).
 
   Returns: Nothing.
   """
@@ -188,9 +188,15 @@ def notify_request(es_cfg, request, result_summary, use_tq=False):
   req.scheduler_id = es_cfg.id
 
   if use_tq:
-    # Push a payload containing |req| proto and es_host to
-    # a task-queue.
-    pass
+    request_bytes = req.SerializeToString()
+    enqueued = utils.enqueue_task(
+      url='/internal/taskqueue/es-notify-tasks',
+      queue_name='es-notify-tasks',
+      params={'es_host': es_cfg.address},
+      transactional=True,
+      payload=request_bytes)
+    if not enqueued:
+      raise datastore_utils.CommitError('Failed to enqueue task')
   else:
     # Ignore return value, the response proto is empty.
     notify_request_now(es_cfg.address, req)
