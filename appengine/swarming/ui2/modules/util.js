1fc18188dafc1883b3a529fc9f749819a4d82f2a
Kevin Lubick <kjlubick@google.com>
2019-02-06 10:11:18 -0500

    [swarming-ui] task-list improvements
    add additional affordance for exposure and small mobile-friendly improvements
    
    don't present -tag in filters or columns.  Intentionally leaving it
    in for filters to keep them compact and more readable (to me, at least).
    
    catch edgecase in counts
    
    Tweak color of links to have more contrast.
    
    Bug: 842181
    Change-Id: I55c163559ea1c0360e6b2891195306cdd733781f

--- appengine/swarming/ui2/modules/util.js
+++ appengine/swarming/ui2/modules/util.js
@@ -130,6 +130,10 @@ export function initPropertyFromAttrOrProperty(ele, prop, removeAttr=true) {
   }
 }
 
+export function onSmallScreen() {
+  return window.innerWidth < 600 || window.innerHeight < 600;
+}
+
 /** sanitizeAndHumanizeTime parses a date string or ms_since_epoch into a JS
  *  Date object, assuming UTC time. It also creates a human readable form in
  *  the obj under a key with a human_ prefix.  E.g.
