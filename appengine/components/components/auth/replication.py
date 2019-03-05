9d08ae91f504fdbf8eac7cf7b40d5f401d6deba8
Vadim Shtayura <vadimsh@chromium.org>
2019-03-04 21:06:41 -0800

    [auth] Ingest security.cfg config file and put into AuthDB.
    This file will contain security-related configuration that is distributed as
    part of AuthDB to all linked services. Moving it to a separate proto to avoid
    redoing all this work once again if we add another field.
    
    The initial field is 'internal_service_regexp' with a list of regexps for
    hostnames that should be recognized as being part of LUCI deployment.
    
    The main complication is to make sure oauth.cfg and security.cfg updates do not
    clobber each other since they both result in modification of AuthGlobalConfig
    entity. This is done by slightly refactoring how 'refetch_config' applies AuthDB
    updates.
    
    The changes to AuthGlobalConfig.security_config do not propagate anywhere yet.
    
    R=nodir@chromium.org, fmatenaar@chromium.org
    BUG=938083
    
    Change-Id: I90499f98a130ef3baa0ac2ff0575fe3cab568d36

--- appengine/components/components/auth/replication.py
+++ appengine/components/components/auth/replication.py
@@ -160,6 +160,7 @@ def auth_db_snapshot_to_proto(snapshot, auth_db_proto=None):
         snapshot.global_config.oauth_additional_client_ids)
 
   auth_db_proto.token_server_url = snapshot.global_config.token_server_url or ''
+  auth_db_proto.security_config = snapshot.global_config.security_config or ''
 
   for ent in snapshot.groups:
     msg = auth_db_proto.groups.add()
@@ -205,7 +206,8 @@ def proto_to_auth_db_snapshot(auth_db_proto):
       oauth_client_secret=auth_db_proto.oauth_client_secret,
       oauth_additional_client_ids=list(
           auth_db_proto.oauth_additional_client_ids),
-      token_server_url=auth_db_proto.token_server_url)
+      token_server_url=auth_db_proto.token_server_url,
+      security_config=auth_db_proto.security_config)
 
   groups = [
     model.AuthGroup(
