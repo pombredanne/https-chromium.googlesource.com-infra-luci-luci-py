luci/luci-py

commit 46082109eb204bae3ecf1fe403bcd7993dae8b7e

[swarming] convert artificial 503 to 429
This will reduce unactionable alert.

R=qyearsley@chromium.org

Bug: 900042
Change-Id: Iddc0add3bd8a4998d2dea4f48897764f6243d1a9
Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1521061
Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>

appengine/swarming/handlers_backend.py
@@ -298,10 +298,11 @@ class TaskDimensionsHandler(webapp2.RequestHandler):
   @decorators.require_taskqueue('rebuild-task-cache')
   def post(self):
     if not task_queues.rebuild_task_cache(self.request.body):
-      # The task needs to be retried. Reply that the service is unavailable
-      # (503) instead of an internal server error (500) to help differentiating
-      # in the logs, even if it is not technically correct.
-      self.response.set_status(503)
+      # The task likely failed due to DB transaction contention,
+      # so we can reply that the service has had too many requests (429).
+      # Using a 400-level response also prevents failures here from causing
+      # unactionable alerts due to a high rate of 500s.
+      self.response.set_status(429)
 
 
 class TaskSendPubSubMessage(webapp2.RequestHandler):

pombredanne/https-chromium.googlesource.com-infra-luci-luci-py

commit 5bf785077d59a0b0d2851c91e735dd6ee40fb2b9

[swarming] fix 429 to not throw
- Fix bug introduced in 46082109eb204bae3ecf1fe403bcd7993dae8b7e
- Add unit test to confirm.

Ironically the 429 threw a 500 because a message is required by webapp2.

R=qyearsley@chromium.org

Bug: 900042
Change-Id: Ic37ad61a158bdb0f7bcf817a2f0c2006c025222f

appengine/swarming/handlers_backend.py
@@ -302,7 +302,7 @@ class TaskDimensionsHandler(webapp2.RequestHandler):
       # so we can reply that the service has had too many requests (429).
       # Using a 400-level response also prevents failures here from causing
       # unactionable alerts due to a high rate of 500s.
-      self.response.set_status(429)
+      self.response.set_status(429, 'Need to retry')
 
 
 class TaskSendPubSubMessage(webapp2.RequestHandler):

appengine/swarming/handlers_backend_test.py
@@ -18,6 +18,7 @@ import webtest
 import handlers_backend
 from components import utils
 from server import bot_management
+from server import task_queues
 from server import task_request
 from server import task_result
 
@@ -195,6 +196,15 @@ class BackendTest(test_env_handlers.AppTestBase):
             status=403)
       except Exception as e:
         self.fail('%s: %s' % (url, e))
+
+  def test_taskqueue_important_task_queues_rebuild_cache_fail(self):
+    self.set_as_admin()
+    def rebuild_task_cache(_body):
+      return False
+    self.mock(task_queues, 'rebuild_task_cache', rebuild_task_cache)
+    self.app.post(
+        '/internal/taskqueue/important/task_queues/rebuild-cache',
+        headers={'X-AppEngine-QueueName': 'rebuild-task-cache'}, status=429)
 
   def test_taskqueue_monitoring_bq_bots_events(self):
     self.set_as_admin()