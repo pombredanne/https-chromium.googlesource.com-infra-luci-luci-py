c638ff987b7b553129afdb729ff2daf5c5201da1
Marc-Antoine Ruel <maruel@chromium.org>
2019-04-18 09:46:30 -0400

    [client] Change idna stub to use python's default
    It's simpler than https://pypi.org/project/idna/ but better than ignoring the
    "xn-" encoding.
    
    This is required by openssl/x509.py
    
    Bug: 916644
    Change-Id: Ia999a56b981d943e2f3d942f83e40d40e1bb805b

--- client/third_party/idna/__init__.py
+++ client/third_party/idna/__init__.py
@@ -1,4 +1,13 @@
 # Emulate the bare minimum for idna for the Swarming bot.
 # In practice, we do not need it, and it's very large.
-def encode(host, uts46):
-  return unicode(host)
+# See https://pypi.org/project/idna/
+
+from encodings import idna
+
+def encode(host, uts46=False):
+  # Used by urllib3
+  return idna.ToAscii(host)
+
+def decode(host):
+  # Used by cryptography/hazmat/backends/openssl/x509.py
+  return idna.ToUnicode(host)
