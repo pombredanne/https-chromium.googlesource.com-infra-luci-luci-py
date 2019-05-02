10d0afc67e4dd9c12f08f14723ae829bf8d3b705
Felix Matenaar <fmatenaar@chromium.org>
2019-05-02 16:46:02 -0700

    [net] request_async support for project scoped service account authZ.
    request_async using auth interface for obtaining project scoped service
    accounts.
    
    Bug: 911811
    Change-Id: I809b33cc0fb4b0c1795327ae2b5a832414b5b6ed

--- appengine/components/components/auth/check.py
+++ appengine/components/components/auth/check.py
@@ -10,7 +10,6 @@ api.py and delegation.py.
 
 from . import api
 from . import delegation
-from . import exceptions
 from . import model
 
 
@@ -103,7 +102,7 @@ def check_request(
           delegation_token, peer_identity, auth_db)
       ctx.current_identity = ident
       ctx.delegation_token = unwrapped_tok
-    except exceptions.BadTokenError as exc:
+    except delegation.BadTokenError as exc:
       raise api.AuthorizationError('Bad delegation token: %s' % exc)
   elif use_project_identites and project_header:
     # X-Luci-Project header can be used only by LUCI services (which we
