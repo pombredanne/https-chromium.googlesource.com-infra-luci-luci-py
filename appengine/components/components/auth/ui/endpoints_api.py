dd60004962d725b8799b68c8977ab080227d9e84
Felix Matenaar <fmatenaar@chromium.org>
2019-05-31 12:42:27 -0700

    LUCI Buckets
    Bug: 966567
    Change-Id: I0c8a683006a8a26b8de7473fd5e6ba290140c7ee

--- appengine/components/components/auth/ui/endpoints_api.py
+++ appengine/components/components/auth/ui/endpoints_api.py
@@ -28,12 +28,35 @@ class MembershipResponse(messages.Message):
   is_member = messages.BooleanField(1)
 
 
+AuthorizationRequest = endpoints.ResourceContainer(
+  message_types.VoidMessage,
+  identity=messages.StringField(1, required=True),
+  project=messages.StringField(2, required=True),
+  bucket=messages.StringField(3, required=True),
+  service=messages.StringField(4, required=True))
+
+class AuthorizationResponse(messages.Message):
+  is_authorized = messages.BooleanField(1)
+
 ### API
 
 
+def normalize_identity(identity):
+  if ':' not in identity:
+    identity = 'user:%s' % identity
+  try:
+    identity = model.Identity.from_bytes(identity)
+  except ValueError as e:
+    raise endpoints.BadRequestException('Invalid identity: %s.' % e)
+  return identity
+
 @endpoints_support.endpoints_api(name='auth', version='v1')
 class AuthService(remote.Service):
-  """Verifies if a given identity is a member of a particular group."""
+  """Verifies if a given identity authorized a specific action.
+
+  Action is authorized within a specific bucket in a project
+  against a service.
+  """
 
   @endpoints_support.endpoints_method(
       MembershipRequest, MembershipResponse,
@@ -41,12 +64,21 @@ class AuthService(remote.Service):
       path='/membership')
   @api.require(acl.has_access)
   def membership(self, request):
-    identity = request.identity
-    if ':' not in identity:
-      identity = 'user:%s' % identity
-    try:
-      identity = model.Identity.from_bytes(identity)
-    except ValueError as e:
-      raise endpoints.BadRequestException('Invalid identity: %s.' % e)
+    identity = normalize_identity(request.identity)
     is_member = api.is_group_member(request.group, identity)
     return MembershipResponse(is_member=is_member)
+
+  @endpoints_support.endpoints_method(
+    AuthorizationRequest, AuthorizationResponse,
+    http_method='GET',
+    path='/authorized')
+  @api.require(acl.has_access)
+  def authorized(self, request):
+    identity = normalize_identity(request.identity)
+    authorized = api.can_perform_action(
+      request.project,
+      request.bucket,
+      request.service,
+      request.action,
+      identity)
+    return AuthorizationResponse(is_authorized=authorized)
