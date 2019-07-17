8903f8f4444ffe055598e8e614856fe04b6bb308
adoneria <adoneria@google.com>
2019-07-17 12:15:34 -0700

    Server side changes to add User-defined bot keepalive value - added schema and proto changes.
    Change-Id: I1ebfdba089678d40077eb64b6d79330c995f55fa
    Bug:974350

--- appengine/swarming/server/task_result_test.py
+++ appengine/swarming/server/task_result_test.py
@@ -69,6 +69,7 @@ def _gen_request_slice(**kwargs):
       task_request.TaskSlice(expiration_secs=60, properties=_gen_properties()),
     ],
     'user': 'Jesus',
+    'bot_ping_tolerance_secs': 120,
   }
   args.update(kwargs)
   ret = task_request.TaskRequest(**args)
@@ -195,6 +196,7 @@ class TaskResultApiTest(TestCase):
       'completed_ts': None,
       'cost_usd': 0.,
       'current_task_slice': 0,
+      'dead_after_ts': None,
       'duration': None,
       'exit_code': None,
       'failure': False,
