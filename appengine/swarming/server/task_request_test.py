209ab09215fd64f87c5fdce32a90c79dce2c25f1
adoneria <adoneria@google.com>
2019-08-20 14:53:59 -0700

    [swarming] add bot_ping_tolerance
    The value is not used yet, it will replace the default server-wide hardcoded
    value.
    
    Requires BigQuery table update; run setup_bigquery.sh on all instances.
    
    
    Change-Id: I1ebfdba089678d40077eb64b6d79330c995f55fa
    Bug:974350

--- appengine/swarming/server/task_request_test.py
+++ appengine/swarming/server/task_request_test.py
@@ -97,6 +97,7 @@ def _gen_request_slices(**kwargs):
       task_request.TaskSlice(expiration_secs=30, properties=_gen_properties()),
     ],
     u'user': u'Jesus',
+    u'bot_ping_tolerance_secs': 120,
   }
   args.update(kwargs)
   # Note that ndb model constructor accepts dicts for structured properties.
@@ -536,6 +537,7 @@ class TaskRequestApiTest(TestCase):
         },
       ],
       'user': u'Jesus',
+      'bot_ping_tolerance_secs': 120,
     }
     actual = req.to_dict()
     actual.pop('created_ts')
@@ -641,6 +643,7 @@ class TaskRequestApiTest(TestCase):
         },
       ],
       'user': u'Jesus',
+      'bot_ping_tolerance_secs': 120,
     }
     actual = req.to_dict()
     # expiration_ts - created_ts == scheduling_expiration_secs.
@@ -994,6 +997,7 @@ class TaskRequestApiTest(TestCase):
         # Notification. auth_token cannot be retrieved.
         pubsub_notification=swarming_pb2.PubSub(
             topic=u'projects/a/topics/abc', userdata=u'obscure_reference'),
+        bot_ping_tolerance=duration_pb2.Duration(seconds=120),
     )
 
     actual = swarming_pb2.TaskRequest()
@@ -1439,6 +1443,13 @@ class TaskRequestApiTest(TestCase):
     with self.assertRaises(datastore_errors.BadValueError):
       _gen_request(priority=task_request.MAXIMUM_PRIORITY+1)
     _gen_request(priority=task_request.MAXIMUM_PRIORITY).put()
+
+  def test_request_bad_bot_ping_tolerance(self):
+    with self.assertRaises(datastore_errors.BadValueError):
+      _gen_request(
+          bot_ping_tolerance_secs=task_request._MAX_BOT_PING_TOLERANCE_SECS+1)
+      _gen_request(
+          bot_ping_tolerance_secs=task_request._MIN_BOT_PING_TOLERANCE_SECS-1)
 
   def test_request_bad_execution_timeout(self):
     # When used locally, it is set to 1, which means it's impossible to test
