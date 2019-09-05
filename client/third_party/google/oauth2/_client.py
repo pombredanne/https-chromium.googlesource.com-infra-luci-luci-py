8a112ec7ae9aa0651969c38958f4ffafe558adfe
adoneria <adoneria@google.com>
2019-09-05 16:17:01 -0700

    [client] Add retry loop for fetching authentication token if any 'Internal failure' occurs
    This is to prevent errors while client tries to connect with swarming server to fetch results
    and fails with 'Internal Failure' because it was unable to fetch OAuth2 token and connect
    with server.
    
    Bug:1001206
    Change-Id: Ifcaba15b2019779d90224f5e7cec3aa301db99bb

--- client/third_party/google/oauth2/_client.py
+++ client/third_party/google/oauth2/_client.py
@@ -99,14 +99,24 @@ def _token_endpoint_request(request, token_uri, body):
     headers = {
         'content-type': _URLENCODED_CONTENT_TYPE,
     }
+    retry = 0
+    # retry to fetch token for maximum of two times if any internal failure
+    # occurs.
+    while retry < 2:
+      response = request(
+          method='POST', url=token_uri, headers=headers, body=body)
 
-    response = request(
-        method='POST', url=token_uri, headers=headers, body=body)
+      response_body = response.data.decode('utf-8')
 
-    response_body = response.data.decode('utf-8')
-
-    if response.status != http_client.OK:
-        _handle_error_response(response_body)
+      if response.status != http_client.OK:
+        error_desc = json.loads(response_body).get('error_description') or ''
+        if error_desc == 'internal_failure' and retry < 2:
+          retry += 1
+          continue
+        else:
+          _handle_error_response(response_body)
+      else:
+        break
 
     response_data = json.loads(response_body)
 
