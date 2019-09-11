2cd24e1cb68aa6529e36a27a16553af884f562e2
Takuto Ikuta <tikuta@chromium.org>
2019-09-11 20:51:38 +0900

    cancel
    Change-Id: Ie6511855e6a43a675f399f59c4104f0635ee34bc

--- appengine/swarming/server/task_scheduler.py
+++ appengine/swarming/server/task_scheduler.py
@@ -1579,6 +1579,18 @@ def cancel_task(request, result_key, kill_running, bot_id):
   now = utils.utcnow()
   es_cfg = external_scheduler.config_for_task(request)
 
+  task_id = result_key.get().task_id
+  ok = utils.enqueue_task(
+      '/internal/taskqueue/important/tasks/cancel-children-tasks',
+      'cancel-children-tasks', payload=utils.encode_to_json({
+          'task': task_id,
+      })
+  )
+  if not ok:
+    raise Error(
+        'Failed to enqueue task to cancel-children-tasks queue; task_id: %s' % (
+            task_id))
+
   def run():
     """1 DB GET, 1 memcache write, 2x DB PUTs, 1x task queue."""
     # Need to get the current try number to know which TaskToRun to fetch.
