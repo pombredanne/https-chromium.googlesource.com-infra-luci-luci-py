f792b414991152621a6647fb7c8145225b6118ac
Junji Watanabe <jwata@google.com>
2019-10-07 13:19:23 +0900

    [swarming] run tests without PRESUBMIT.py
    This is the first step of unittest migration from presubmit.py
    - use nose2 as a unittest runner which discovers, filters tests
      - nose2: https://github.com/nose-devs/nose2
    To make all tests pass, I did following changes
    - fix imports errors, collisons
    
    Next steps
    - Setup test.py for other directories than appengine/swarming
    - Enable parallel testing nose2 plugin
    
    Bug: 917479
    Change-Id: Ic3cc618d3f9954ab4997e0bd7730acbfb1dbbc98

---
+++ appengine/swarming/test_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+# test starting from current dir and sub packages
+./test.py -v --config unittest.cfg -A '!run_later,!skip_on_ci' -s .
+./test.py -v --config unittest.cfg -A 'run_later,!skip_on_ci' -s .
+
+# test swarming_bot tests
+./test.py -v --config unittest.cfg -A '!run_later,!skip_on_ci' -s ./swarming_bot
+./test.py -v --config unittest.cfg -A 'run_later,!skip_on_ci' -s ./swarming_bot
