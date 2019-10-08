3e0da46a5473386f420b2ee9b4222ec684a72479
Junji Watanabe <jwata@google.com>
2019-10-08 16:31:33 +0900

    [luci-py] run tests without PRESUBMIT.py
    The previous CL
    https://crrev.com/c/1841591
    
    --
    This is the first step of unittest migration from PRESUBMIT.py
    - use nose2 as a unittest runner which discovers, filters tests
      - nose2: https://github.com/nose-devs/nose2
    - add test.py, test_all.sh as entry points
    
    To make all tests pass, I did following changes
    - fix imports errors, collisons
    - add a plugin to ignore symlinks
    - add a plugin to discover python3 compatible tests by checking shebang
    
    Next steps
    - Setup test.py for other directories than appengine/swarming
    - Enable parallel testing nose2 plugin
    
    
    Change-Id: I2c047c5634e490ad330055593a2cc6e8c08c1440

---
+++ appengine/swarming/test_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+cfgopt= '--config unittest.cfg'
+attr='!no_run'
+
+# Python2
+echo "Running tests in python2"
+
+echo "Tests in swarming dir and packages"
+./test.py -v ${cfgopt} -A "${attr},!run_later" -s .
+./test.py -v ${cfgopt} -A "${attr},run_later" -s .
+
+echo "Tests in swarming/swarming_bot dir and packages"
+./test.py -v ${cfgopt} -A "${attr},!run_later" -s ./swarming_bot
+./test.py -v ${cfgopt} -A "${attr},run_later" -s ./swarming_bot
+
+echo "Smoke test"
+./local_smoke_test.py
+
+# Python3
+echo "Running tests in python3"
+py3cmd="python3 ./test.py"
+
+echo "Tests in swarming dir and packages"
+${py3cmd} -v ${cfgopt} --python3 -A "${attr},!run_later" -s .
+${py3cmd} ./test.py -v ${cfgopt} --python3 -A "${attr},run_later" -s .
+
+echo "Tests in swarming/swarming_bot dir and packages"
+${py3cmd} -v ${cfgopt} --python3 -A "${attr},!run_later" -s ./swarming_bot
+${py3cmd} -v ${cfgopt} --python3 -A "${attr},run_later" -s ./swarming_bot
