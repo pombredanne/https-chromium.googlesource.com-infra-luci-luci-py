2eaec9eaae3663f22f0e47e6f10599ec97ffdfc6
Junji Watanabe <jwata@google.com>
2019-10-09 16:16:22 +0900

    [swarming] add test_all.py to migrate tests from PRESUBMIT.py
    The first step is to migrate local_smoke_test.py since it fails with timeout in presubmit.
    
    Bug: 917479
    Change-Id: I11e04c2f22bc9c5c7f1f8d82a04f9fc104673947

---
+++ appengine/swarming/test_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import sys
+import os
+import subprocess
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def run_smoketest(python3):
+  if python3:
+    print('Skipping running smoke test in Python3')
+    return 0
+
+  smoke_test = os.path.join(ROOT_DIR, 'local_smoke_test.py')
+  cmd = [smoke_test, '-v']
+
+  print('Running smoke test: %r' % cmd)
+  return subprocess.call(cmd)
+
+
+def main():
+  exit_code = 0
+
+  for py3 in [False, True]:
+    # smoke test
+    exit_code = run_smoketest(py3) or exit_code
+
+  return exit_code
+
+
+if __name__ == '__main__':
+  sys.exit(main())
