c8e7fd1398106ab6cb531e33e6deda15598957a3
Junji Watanabe <jwata@google.com>
2019-10-10 13:37:04 +0900

    [swarming] run unittests using nose2
    I separated changes to other small CLs from the previous CL.
    https://crrev.com/c/1844594
    
    This CL is mainly adding nose2 test runner to swarming, swarming_bot dirs
    - add dependencies to vpython, vpython3
    - add test.py to swarming and swarming_bot
    - add nose2 plugins
    - some test fixes
    
    Bug: 917479
    Change-Id: Id9273af3189f0f732f5d8d46bfd7292a99d74d39

---
+++ appengine/swarming/swarming_bot/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env vpython
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import os
+import sys
+import logging
+
+import six
+from nose2 import discover
+
+SWARMING_BOT_DIR = os.path.dirname(os.path.abspath(__file__))
+APPENGINE_DIR = os.path.dirname(SWARMING_BOT_DIR)
+PLUGINS_DIR = os.path.join(APPENGINE_DIR,
+                           'components', 'test_support', 'nose2_plugins')
+
+
+def main():
+  import test_env_bot
+  test_env_bot.setup_test_env()
+
+  from depot_tools import fix_encoding
+  fix_encoding.fix_encoding()
+
+  # add nose2 plugin dir to path
+  sys.path.insert(0, PLUGINS_DIR)
+
+  discover()
+
+
+def _has_arg(argv, arg):
+  return len([a for a in argv if arg in a]) > 0
+
+
+if __name__ == '__main__':
+  if not _has_arg(sys.argv, '--log-level'):
+    logging.basicConfig(level=logging.CRITICAL)
+  main()
