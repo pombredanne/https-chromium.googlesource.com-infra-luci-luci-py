1265a3fb133c438d82feb5754a5306dd6c4f5497
Junji Watanabe <jwata@google.com>
2019-10-17 13:00:19 +0900

    [swarming] fix unittests in lucy_py tryjobs
    Bug: 917479
    Change-Id: I178201f3838172aa30b4c71c0e5d324c2e17835a

--- appengine/swarming/test_seq.py
+++ appengine/swarming/test_seq.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env vpython
 # Copyright 2019 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
@@ -7,10 +7,16 @@ import os
 import subprocess
 import sys
 
+import six
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def run_test(test_file, python3=False):
+  if python3 and not _has_py3_shebang(test_file):
+    print('Skipping test in python3: %s' % test_file)
+    return 0
+
   # vpython
   vpython = 'vpython'
   if python3:
@@ -22,6 +28,12 @@ def run_test(test_file, python3=False):
   return subprocess.call(cmd)
 
 
+def _has_py3_shebang(path):
+  with open(path, 'r') as f:
+    maybe_shebang = f.readline()
+  return maybe_shebang.startswith('#!') and 'python3' in maybe_shebang
+
+
 def main():
   exit_code = 0
 
@@ -29,13 +41,18 @@ def main():
   # because they don't pass when running in parallel
   # or run via test runner
   test_files = [
+      'handlers_backend_test.py',
+      'handlers_endpoints_test.py',
+      'handlers_prpc_test.py',
       'server/bot_groups_config_test.py',
+      'swarming_bot/api/os_utilities_test.py',
       'swarming_bot/bot_code/singleton_test.py',
+      'swarming_bot/bot_code/bot_main_test.py',
       'local_smoke_test.py',
   ]
 
   for test_file in test_files:
-    exit_code = run_test(test_file) or exit_code
+    exit_code = run_test(test_file, python3=six.PY3) or exit_code
 
   return exit_code
 
