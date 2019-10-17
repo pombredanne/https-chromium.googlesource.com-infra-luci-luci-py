dcc3dbdb7112b7ff3b6df667a887b6a5bf9b8d53
Junji Watanabe <jwata@google.com>
2019-10-17 10:19:23 +0900

    [swarming] fix unittests in lucy_py tryjobs
    Bug: 917479
    Change-Id: I178201f3838172aa30b4c71c0e5d324c2e17835a

--- appengine/swarming/test_seq.py
+++ appengine/swarming/test_seq.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env vpython
 # Copyright 2019 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
@@ -7,10 +7,16 @@ import os
 import subprocess
 import sys
 
+import six
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def run_test(test_file, python3=False):
+  if python3 and not _has_py3_shebang(test_file):
+    print('Skipping test in python3: %s' % test_file)
+    return 0
+
   # vpython
   vpython = 'vpython'
   if python3:
@@ -20,6 +26,12 @@ def run_test(test_file, python3=False):
 
   print('Running test script: %r' % cmd)
   return subprocess.call(cmd)
+
+
+def _has_py3_shebang(path):
+  with open(path, 'r') as f:
+    maybe_shebang = f.readline()
+  return maybe_shebang.startswith('#!') and 'python3' in maybe_shebang
 
 
 def main():
@@ -35,7 +47,7 @@ def main():
   ]
 
   for test_file in test_files:
-    exit_code = run_test(test_file) or exit_code
+    exit_code = run_test(test_file, python3=six.PY3) or exit_code
 
   return exit_code
 
