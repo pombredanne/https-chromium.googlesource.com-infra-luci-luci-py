f1fcc79050f991273e8a8c51c4a0e1a9f9321740
Takuto Ikuta <tikuta@chromium.org>
2019-10-21 21:44:13 +0900

    client: make large_test.py python3 compatible
    Bug: 1010816
    Change-Id: Iafc4c7b77b98d639309637b2009f131357b1e9cf

--- client/utils/large.py
+++ client/utils/large.py
@@ -14,6 +14,8 @@ can be very high for monotonically increasing sets.
 import sys
 import zlib
 
+import six
+
 
 def pack(values):
   """Returns a deflate'd buffer of delta encoded varints.
@@ -24,9 +26,9 @@ def pack(values):
   Returns:
     compressed buffer as a str.
   """
-  out = ''
+  out = bytearray()
   if not values:
-    return ''
+    return b''
   last = 0
   max_value = long(2)**63 if sys.version_info.major == 2 else 2**63
   assert 0 <= values[0] < max_value, 'Values must be between 0 and 2**63'
@@ -37,10 +39,10 @@ def pack(values):
     assert value >= 0, 'List must be sorted ascending'
     last = v
     while value > 127:
-      out += chr((1 << 7) | (value & 0x7F))
+      out.append((1 << 7) | (value & 0x7F))
       value >>=  7
-    out += chr(value)
-  return zlib.compress(out)
+    out.append(value)
+  return zlib.compress(bytes(out))
 
 
 def unpack(data):
@@ -59,7 +61,10 @@ def unpack(data):
   base = 1
   last = 0
   for d in zlib.decompress(data):
-    val_byte = ord(d)
+    if six.PY2:
+      val_byte = ord(d)
+    else:
+      val_byte = d
     value += (val_byte & 0x7F) * base
     if val_byte & 0x80:
       base <<= 7
