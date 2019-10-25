365a0a9fe7fd530bc4519381591f13871b16e20a
Takuto Ikuta <tikuta@chromium.org>
2019-10-25 18:42:46 +0900

    bot_code: make file_reader_test.py run on Python3
    Bug: 1010816
    Change-Id: Id88c1a6b445ba365fffbe48f36a48d5091089094

--- appengine/swarming/swarming_bot/bot_code/file_reader_test.py
+++ appengine/swarming/swarming_bot/bot_code/file_reader_test.py
@@ -33,7 +33,7 @@ class TestFileReaderThread(auto_stub.TestCase):
     super(TestFileReaderThread, self).tearDown()
 
   def test_works(self):
-    with open(self.path, 'wb') as f:
+    with open(self.path, 'w') as f:
       json.dump({'A': 'a'}, f)
 
     r = file_reader.FileReaderThread(self.path, 0.1)
@@ -45,7 +45,10 @@ class TestFileReaderThread(auto_stub.TestCase):
       attempt = 0
       while True:
         try:
-          file_path.atomic_replace(self.path, json.dumps({'B': 'b'}))
+          file_path.atomic_replace(self.path,
+                                   json.dumps({
+                                       'B': 'b'
+                                   }).encode('utf-8'))
           break
         except OSError:
           attempt += 1
