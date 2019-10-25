854bd62280dbf0ed790274ff33ece9b2565b9a80
Junji Watanabe <jwata@google.com>
2019-10-25 15:18:48 +0900

    [swarming] remove test_support symlink from client and bot
    symlinks aren't created in windows
    
    Bug: 1017545
    Change-Id: I61f1e1a38cf2966069bf3b60f52d3b4ad1dd8bb5

--- client/test.py
+++ client/test.py
@@ -5,23 +5,29 @@
 
 import os
 import sys
+print(sys.version_info)
 
 import six
 
-from test_support import parallel_test_runner
-
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TESTS_DIR = os.path.join(THIS_DIR, 'tests')
+LUCI_DIR = os.path.dirname(THIS_DIR)
+COMPONENTS_DIR = os.path.join(LUCI_DIR, 'appengine', 'components')
 
 def main():
   sys.path.insert(0, TESTS_DIR)
   import test_env
   test_env.setup()
 
+  sys.path.insert(0, COMPONENTS_DIR)
+  from test_support import parallel_test_runner
+
   # Need to specify config path explicitly
   # because test_env.setup() changes directory
-  cfg = os.path.join(THIS_DIR, 'unittest.cfg')
+  cfg = os.path.join(COMPONENTS_DIR, 'test_support', 'unittest.cfg')
   sys.argv.extend(['-c', cfg])
+
+  sys.argv.extend(['--log-level', 'DEBUG'])
 
   # execute test runner
   return parallel_test_runner.run_tests(python3=six.PY3)
