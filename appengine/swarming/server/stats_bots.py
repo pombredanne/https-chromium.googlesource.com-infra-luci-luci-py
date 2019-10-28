b0bebc6106b25961bf444de1f6304614b58e4d0c
Marc-Antoine Ruel <maruel@chromium.org>
2019-10-28 16:58:59 -0400

    [swarming] Use stats_framework to send bots BQ data
    This is an example CL of how this could be done.
    
    Bug: 864724
    Change-Id: I69d376a09ec408512d77fb61f6a7a00c989da4cb

--- appengine/swarming/server/stats_bots.py
+++ appengine/swarming/server/stats_bots.py
@@ -5,8 +5,45 @@
 
 """Generates statistics for the bots. Contains the backend code."""
 
+from google.appengine.ext import ndb
+
+
+### Models
+
+
+class _Snapshot(ndb.Model):
+  """A snapshot of statistics, to be embedded in another entity."""
+  # Number of individual uploads and total amount of bytes. Same for downloads.
+  uploads = ndb.IntegerProperty(default=0, indexed=False)
+  uploads_bytes = ndb.IntegerProperty(default=0, indexed=False)
+  downloads = ndb.IntegerProperty(default=0, indexed=False)
+  downloads_bytes = ndb.IntegerProperty(default=0, indexed=False)
+
+  # Number of /contains requests and total number of items looked up.
+  contains_requests = ndb.IntegerProperty(default=0, indexed=False)
+  contains_lookups = ndb.IntegerProperty(default=0, indexed=False)
+
+  # Total number of requests to calculate QPS
+  requests = ndb.IntegerProperty(default=0, indexed=False)
+  # Number of non-200 requests.
+  failures = ndb.IntegerProperty(default=0, indexed=False)
+
+  def accumulate(self, rhs):
+    return stats_framework.accumulate(self, rhs, [])
+
+
+def _to_proto(s):
+  """Shorthand to create a proto."""
+  out = isolated_pb2.StatsSnapshot()
+  snapshot_to_proto(s, out)
+  return out
+
 
 ### Public API
+
+
+STATS_HANDLER = stats_framework.StatisticsFramework(
+      'bots', _Snapshot, _extract_snapshot_from_logs)
 
 
 def cron_generate_stats():
