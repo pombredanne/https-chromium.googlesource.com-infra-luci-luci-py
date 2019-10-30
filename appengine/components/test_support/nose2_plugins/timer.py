7e7f634be76eb95e2139fc710ad087a97e377407
Takuto Ikuta <tikuta@chromium.org>
2019-10-30 20:39:00 +0900

    components: support timeout in parallel_test_runner.py
    and shows stacktrace when timeout happens in Mac.
    
    Bug: 1019105
    Change-Id: Ife15aed8e4824463e835e65c87cc0fe5d4e93ba5

---
+++ appengine/components/test_support/nose2_plugins/timer.py
@@ -0,0 +1,42 @@
+# Copyright 2019 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+from __future__ import print_function
+
+import logging
+import threading
+import sys
+import os
+
+from nose2.events import Plugin
+
+log = logging.getLogger('nose2.plugins.timer')
+
+
+def _show_all_stacktraces():
+  """This is used to show where the threads are stucked."""
+  frames = sys._current_frames()
+  for th in threading.enumerate():
+    print('Thread:%s' % th.name, file=sys.stderr)
+    if th.ident is None:
+      print('not started, skipped', file=sys.stderr)
+      continue
+    traceback.print_stack(frames[th.ident], limit=None)
+
+  sys.stderr.flush()
+  # os._exit is necessary to exit whole process.
+  os._exit(1)
+
+
+class Timer(Plugin):
+  alwaysOn = True
+
+  def startTest(self, event):
+    self._timer = threading.Timer(30, _show_all_stacktraces)
+    self._timer.start()
+    log.debug('start %s', event.test)
+
+  def stopTest(self, event):
+    self._timer.cancel()
+    log.debug('stop %s', event.test)
