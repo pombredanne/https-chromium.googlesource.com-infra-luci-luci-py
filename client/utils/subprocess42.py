3b0472b568c87afe70f14ffa0f23321e2849ee76
Marc-Antoine Ruel <maruel@chromium.org>
2019-10-30 14:14:13 -0400

    [subprocess42] Use WaitForMultipleObjects on Windows
    This is more reliable than polling.
    
    Change-Id: I23c917d28dfc231489dc23f3f2e43d8415de68e5

--- client/utils/subprocess42.py
+++ client/utils/subprocess42.py
@@ -1044,3 +1044,356 @@ def split(data, sep='\n'):
   for pipe_name, chunks in sorted(pending_chunks.items()):
     if chunks:
       yield pipe_name, ''.join(chunks)
+
+
+### JUNK ###
+
+if subprocess.mswindows:
+  import msvcrt  # pylint: disable=F0401
+  from ctypes import wintypes
+  from ctypes import windll
+
+  INFINITE = -1
+  WAIT_TIMEOUT = 0x00000102
+  WAIT_FAILED = 0xFFFFFFFF
+
+  ERROR_IO_PENDING = 997
+  FILE_FLAG_OVERLAPPED = 0x40000000
+
+  # http://msdn.microsoft.com/en-us/library/windows/desktop/ms684342.aspx
+  class _US(ctypes.Structure):
+    _fields_ = [
+        ("Offset", wintypes.DWORD),
+        ("OffsetHigh", wintypes.DWORD),
+    ]
+
+  class _U(ctypes.Union):
+    _fields_ = [
+        ("s", _US),
+        ("Pointer", wintypes.LPVOID),
+    ]
+    _anonymous_ = ("s",)
+
+  class OVERLAPPED(ctypes.Structure):
+    _fields_ = [
+        ("Internal", wintypes.POINTER(wintypes.ULONG)),
+        ("InternalHigh", wintypes.POINTER(wintypes.ULONG)),
+        ("u", _U),
+        ("hEvent", wintypes.HANDLE),
+    ]
+    _anonymous_ = ("u",)
+
+  ctypes.windll.kernel32.CloseHandle.argtypes = (wintypes.HANDLE,)
+  ctypes.windll.kernel32.CloseHandle.restype = wintypes.BOOL
+
+  ctypes.windll.kernel32.CreateEventA.argtypes = (
+      # No need to support DACL here.
+      wintypes.LPVOID,
+      wintypes.BOOL,
+      wintypes.BOOL,
+      wintypes.LPCSTR)
+  ctypes.windll.kernel32.CreateEventA.restype = wintypes.HANDLE
+
+  windll.kernel32.PeekNamedPipe.argtypes = (wintypes.HANDLE, wintypes.LPVOID,
+                                            wintypes.DWORD,
+                                            wintypes.POINTER(wintypes.DWORD),
+                                            wintypes.POINTER(wintypes.DWORD),
+                                            wintypes.POINTER(wintypes.DWORD))
+  windll.kernel32.PeekNamedPipe.restype = wintypes.BOOL
+
+  windll.kernel32.ReadFile.argtypes = (
+      wintypes.HANDLE, wintypes.LPVOID, wintypes.DWORD,
+      wintypes.POINTER(wintypes.DWORD),
+      wintypes.LPVOID)  # wintypes.POINTER(OVERLAPPED))
+  windll.kernel32.ReadFile.restype = wintypes.BOOL
+
+  windll.kernel32.WaitForMultipleObjects.argtypes = (wintypes.DWORD,
+                                                     wintypes.POINTER(
+                                                         wintypes.HANDLE),
+                                                     wintypes.BOOL,
+                                                     wintypes.DWORD)
+  windll.kernel32.WaitForMultipleObjects.restype = wintypes.DWORD
+
+  def ReadFile(handle, desired_bytes):
+    """Calls kernel32.ReadFile()."""
+    c_read = wintypes.DWORD()
+    buff = wintypes.create_string_buffer(desired_bytes + 1)
+    result = windll.kernel32.ReadFile(handle, buff, desired_bytes,
+                                      wintypes.byref(c_read), None)
+    if not result:
+      raise OSError(wintypes.GetLastError())
+    # NULL terminate it.
+    buff[c_read.value] = '\x00'
+    return buff.value
+
+  def PeekNamedPipe(handle):
+    """Calls kernel32.PeekNamedPipe(). Simplified version."""
+    c_avail = wintypes.DWORD()
+    c_message = wintypes.DWORD()
+    success = windll.kernel32.PeekNamedPipe(handle, None, 0, None,
+                                            wintypes.byref(c_avail),
+                                            wintypes.byref(c_message))
+    if not success:
+      raise OSError(wintypes.GetLastError())
+    return c_avail.value
+
+  def WaitForMultipleObjects(handles, timeout_ms):
+    if timeout_ms is None:
+      timeout_ms = 0xFFFFFFFF
+    arrtype = wintypes.HANDLE * len(handles)
+    return windll.kernel32.WaitForMultipleObjects(
+        len(handles), arrtype(*handles), False, int(timeout_ms))
+
+  def create_overlapped_pipe():
+    """Creates a pair of readonly/writeonly pipes to be used in asynchronous
+    I/O.
+
+    Similar to CreatePipe() except that the pipes support asynchronous I/O.
+
+    Calls random.random() internally, which could affect programs that depends
+    on deterministic random number generation.
+    """
+    # Use random.random() first for faster default path. If the pipe already
+    # exists, fall back to os.urandom().
+    space = len(_RANDOM_LETTERS)
+    unique = ''.join(
+        _RANDOM_LETTERS[random.randrange(space)] for _ in xrange(8))
+    for _ in xrange(3):
+      pipename = r'\\\\.\\Pipe\\PythonPipe_%s' % unique
+      # It's fine to not set a DACL since the pipe only accept a single
+      # connection. Once it accepted its first connection, it must not accept
+      # any other.
+      read = ctypes.windll.kernel32.CreateNamedPipeA(
+          pipename, PIPE_ACCESS_INBOUND | FILE_FLAG_OVERLAPPED,
+          PIPE_TYPE_BYTE | PIPE_WAIT, 1, 0, 0, 120 * 1000, None)
+      if read:
+        break
+      # The pipe existed already, use a more random source of randomness.
+      for i in os.urandom(8):
+        # TODO(maruel): Lame. Reduces randomness.
+        unique += _RANDOM_LETTERS[i % space]
+      unique += ''.join(_RANDOM_LETTERS[os.urandom(space)] for _ in xrange(8))
+    else:
+      raise OSError('Impossible to create a pipe')
+    write = ctypes.windll.kernel32.CreateFileA(pipename, GENERIC_WRITE, 0, None,
+                                               OPEN_EXISTING,
+                                               FILE_FLAG_OVERLAPPED, None)
+    if not write:
+      raise OSError('Failed to open the pipe just created')
+    return read, write
+
+  def recv_multi_impl(conns, maxsize, timeout):
+    """Reads from the first available pipe.
+
+    If timeout is None, it's blocking. If timeout is 0, it is not blocking.
+    """
+    # TODO(maruel): Use WaitForMultipleObjects(). Python creates anonymous pipes
+    # for proc.stdout and proc.stderr but they are implemented as named pipes on
+    # Windows. Since named pipes are not waitable object, they can't be passed
+    # as-is to WFMO(). So this means N times CreateEvent(), N times ReadFile()
+    # and finally WFMO(). This requires caching the events handles in the Popen
+    # object and remembering the pending ReadFile() calls. This will require
+    # some re-architecture.
+    maxsize = max(maxsize or 16384, 1)
+    if timeout:
+      start = time.time()
+    #logging.debug('Handles1: %s', [conn.fileno() for conn in conns])
+    handles = [
+        (i, msvcrt.get_osfhandle(c.fileno())) for i, c in enumerate(conns)
+    ]
+    events = [CreateEvent(False, None) for _ in handles]
+    overlapped = [OVERLAPPED(e) for e in events]
+    while handles:
+      if timeout:
+        remaining = max(0, (timeout - (time.time() - start)) * 1000)
+      else:
+        remaining = timeout
+      start = time.time()
+      result = WaitForMultipleObjects(events, remaining)
+      logging.debug('WaitForMultipleObjects(%s, %s) = %s in %1.3f',
+                    len(handles), remaining, result,
+                    time.time() - start)
+      if 0 <= result < len(conns):
+        #logging.debug('A')
+        try:
+          avail = min(PeekNamedPipe(handles[result]), maxsize)
+          #logging.debug('B %s', avail)
+          if avail:
+            data = ReadFile(handles[result], avail)
+            #logging.debug('C %s', data)
+            return result, data
+          #return result, ''
+          continue
+        except OSError as e:
+          logging.debug('OSError %s', e)
+          # Not classy but fits our needs, this is usually caused by the process
+          # shutting down.
+          return None, None
+      elif result == WAIT_TIMEOUT:
+        logging.debug('WAIT_TIMEOUT')
+        return None, None
+      elif result == WAIT_FAILED:
+        logging.debug('WAIT_FAILED')
+        raise OSError(result)
+      else:
+        logging.error('No idea why WaitForMultipleObjects failed')
+        raise OSError(result)
+
+else:
+  import fcntl  # pylint: disable=F0401
+  import select
+
+  def recv_multi_impl(conns, maxsize, timeout):
+    """Reads from the first available pipe.
+
+    If timeout is None, it's blocking. If timeout is 0, it is not blocking.
+    """
+    try:
+      r, _, _ = select.select(conns, [], [], timeout)
+    except select.error:
+      return None, None
+    if not r:
+      return None, None
+
+    conn = r[0]
+    # Temporarily make it non-blocking.
+    flags = fcntl.fcntl(conn, fcntl.F_GETFL)
+    if not conn.closed:
+      # pylint: disable=E1101
+      fcntl.fcntl(conn, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+    try:
+      data = conn.read(max(maxsize or 16384, 1))
+      return conns.index(conn), data
+    finally:
+      if not conn.closed:
+        fcntl.fcntl(conn, fcntl.F_SETFL, flags)
+
+
+class Failure(Exception):
+  pass
+
+
+class Popen(subprocess.Popen):
+  """Adds timeout support on stdout and stderr.
+
+  Inspired by
+  http://code.activestate.com/recipes/440554-module-to-allow-asynchronous-subprocess-use-on-win/
+  """
+
+  def __init__(self, *args, **kwargs):
+    self.start = time.time()
+    self.end = None
+    # TODO(maruel): Doesn't work.
+    # Named pipes must be used with asynchronous reads.
+    # subprocess.Popen._get_handles() creates anonymous pipes which don't
+    # support asynchronous I/O.
+    # The problem is how to push the tuple to _get_handles.
+
+    # Also, we're screwed if the user passes a file object.
+    if kwargs.get('stdout') == subprocess.PIPE:
+      kwargs['stdout'] = create_overlapped_pipe()
+    if kwargs.get('stderr') == subprocess.PIPE:
+      kwargs['stderr'] = create_overlapped_pipe()
+    super(Popen, self).__init__(*args, **kwargs)
+
+  def duration(self):
+    """Duration of the child process.
+
+    It is greater or equal to the actual time the child process ran. It can be
+    significantly higher than the real value if neither .wait() nor .poll() was
+    used.
+    """
+    return (self.end or time.time()) - self.start
+
+  def wait(self):
+    ret = super(Popen, self).wait()
+    if not self.end:
+      # communicate() uses wait() internally.
+      self.end = time.time()
+    return ret
+
+  def poll(self):
+    ret = super(Popen, self).poll()
+    if ret is not None and not self.end:
+      self.end = time.time()
+    return ret
+
+  def yield_any(self, timeout=None):
+    """Yields output until the process terminates or is killed by a timeout.
+
+    Yielded values are in the form (pipename, data).
+
+    If timeout is None, it is blocking. If timeout is 0, it doesn't block. This
+    is not generally useful to use timeout=0.
+    """
+    remaining = 0
+    while self.poll() is None:
+      if timeout:
+        # While these float() calls seem redundant, they are to force
+        # ResetableTimeout to "render" itself into a float. At each call, the
+        # resulting value could be different, depending if a .reset() call
+        # occurred.
+        remaining = max(float(timeout) - self.duration(), 0.001)
+      else:
+        remaining = timeout
+      t, data = self.recv_any(timeout=remaining)
+      if data or timeout == 0:
+        yield (t, data)
+      if timeout and self.duration() >= float(timeout):
+        break
+    if self.poll() is None and timeout and self.duration() >= float(timeout):
+      logging.debug('Kill %s %s', self.duration(), float(timeout))
+      self.kill()
+    self.wait()
+    # Read all remaining output in the pipes.
+    while True:
+      t, data = self.recv_any()
+      if not data:
+        break
+      yield (t, data)
+
+  def recv_any(self, maxsize=None, timeout=None):
+    """Reads from stderr and if empty, from stdout.
+
+    If timeout is None, it is blocking. If timeout is 0, it doesn't block.
+    """
+    pipes = [
+        x for x in ((self.stderr, 'stderr'), (self.stdout, 'stdout')) if x[0]
+    ]
+    if len(pipes) == 2 and self.stderr.fileno() == self.stdout.fileno():
+      pipes.pop(0)
+    if not pipes:
+      return None, None
+    conns, names = zip(*pipes)
+    index, data = recv_multi_impl(conns, maxsize, timeout)
+    if index is None:
+      return index, data
+    if not data:
+      self._close(names[index])
+      return None, None
+    if self.universal_newlines:
+      data = self._translate_newlines(data)
+    return names[index], data
+
+  def recv_out(self, maxsize=None, timeout=None):
+    """Reads from stdout asynchronously."""
+    return self._recv('stdout', maxsize, timeout)
+
+  def recv_err(self, maxsize=None, timeout=None):
+    """Reads from stderr asynchronously."""
+    return self._recv('stderr', maxsize, timeout)
+
+  def _close(self, which):
+    getattr(self, which).close()
+    setattr(self, which, None)
+
+  def _recv(self, which, maxsize, timeout):
+    conn = getattr(self, which)
+    if conn is None:
+      return None
+    data = recv_multi_impl([conn], maxsize, timeout)
+    if not data:
+      return self._close(which)
+    if self.universal_newlines:
+      data = self._translate_newlines(data)
+    return data
