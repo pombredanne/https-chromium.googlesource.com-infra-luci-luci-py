95d9c1c046d00ebd35766d2b2c84f019926fbe85
Takuto Ikuta <tikuta@chromium.org>
2019-12-03 13:07:16 +0900

    Reland "task_runner: enable go isolated client"
    This is a reland of 2c071733b79e6929299b5704283c14771658e0c4
    
    Original change's description:
    > task_runner: enable go isolated client
    > 
    > Let me see what happens on dev instances.
    > 
    > Bug: 932396
    > Change-Id: I636243dd350111fd2d4acbaa42f264b34ddf21d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1943743
    > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Junji Watanabe <jwata@google.com>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>
    
    Bug: 932396
    Change-Id: I1a378341261f42825ed38dc752dfe254d98ded48

--- appengine/swarming/tools/start_bot.py
+++ appengine/swarming/tools/start_bot.py
@@ -27,6 +27,11 @@ from six.moves import urllib
 
 from utils import file_path
 from utils import subprocess42
+
+sys.path.insert(0, os.path.dirname(THIS_DIR))
+from swarming_bot.bot_code import bot_main
+
+sys.path.pop(0)
 sys.path.pop(0)
 sys.path.pop(0)
 
@@ -65,12 +70,16 @@ class LocalBot(object):
       # effect.
       # These values are from ./swarming_bot/bot_code/bot_main.py.
       _safe_rm(os.path.join(self._botdir, 'c'))
+      # TODO(crbug.com/932396): remove this line.
       _safe_rm(os.path.join(self._botdir, 'isolated_cache'))
+      _safe_rm(os.path.join(self._botdir, bot_main.GO_ISOLATED_CACHE_DIR))
       self.start()
     else:
       logging.info('wipe_cache(): wiping cache without telling the bot')
       _safe_rm(os.path.join(self._botdir, 'c'))
+      # TODO(crbug.com/932396): remove this line.
       _safe_rm(os.path.join(self._botdir, 'isolated_cache'))
+      _safe_rm(os.path.join(self._botdir, bot_main.GO_ISOLATED_CACHE_DIR))
 
   @property
   def bot_id(self):
