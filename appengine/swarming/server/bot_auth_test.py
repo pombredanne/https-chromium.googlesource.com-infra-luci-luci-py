871f04dd538cd1b7e39a390cc60df2a7b91ad620
Junji Watanabe <jwata@google.com>
2020-01-08 19:42:17 +0900

    [swarming] support foo--bar bot_id in bot group config
    Change-Id: If94a041acec4c4c7f3cac8862dcbe00025e5bad6

--- appengine/swarming/server/bot_auth_test.py
+++ appengine/swarming/server/bot_auth_test.py
@@ -74,6 +74,10 @@ TEST_CONFIG = bots_pb2.BotsCfg(
         bots_pb2.BotAuth(ip_whitelist='ip_whitelist'),
       ],
       dimensions=['pool:with_fallback_to_ip_wl']),
+    bots_pb2.BotGroup(
+      bot_id=['bot_host--container'],
+      auth=[bots_pb2.BotAuth(require_luci_machine_token=True)],
+      dimensions=['pool:with_token']),
   ],
 )
 
@@ -264,6 +268,12 @@ class BotAuthTest(test_case.TestCase):
       bot_auth.validate_bot_id_and_fetch_config('bot_with_token--vm123')
     self.assert_error_log('bot ID doesn\'t match the machine token used')
     self.assert_error_log('bot_id: "bot_with_token"')
+
+  def test_containerized_bot_id(self):
+    # Caller is using machine token that maches hostname
+    self.mock_caller('bot:bot_host.domain', '1.2.3.5')
+    cfg = bot_auth.validate_bot_id_and_fetch_config('bot_host--container')
+    self.assertEquals({u'pool': [u'with_token']}, cfg.dimensions)
 
   def test_first_method_is_used(self):
     self.mock_caller('bot:bot_with_fallback_to_ip_wl.domain', '2.2.2.2')
