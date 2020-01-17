34e69661e565b7bb37db4893643b27e1469f3ae0
Junji Watanabe <jwata@google.com>
2020-01-17 19:04:14 +0900

    [swarming] external scheduler: do not raise exception when reaping nonpending task
    When external schedler assigning a task, the task is sometimes not pending.
    The task is either running or finished, including failures.
    
    In that situation, there is nothing swarming can do other than notifying to external scheduler
    to fix the inconsistent state, and the exception can be ignore.
    
    This is a dominant error in chromeos-swarming console.
    http://screen/AJ1tsMNA7DZ
    
    Bug: 1032894
    Change-Id: I434a32ad7e775d662a461f35892802798310058c

--- appengine/swarming/server/task_scheduler_test.py
+++ appengine/swarming/server/task_scheduler_test.py
@@ -614,11 +614,14 @@ class TaskSchedulerApiTest(test_env_handlers.AppTestBase):
     self._mock_es_notify()
     result_summary = self._quick_schedule(1)
     self._mock_es_assign(result_summary.task_id, 0)
+    calls = []
+    self.mock(external_scheduler, 'notify_requests',
+              lambda *_: calls.append(True))
 
-    # Exception is raised, because external scheduler attempts to reap a
-    # non-PENDING task.
-    with self.assertRaises(external_scheduler.ExternalSchedulerException):
-      task_scheduler.bot_reap_task(self.bot_dimensions, 'abc')
+    # It should notify to external scheduler. But an exception won't be raised
+    # because the task is already running or has finished including failures.
+    task_scheduler.bot_reap_task(self.bot_dimensions, 'abc')
+    self.assertEqual(len(calls), 1)
 
   def test_bot_reap_task_es_with_pending_task(self):
     self._setup_es(False)
@@ -2737,7 +2740,8 @@ class TaskSchedulerApiTest(test_env_handlers.AppTestBase):
   def test_ensure_active_slice_nonpending(self):
     # Non-PENDING task cannot have active slice set.
     r = self._quick_schedule(1)
-    self.assertEqual(task_scheduler._ensure_active_slice(r.request, 1, 1), None)
+    self.assertEqual(
+        task_scheduler._ensure_active_slice(r.request, 1, 1), (None, False))
 
   def test_ensure_active_slice_pending(self):
     # Pending task can be forced between different active slices via
@@ -2758,15 +2762,15 @@ class TaskSchedulerApiTest(test_env_handlers.AppTestBase):
         ],
     )
 
-    to_run = task_scheduler._ensure_active_slice(r.request, 1, 0)
+    to_run, _ = task_scheduler._ensure_active_slice(r.request, 1, 0)
     self.assertEqual(to_run.task_slice_index, 0)
-    to_run = task_scheduler._ensure_active_slice(r.request, 1, 1)
+    to_run, _ = task_scheduler._ensure_active_slice(r.request, 1, 1)
     self.assertEqual(to_run.task_slice_index, 1)
-    to_run = task_scheduler._ensure_active_slice(r.request, 1, 0)
+    to_run, _ = task_scheduler._ensure_active_slice(r.request, 1, 0)
     self.assertEqual(to_run.task_slice_index, 0)
     # This works even if there is no to_run entity.
     to_run.key.delete()
-    to_run = task_scheduler._ensure_active_slice(r.request, 1, 1)
+    to_run, _ = task_scheduler._ensure_active_slice(r.request, 1, 1)
     self.assertEqual(to_run.task_slice_index, 1)
 
   def test_task_expire_tasks(self):
