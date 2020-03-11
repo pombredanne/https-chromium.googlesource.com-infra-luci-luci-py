99be320cfa27a4b74a9a0d2fc47dedc632ee29a2
Junji Watanabe <jwata@google.com>
2020-03-11 14:23:07 +0900

    [swarming] refactor bot_management.bot_event() in tests
    This is a refactoring CL. and No functionality changes.
    Use _bot_event() helper which provides reasonable defaults in tests to simplify calling bot_management.bot_event().
    
    Change-Id: I7107c05dedb815043d9b81e7dfe169c43a08a7c2

--- appengine/swarming/server/bot_management_test.py
+++ appengine/swarming/server/bot_management_test.py
@@ -61,7 +61,7 @@ def _bot_event(
       external_ip=external_ip,
       authenticated_as=authenticated_as,
       dimensions=dimensions,
-      state=state or {'ram': 65},
+      state=state or {'ram': 65.0},
       version=version,
       quarantined=quarantined,
       maintenance_msg=maintenance_msg,
@@ -101,7 +101,7 @@ def _gen_bot_info(**kwargs):
     'machine_type': None,
     'quarantined': False,
     'maintenance_msg': None,
-    'state': {u'ram': 65},
+    'state': {u'ram': 65.0},
     'task_id': None,
     'task_name': None,
     'version': _VERSION,
@@ -168,11 +168,8 @@ class BotManagementTest(test_case.TestCase):
       u'pool': [u'default'],
     }
     for name in bot_management.BotEvent.ALLOWED_EVENTS:
-      event_key = bot_management.bot_event(
-          event_type=name, bot_id=u'id1',
-          external_ip=u'8.8.4.4', authenticated_as=u'bot:id1.domain',
-          dimensions=dimensions, state={u'ram': 65}, version=_VERSION,
-          quarantined=False, maintenance_msg=None, task_id=None, task_name=None)
+      event_key = _bot_event(
+          event_type=name, bot_id=u'id1', dimensions=dimensions)
       if name in (u'request_sleep', u'task_update'):
         # TODO(maruel): Store request_sleep IFF the state changed.
         self.assertIsNone(event_key, name)
@@ -183,12 +180,9 @@ class BotManagementTest(test_case.TestCase):
 
   def test_BotEvent_proto_maintenance(self):
     # Also test a misconfigured bot not in a pool.
-    event_key = bot_management.bot_event(
+    event_key = _bot_event(
         event_type=u'bot_connected', bot_id=u'id1',
-        external_ip=u'8.8.4.4', authenticated_as=u'bot:id1.domain',
-        dimensions={u'id': [u'id1']}, state={u'ram': 65.0}, version=_VERSION,
-        quarantined=False, maintenance_msg=u'Too hot', task_id=None,
-        task_name=None)
+        dimensions={u'id': [u'id1']}, maintenance_msg=u'Too hot')
     actual = swarming_pb2.BotEvent()
     event_key.get().to_proto(actual)
     expected = swarming_pb2.BotEvent(
@@ -216,14 +210,11 @@ class BotManagementTest(test_case.TestCase):
 
   def test_BotEvent_proto_quarantine(self):
     # Also test that a bot can belong to two pools.
-    event_key = bot_management.bot_event(
+    event_key = _bot_event(
         event_type=u'bot_connected', bot_id=u'id1',
-        external_ip=u'8.8.4.4', authenticated_as=u'bot:id1.domain',
         dimensions={u'id': [u'id1'], u'pool': [u'next', u'previous']},
         state={u'ram': 65.0, u'quarantined': u'sad bot'},
-        version=_VERSION,
-        quarantined=True, maintenance_msg=None, task_id=None,
-        task_name=None)
+        quarantined=True)
     actual = swarming_pb2.BotEvent()
     event_key.get().to_proto(actual)
     expected = swarming_pb2.BotEvent(
@@ -261,11 +252,7 @@ class BotManagementTest(test_case.TestCase):
       u'pool': [u'default'],
     }
     event = 'request_sleep'
-    bot_management.bot_event(
-        event_type=event, bot_id='id1',
-        external_ip='8.8.4.4', authenticated_as='bot:id1.domain',
-        dimensions=d, state={'ram': 65}, version=_VERSION, quarantined=False,
-        maintenance_msg=None, task_id=None, task_name=None)
+    _bot_event(event_type=event, bot_id='id1', dimensions=d)
 
     expected = _gen_bot_info()
     self.assertEqual(
@@ -293,16 +280,10 @@ class BotManagementTest(test_case.TestCase):
     }
     bot_info = _ensure_bot_info(
         bot_id=bot_id, dimensions=d, task_id=task_id, task_name=task_name)
-    bot_management.bot_event(
+    _bot_event(
         event_type=event,
         bot_id=bot_id,
-        external_ip='8.8.4.4',
-        authenticated_as='bot:id1.domain',
         dimensions=d,
-        state={'ram': 65},
-        version=_VERSION,
-        quarantined=False,
-        maintenance_msg=None,
         task_id=task_id,
         task_name=task_name)
 
