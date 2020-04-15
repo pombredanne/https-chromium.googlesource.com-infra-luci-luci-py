00e411773418fb537577eba111474b6422f974c1
Takuto Ikuta <tikuta@chromium.org>
2020-04-15 23:52:53 +0900

    resultdb proto
    Change-Id: Ifc2ffdf22ec13a656518580b27acfb9408f796ee

---
+++ appengine/swarming/proto/resultdb/gen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright 2020 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+set -eux
+
+cd $(dirname $0)
+
+revision=7c33ecc8bb3c35ecd473dcbe52edeffd3ffaf643
+for f in rpc/v1/invocation.proto rpc/v1/predicate.proto type/common.proto
+do
+    # curl "https://chromium.googlesource.com/infra/luci/luci-go/+/${revision}/resultdb/proto/${f}?format=TEXT" | base64 -d > "${f}"
+    :
+done
+
+pwd
+../../../components/tools/compile_proto.py --proto_path . .
