6a145f6d0251ab195213f418d30e91152d659629
Takuto Ikuta <tikuta@chromium.org>
2020-05-08 19:04:01 +0900

    git squash commit for report_ini.
    df413e9c17c4fac4c61ed7f1d51c0b87c2536b45
    report ini
    
    05b9ee512c0ef485def454af4eb7290e24f76ce4
    report
    
    6e4968e64aa6489976c5b7dc6fd205c1592bd902
    exception
    
    Change-Id: I348eb179366b80c14a7cf5030a7e77c3a2c4f3aa

--- client/utils/on_error.py
+++ client/utils/on_error.py
@@ -236,13 +236,6 @@ def report_on_exception_exit(server):
     return False
 
   _HOSTNAME = socket.getfqdn()
-  if not _HOSTNAME.endswith(_ENABLED_DOMAINS):
-    # Silently skip non-google infrastructure. Technically, it reports to the
-    # server the client code is talking to so in practice, it would be safe for
-    # non googler to manually enable this assuming their client code talks to a
-    # server they also own. Please send a CL if you desire this functionality.
-    return False
-
   _SERVER = net.get_http_service(server, allow_cached=False)
   atexit.register(_check_for_exception_on_exit)
   return True
