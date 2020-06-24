7322d8c9c474d0268cc764e2278421eff2049576
Junji Watanabe <jwata@google.com>
2020-06-24 18:01:59 +0900

    [swarming-ui] strip tags and ignore spaces in toMatchTextContent()
    The custom matcher doesn't strip white spaces and tabs, line brakes properly.
    It makes tests failing when minify is disabled during local development.
    
    Change-Id: Ie4a50ee5061434663390a819090f79785e12a983

--- appengine/swarming/ui2/modules/test_util.js
+++ appengine/swarming/ui2/modules/test_util.js
@@ -66,8 +66,13 @@ export const customMatchers = {
         if (!isElement(actual)) {
           throw `${actual} is not a DOM element`;
         }
-        text = text.trim();
-        let actualText = actual.textContent.trim();
+        function trim(s) {
+          return s.trim()
+            .replace('\t', ' ')
+            .replace(/ {2,}/g, ' ');
+        }
+        text = trim(text);
+        let actualText = trim(actual.innerText);
         if (actualText === text) {
           return {
             // craft the message for the negated version
