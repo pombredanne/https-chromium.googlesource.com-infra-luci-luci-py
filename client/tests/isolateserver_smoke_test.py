9d46fc45c1572c5c9d21dff33acf84ee9586656b
Takuto Ikuta <tikuta@chromium.org>
2020-07-01 20:13:12 +0900

    client: isolateserver_smoke_test
    Change-Id: I99b7102df2cfbd490ed0535bab877bfb28743942

--- client/tests/isolateserver_smoke_test.py
+++ client/tests/isolateserver_smoke_test.py
@@ -41,7 +41,7 @@ class IsolateServerArchiveSmokeTest(unittest.TestCase):
     # before being uploaded.
     # TODO(maruel): This should not be leaked to the client. It's a
     # transport/storage detail.
-    self.namespace = ('temporary' + str(long(time.time())).split('.', 1)[0]
+    self.namespace = ('temporary' + str(int(time.time())).split('.', 1)[0]
                       + '-gzip')
     self.tempdir = tempfile.mkdtemp(prefix=u'isolateserver')
     self.rootdir = os.path.join(self.tempdir, 'rootdir')
