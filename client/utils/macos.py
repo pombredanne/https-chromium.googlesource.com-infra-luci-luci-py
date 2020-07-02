c15a939af684ecc391de7ff4433ee7c380e61e97
tikuta <tikuta@chromium.org>
2020-07-02 17:41:51 +0900

    client: do not use Carbon.File/MacOS
    These package are not there in python3.
    Use pyobjc instead.
    
    Bug: 1010816
    Change-Id: I84999596a01e8f56e9ff059f31f15d7698226ff9

---
+++ client/utils/macos.py
@@ -0,0 +1,28 @@
+# Copyright 2020 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+"""This is simple wrapper of MacOS.Error or OSError for macos."""
+
+import sys
+
+import six
+
+if sys.platform == 'darwin':
+  if six.PY3:
+    import re
+    # Extract 43 from error like 'Mac Error -43'
+    _mac_error_re = re.compile('^MAC Error -(\d+)$')
+
+    Error = OSError
+
+    def get_errno(e):
+      m = _mac_error_re.match(e.args[0])
+      if not m:
+        return None
+      return -int(m.groups()[0])
+  else:
+    import MacOS
+    Error = MacOS.Error
+
+    def get_errno(e):
+      return e.args[0]
