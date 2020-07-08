b7bb073a66660d105e4a0b350997c64d5d64eea4
Junji Watanabe <jwata@google.com>
2020-07-08 12:08:48 +0900

    wip
    Change-Id: Id3887174f1945e40f5fab14c35e981c962a902ff

--- appengine/swarming/swarming_bot/bot_code/task_runner_test.py
+++ appengine/swarming/swarming_bot/bot_code/task_runner_test.py
@@ -844,8 +844,6 @@ class TestTaskRunnerKilled(TestTaskRunnerBase):
 
 
   @unittest.skipIf(six.PY3, 'crbug.com/1010816')
-  @unittest.skipIf(sys.platform == 'darwin',
-                   'TODO(crbug.com/1103068): Fails on Mac CQ')
   @unittest.skipIf(
       sys.platform == 'win32', 'TODO(crbug.com/1017545): fix assertions')
   def test_io(self):
@@ -883,8 +881,6 @@ class TestTaskRunnerKilled(TestTaskRunnerBase):
         output='hi\ngot signal %s\nbye\n' % task_runner.SIG_BREAK_OR_TERM)
 
   @unittest.skipIf(six.PY3, 'crbug.com/1010816')
-  @unittest.skipIf(sys.platform == 'darwin',
-                   'TODO(crbug.com/1103068): Fails on Mac CQ')
   @unittest.skipIf(
       sys.platform == 'win32', 'TODO(crbug.com/1017545): fix assertions')
   def test_io_signal(self):
@@ -925,8 +921,6 @@ class TestTaskRunnerKilled(TestTaskRunnerBase):
     self.expectTask(hard_timeout=True, exit_code=exit_code)
 
   @unittest.skipIf(six.PY3, 'crbug.com/1010816')
-  @unittest.skipIf(sys.platform == 'darwin',
-                   'TODO(crbug.com/1103068): Fails on Mac CQ')
   @unittest.skipIf(
       sys.platform == 'win32',
       'As run_isolated is killed, the children process leaks')
@@ -969,8 +963,6 @@ class TestTaskRunnerKilled(TestTaskRunnerBase):
         output='hi\ngot signal %s\nbye\n' % task_runner.SIG_BREAK_OR_TERM)
 
   @unittest.skipIf(six.PY3, 'crbug.com/1010816')
-  @unittest.skipIf(sys.platform == 'darwin',
-                   'TODO(crbug.com/1103068): Fails on Mac CQ')
   @unittest.skipIf(sys.platform == 'win32',
                    'As run_isolated is killed, the children process leaks')
   def test_io_signal_no_grace(self):
@@ -995,8 +987,6 @@ class TestTaskRunnerKilled(TestTaskRunnerBase):
         output='hi\ngot signal %s\nbye\n' % task_runner.SIG_BREAK_OR_TERM)
 
   @unittest.skipIf(six.PY3, 'crbug.com/1010816')
-  @unittest.skipIf(sys.platform == 'darwin',
-                   'TODO(crbug.com/1103068): Fails on Mac CQ')
   @unittest.skipIf(sys.platform == 'win32',
                    'TODO(crbug.com/1017545): KeyError output')
   def test_isolated_io_signal_grand_children(self):
