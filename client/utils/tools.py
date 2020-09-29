c9d8f7492b2ad103b00be56dbfbfdeed6ff94a4f
Ye Kuang <yekuang@google.com>
2020-09-29 18:55:17 +0900

    Rename 'master' -> 'client', 'slave' -> 'agent'
    The remaining mentionings of 'master' were referring to git branches.
    
    Bug: 1117908
    Change-Id: I601ad2e8cb8a7fecbed8bfe4e540d21b80675cea

--- client/utils/tools.py
+++ client/utils/tools.py
@@ -396,11 +396,8 @@ def is_headless():
   Examines os.environ for presence of SWARMING_HEADLESS var.
   """
   headless_env_keys = (
-    # This is Chromium specific. Set when running under buildbot slave.
-    'CHROME_HEADLESS',
-    # Set when running under swarm bot.
-    'SWARMING_HEADLESS',
-  )
+      # Set when running under swarm bot.
+      'SWARMING_HEADLESS',)
   return any(get_bool_env_var(key) for key in headless_env_keys)
 
 
