033d7b2354e81ef80ed7c5e96c871edf568bf451
Quinten Yearsley <qyearsley@chromium.org>
2020-10-01 14:12:53 -0700

    Delete references to Buildbot in luci-py
    This is almost all but not quite all references, but it does include all docs.
    In some places the mention of Buildbot is kept, but rephrased to clarify that
    it is past-tense.
    
    Bug: 1008557
    Change-Id: I7ca79a594344d99c6fd3ba922f69657c0b923493

--- client/utils/tools.py
+++ client/utils/tools.py
@@ -396,10 +396,11 @@ def is_headless():
   Examines os.environ for presence of SWARMING_HEADLESS var.
   """
   headless_env_keys = (
-    # This is Chromium specific. Set when running under buildbot slave.
-    'CHROME_HEADLESS',
-    # Set when running under swarm bot.
-    'SWARMING_HEADLESS',
+      # This is Chromium specific. Set when running under buildbot slave.
+      # This may never apply anywhere because Buildbot is no longer used.
+      'CHROME_HEADLESS',
+      # Set when running under swarm bot.
+      'SWARMING_HEADLESS',
   )
   return any(get_bool_env_var(key) for key in headless_env_keys)
 
