964fe1657169f080de77126d55289f6c425384e9
Takuto Ikuta <tikuta@chromium.org>
2020-10-02 17:37:18 +0900

    add example for GAE App using remotebuildexecution
    git squash commit for rbe_gae.
    
    72a14f923ce0211eb456c30014fa5226515e1aec
    gae
    
    9b07a791687f3682bf162cd852daf898434f347c
    example
    
    7ad4e021c4fb94aac7f815879c0798ef50c82146
    example
    
    a3f3062f938a633a810ad0219e8656077860b094
    resp
    
    Change-Id: Icb863f27a8b7a0c885dd03c7346a74ccbd758e93

---
+++ appengine/rbe/main.py
@@ -0,0 +1,65 @@
+# Copyright 2020 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import base64
+import hashlib
+import logging
+
+from google.appengine.api import memcache
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+import httplib2
+import webapp2
+
+http = httplib2.Http(memcache)
+
+service = discovery.build(
+    "remotebuildexecution",
+    "v2",
+    http=http,
+    credentials=GoogleCredentials.get_application_default())
+
+
+def get_digest(data):
+  return {"sizeBytes": str(len(data)), "hash": hashlib.sha256(data).hexdigest()}
+
+
+def get_request(data):
+  return {
+      "data": base64.b64encode(data),
+      "digest": get_digest(data),
+  }
+
+
+class MainHandler(webapp2.RequestHandler):
+
+  def get(self):
+    instance = "projects/tikuta-experiment/instances/default_instance"
+
+    update_resp = service.blobs().batchUpdate(
+        instanceName=instance,
+        body={
+            "requests": [
+                get_request(""),
+                get_request("a"),
+            ]
+        }).execute()
+    logging.info("%s", update_resp)
+
+    read_resp = service.blobs().batchRead(
+        instanceName=instance,
+        body={
+            "digests": [
+                get_digest(""),
+                get_digest("a"),
+            ]
+        }).execute()
+    logging.info("%s", read_resp)
+    self.response.write("%s %s" % (update_resp, read_resp))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+], debug=True)
