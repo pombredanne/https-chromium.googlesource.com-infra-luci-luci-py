ecd7f14573c6053304fa8ef00a93e18ede19394b
Ola Karlsson <olakar@chromium.org>
2020-11-23 22:02:59 -0800

    Have the Swarming UI use the `chops_git_version` if set.
    Change-Id: I0e05cb5b00584dff4c0fdaf0e6681a3ea6ee3b07

--- appengine/swarming/ui2/modules/test_util.js
+++ appengine/swarming/ui2/modules/test_util.js
@@ -96,25 +96,30 @@ function isElement(ele) {
   return ele instanceof Element || ele instanceof HTMLDocument;
 }
 
-export function mockAppGETs(fetchMock, permissions) {
+export function mockAuthdAppGETs(fetchMock, permissions) {
   fetchMock.get('/_ah/api/swarming/v1/server/details', {
-    server_version: '1234-abcdefg',
+    project_id: 'chromium-swarm',
+    server_version: '5637-4d8b832',
+    //server_version: 'swarming-staging-default-v024',
+    //chops_git_version: '5629-2cfcb6',
     bot_version: 'abcdoeraymeyouandme',
     machine_provider_template: 'https://example.com/leases/%s',
     display_server_url_template: 'https://example.com#id=%s',
+    cas_viewer_server: 'https://cas-viewer-dev.appspot.com',
   });
 
 
   fetchMock.get('/_ah/api/swarming/v1/server/permissions', permissions);
 }
 
-export function mockAuthdAppGETs(fetchMock, permissions) {
+export function mockAuthdAppGETsGit(fetchMock, permissions) {
   fetchMock.get('/_ah/api/swarming/v1/server/details', requireLogin({
-    server_version: '1234-abcdefg',
+    server_version: 'swarming-staging-default-v024',
     bot_version: 'abcdoeraymeyouandme',
     machine_provider_template: 'https://example.com/leases/%s',
     display_server_url_template: 'https://example.com#id=%s',
     cas_viewer_server: 'https://cas-viewer-dev.appspot.com',
+    chops_git_version: '5629-2cfcb6',
   }));
 
 
