4da495a33d1a468fd811d9c4d6fedcfcf0fce2d8
yusufm <mohsinally@google.com>
2021-02-05 15:26:27 -0800

    Removing obsolete shutdown command params from os_utilities.py
    Change-Id: I9a5ebcd3227c6ed81ac5a090ed96d36ccb6b38e0

--- appengine/swarming/swarming_bot/api/os_utilities.py
+++ appengine/swarming/swarming_bot/api/os_utilities.py
@@ -1329,9 +1329,8 @@ def host_reboot_and_return(message=None):
     # remove the call with -f. We also include 'reboot' in case 'shutdown' gives
     # us 'Failed to talk to init daemon'.
     cmds = [
-        ['sudo', '-n', '/sbin/shutdown', '-f', '-r', 'now'],
         ['sudo', '-n', '/sbin/shutdown', '-r', 'now'],
-        ['sudo', '-n', '/sbin/reboot', '-f'],
+        ['sudo', '-n', '/sbin/reboot'],
     ]
   elif sys.platform == 'darwin':
     # -f is supported on linux but not OSX.
