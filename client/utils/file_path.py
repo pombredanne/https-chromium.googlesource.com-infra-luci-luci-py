62859971c26df776418a17094f4b3efbb1f8e262
Junji Watanabe <jwata@google.com>
2021-02-16 14:28:24 +0900

    Do not call change_tree_permission() at the beggining
    Change-Id: I73a741fb0059a297673f7c6458299197b66a2393

--- client/utils/file_path.py
+++ client/utils/file_path.py
@@ -1128,14 +1128,14 @@ def rmtree(root):
                         repr(root), sys.getdefaultencoding())
   root = six.text_type(root)
 
-  # Change permissions of the tree.
-  start = time.time()
-  try:
-    make_tree_deleteable(root)
-  except OSError as e:
-    logging.warning('Swallowing make_tree_deleteable() error: %s', e)
-  logging.debug('file_path.make_tree_deleteable(%s) took %d seconds', root,
-                time.time() - start)
+  def change_tree_permission():
+    start = time.time()
+    try:
+      make_tree_deleteable(root)
+    except OSError as e:
+      logging.warning('Swallowing make_tree_deleteable() error: %s', e)
+    logging.debug('file_path.make_tree_deleteable(%s) took %d seconds', root,
+                  time.time() - start)
 
   # First try the soft way: tries 3 times to delete and sleep a bit in between.
   # Retries help if test subprocesses outlive main process and try to actively
@@ -1154,6 +1154,11 @@ def rmtree(root):
       if i:
         sys.stderr.write('Succeeded.\n')
       return
+
+    # change tree permission after the first try failure.
+    if i == 0:
+      change_tree_permission()
+
     if not i and sys.platform == 'win32':
       for path in sorted(set(path for _, path, _ in errors)):
         try:
