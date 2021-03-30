69e7c241af4fa84e42426502e84a081ce3c4afb0
Junji Watanabe <jwata@google.com>
2021-03-30 16:50:34 +0900

    fix error in JWT padding
    Change-Id: I6395ad69640870fc69f509aae35f90f26be81d9f

--- appengine/swarming/swarming_bot/api/platforms/gce.py
+++ appengine/swarming/swarming_bot/api/platforms/gce.py
@@ -39,7 +39,7 @@ _CACHED_METADATA_TOKEN_LOCK = threading.Lock()
 def _padded_b64_decode(data):
   mod = len(data) % 4
   if mod:
-    data += '=' * (4 - mod)
+    data += b'=' * (4 - mod)
   return base64.urlsafe_b64decode(data)
 
 
@@ -231,7 +231,6 @@ def signed_metadata_token(audience):
     except (IndexError, KeyError, TypeError, ValueError) as exc:
       logging.error('Metadata server returned invalid JWT (%s):\n%s', exc, jwt)
       return None, None
-
     jwt = six.ensure_str(jwt)
     _CACHED_METADATA_TOKEN[audience] = {'jwt': jwt, 'exp': exp}
     return jwt, exp
