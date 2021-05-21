1fba174017f0e0d1836ce0473afb453be47b6db7
Takuto Ikuta <tikuta@chromium.org>
2021-05-21 17:42:24 +0900

    disable kvs on linux
    Change-Id: Ieebbdeda11d59f3cce26187fdb1ccdae85d4a923

--- appengine/swarming/swarming_bot/bot_code/bot_main.py
+++ appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -751,7 +751,7 @@ def _run_isolated_flags(botobj):
   if os_utilities.get_physical_ram() < 2048:
     use_kvs = False
 
-  if sys.platform.startswith('linux') and platforms.linux.get_inside_docker():
+  if sys.platform.startswith('linux'):
     use_kvs = False
 
   if sys.platform == 'win32' and platform.architecture()[0] == '32bit':
