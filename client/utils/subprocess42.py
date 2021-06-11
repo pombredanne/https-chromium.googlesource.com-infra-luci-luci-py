c7b82eabe6e6c66e468d2e03bf6b4894d6547b47
Momo Sasaki <momosasaki@google.com>
2021-06-11 02:03:39 +0000

    [client] Use subprocess42's wait() on Windows
    Signal handler won't be called in the native Popen.wait() right after a signal is sent and will be sent after timeout seconds and also won't be called if timeout is not specified.
    WaitForSingleObject() needs to be called continuously within timeout seconds to make the signal handler called right after a signal is sent.
    Therefore use subprocess42's wait() on Windows for both Python2 and Python3.
    
    Bug: 1218299
    Change-Id: I31874d5586a086b3be8eae4678a168041a067949

--- client/utils/subprocess42.py
+++ client/utils/subprocess42.py
@@ -706,13 +706,19 @@ class Popen(subprocess.Popen):
       process.
     """
     assert timeout is None or isinstance(timeout, (int, float)), timeout
-    if timeout is None:
+    # Use the native wait() when on Linux in Python 2 when timeout is
+    # not given and on Linux in Python 3.
+    # For Windows, and Linux in Python 2 when timeout is given, use
+    # the non-native one.
+    if timeout is None and not sys.platform == 'win32':
       super(Popen, self).wait()
     elif six.PY3 and not sys.platform == 'win32':
       super(Popen, self).wait(timeout)
     elif self.returncode is None:
       # If you think the following code is horrible, it's because it is
       # inspired by python3's stdlib.
+      if sys.platform == 'win32' and not timeout:
+        timeout = float('inf')
       end = time.time() + timeout
       delay = poll_initial_interval
       wait = self._wait_win if sys.platform == 'win32' else self._wait_non_win
