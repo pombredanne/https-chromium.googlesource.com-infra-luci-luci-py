2fa55eab06b1ba44017f110562b81e4b9ca69b61
Junji Watanabe <jwata@google.com>
2021-08-02 21:05:31 +0900

    copy experimental files
    Change-Id: I7357a3ef265521045773516a343ddbda19472391

---
+++ client/tmp/src/benchmark.py
@@ -0,0 +1,20 @@
+import file_path
+import logging
+import os
+import sys
+
+import six
+
+
+def main(argv):
+  dirpath = six.ensure_text(argv[1])
+  logging.debug(file_path.get_recursive_size(dirpath))
+  logging.info('file_path.get_recursive_size() ended')
+
+
+if __name__ == '__main__':
+  if six.PY2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+  logging.basicConfig(level=logging.DEBUG)
+  main(sys.argv)
