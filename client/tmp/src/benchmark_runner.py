398a54425498905f8ab66e7f85e6a39d39970636
Junji Watanabe <jwata@google.com>
2021-08-02 21:19:47 +0900

    copy experimental files
    Change-Id: I7357a3ef265521045773516a343ddbda19472391

---
+++ client/tmp/src/benchmark_runner.py
@@ -0,0 +1,49 @@
+# [VPYTHON:BEGIN]
+# wheel: <
+#  name: "infra/python/wheels/six-py2_py3"
+#  version: "version:1.12.0"
+# >
+# [VPYTHON:END]
+
+import logging
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+import file_path
+
+
+CMDSUFFFIX = ''
+if sys.platform == 'win32':
+  CMDSUFFFIX = '.exe'
+
+BASEDIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+SRCDIR = os.path.join(BASEDIR, 'src')
+DATADIR = os.path.join(SRCDIR, 'data')
+
+SCRIPT = os.path.join('src', 'benchmark.py')
+
+
+def main():
+  # Python3
+  logging.info('Running script with Python3')
+  dir1 = os.path.join(BASEDIR, 'dir1')
+  if os.path.isdir(dir1):
+    shutil.rmtree(dir1)
+  shutil.copytree(DATADIR, dir1)
+  subprocess.check_output(['vpython' + CMDSUFFFIX, SCRIPT, dir1])
+  
+  # Python2
+  logging.info('Running script with Python2')
+  dir2 = os.path.join(BASEDIR, 'dir2')
+  if os.path.isdir(dir2):
+    shutil.rmtree(dir2)
+  shutil.copytree(DATADIR, dir2)
+  subprocess.check_output(['vpython' + CMDSUFFFIX, SCRIPT, dir2])
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG)
+  main()
---
+++ client/tmp/trigger_task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+digest=`go run go.chromium.org/luci/client/cmd/cas archive -cas-instance chromium-swarm -paths .:src | cut -d\  -f4`
+
+go run go.chromium.org/luci/client/cmd/swarming trigger \
+  -server https://chromium-swarm.appspot.com \
+  -dimension pool=chromium.tests  \
+  -dimension cpu=x86-64 \
+  -dimension cores=8 \
+  -dimension os=Windows-10 \
+  -dimension gce=1 \
+  -dimension ssd=0 \
+  -digest $digest \
+  -- vpython.exe .\\src\\benchmark_runner.py
