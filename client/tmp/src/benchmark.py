5a290981a9f829a95bc0ff33615cb24d202ca820
Junji Watanabe <jwata@google.com>
2021-08-03 21:44:15 +0900

    copy experimental files
    Change-Id: I7357a3ef265521045773516a343ddbda19472391

---
+++ client/tmp/src/benchmark.py
@@ -0,0 +1,28 @@
+# [VPYTHON:BEGIN]
+# wheel: <
+#  name: "infra/python/wheels/six-py2_py3"
+#  version: "version:1.12.0"
+# >
+# [VPYTHON:END]
+
+import file_path
+import logging
+import os
+import sys
+
+import six
+
+
+def main(argv):
+  dirpath = six.ensure_text(argv[1])
+  logging.debug(file_path.get_recursive_size(dirpath))
+  logging.info('file_path.get_recursive_size() ended')
+
+
+if __name__ == '__main__':
+  if six.PY2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+  logging.basicConfig(level=logging.DEBUG)
+  logging.debug('Python version %s', sys.version_info)
+  main(sys.argv)
