ca5c0cb0ca271166ff90ec8ae9a5af54e16db0a7
Joanna Wang <jojwang@google.com>
2021-08-24 20:49:11 -0400

    [swarming] backend: Add BuildToken to schedul_request implementation.
    Bug:1236848
    Change-Id: I94f25e9b471936812814be5aa9a15f311678c194

--- appengine/swarming/handlers_endpoints.py
+++ appengine/swarming/handlers_endpoints.py
@@ -512,8 +512,9 @@ class SwarmingTasksService(remote.Service):
 
     try:
       result_summary = task_scheduler.schedule_request(
-          request_obj, secret_bytes, request_obj.resultdb and
-          request_obj.resultdb.enable)
+          request_obj,
+          request_obj.resultdb and request_obj.resultdb.enable,
+          secret_bytes=secret_bytes)
     except (datastore_errors.BadValueError, TypeError, ValueError) as e:
       logging.exception("got exception around task_scheduler.schedule_request")
       raise endpoints.BadRequestException(e.message)
@@ -1010,8 +1011,7 @@ class SwarmingBotService(remote.Service):
     except (datastore_errors.BadValueError, TypeError, ValueError) as e:
       raise endpoints.BadRequestException(e.message)
 
-    result_summary = task_scheduler.schedule_request(
-        request, secret_bytes=None, enable_resultdb=False)
+    result_summary = task_scheduler.schedule_request(request, False)
     return swarming_rpcs.TerminateResponse(
         task_id=task_pack.pack_result_summary_key(result_summary.key))
 
