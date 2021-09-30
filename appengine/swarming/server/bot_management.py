fb9138b1f87c23be72bc83cb9ccbfc5587e01ef2
Joanna Wang <jojwang@google.com>
2021-09-30 12:44:16 -0700

    [swarming] Avoid potential eventual consistency for updating bot_info.
    Bug:1247362
    Change-Id: I22157c2e2a548288e79da191e1dd3dc609c3fea8

--- appengine/swarming/server/bot_management.py
+++ appengine/swarming/server/bot_management.py
@@ -308,16 +308,21 @@ class BotInfo(_BotCommon):
 
   @classmethod
   def yield_bots_should_be_dead(cls):
-    """Yields bots who should be dead."""
+    """Yields bots that should be dead."""
     q = cls.yield_alive_bots()
     cursor = None
     more = True
     while more:
       bots, cursor, more = q.fetch_page(1000, start_cursor=cursor)
       for b in bots:
-        if not b.should_be_dead:
-          continue
-        yield b
+        if b.should_be_dead:
+          # The query results may be stale. Lookup by key again so
+          # we can bypass cache and also ensure strong consistency.
+          b = b.key.get(use_memcache=False, use_cache=False)
+          if not b:
+            logging.warning('BotInfo %s does not exist. key: %s.', b.id, b.key)
+          elif b.should_be_dead and (b.is_alive or not b.is_dead):
+            yield b
 
   @staticmethod
   def _deadline():
@@ -751,30 +756,19 @@ def cron_update_bot_info():
   """Refreshes BotInfo.composite for dead bots."""
   @ndb.tasklet
   def run(bot):
-    if bot and bot.should_be_dead and (bot.is_alive or not bot.is_dead):
-      # bot composite get updated in _pre_put_hook
-      yield bot.put_async()
-      logging.info('Changing Bot status to DEAD: %s', bot.id)
-      raise ndb.Return(bot.key)
-    raise ndb.Return(None)
+    # bot composite get updated in _pre_put_hook
+    yield bot.put_async()
+    logging.info('Changing Bot status to DEAD: %s', bot.id)
+    raise ndb.Return(bot)
 
   def tx_result(future, stats):
-    bot_key = future.get_result()
-    if not bot_key:
-      # Do nothing.
-      return
+    bot = future.get_result()
 
     try:
       # Unregister the bot from task queues since it can't reap anything.
-      task_queues.cleanup_after_bot(bot_key.parent())
+      task_queues.cleanup_after_bot(bot.key.parent())
 
       stats['dead'] += 1
-
-      bot = bot_key.get()
-      if not bot:
-        logging.warning('BotInfo does not exist. key: %s', bot_key)
-        stats['failed'] += 1
-        return
 
       logging.info('Sending bot_missing event: %s', bot.id)
       bot_event(
