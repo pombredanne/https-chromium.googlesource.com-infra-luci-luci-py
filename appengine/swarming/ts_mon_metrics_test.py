1b9f7616bb032cfc260d9afe61a6df304f1aa915
Justin Luong <justinluong@google.com>
2022-02-28 04:44:31 +0000

    Added task state change metrics for async PubSub notifications
    BUG:b/213376141
    Change-Id: Ib91736b909e749f0eac47b7daa42a4fc9d577dbc

--- appengine/swarming/ts_mon_metrics_test.py
+++ appengine/swarming/ts_mon_metrics_test.py
@@ -339,7 +339,8 @@ class TestMetrics(test_case.TestCase):
                                        state=task_result.State.KILLED)
 
     latency = 500
-    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(summary, latency)
+    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(
+        summary.tags, summary.state, latency)
 
     fields = {
         'project_id': 'test_project',
@@ -353,7 +354,8 @@ class TestMetrics(test_case.TestCase):
             fields=fields).sum)
 
     latency = 250
-    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(summary, latency)
+    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(
+        summary.tags, summary.state, latency)
 
     self.assertEqual(
         750,
@@ -365,7 +367,8 @@ class TestMetrics(test_case.TestCase):
     summary.state = task_result.State.TIMED_OUT
 
     latency = 300
-    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(summary, latency)
+    ts_mon_metrics.on_task_status_change_pubsub_notify_latency(
+        summary.tags, summary.state, latency)
 
     self.assertEqual(
         300,
@@ -392,7 +395,8 @@ class TestMetrics(test_case.TestCase):
                                        tags=tags,
                                        expiration_delay=1,
                                        state=task_result.State.COMPLETED)
-    ts_mon_metrics.on_task_status_change_pubsub_publish_success(summary)
+    ts_mon_metrics.on_task_status_change_pubsub_publish_success(
+        summary.tags, summary.state)
     fields = {
         'project_id': 'test_project',
         'pool': 'test_pool',
@@ -417,7 +421,8 @@ class TestMetrics(test_case.TestCase):
                                        tags=tags,
                                        expiration_delay=1,
                                        state=task_result.State.COMPLETED)
-    ts_mon_metrics.on_task_status_change_pubsub_publish_failure(summary, 404)
+    ts_mon_metrics.on_task_status_change_pubsub_publish_failure(
+        summary.tags, summary.state, 404)
     fields = {
         'project_id': 'test_project',
         'pool': 'test_pool',
