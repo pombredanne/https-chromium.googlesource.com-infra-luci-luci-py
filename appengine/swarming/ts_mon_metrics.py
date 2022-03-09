8f4ec4c4c01e4e73021edcae1e3dee0193ace289
Justin Luong <justinluong@google.com>
2022-03-09 08:16:47 +0000

    Increase PubSub bucketer range to prevent clipping
    PubSub latency metric appears to be clipping see:
    https://screenshot.googleplex.com/4PXi7Vs5MoVJbgg.png
    
    Bug: b/213376141
    Change-Id: I21d6094366324fe323263e9d79019039152644b4

--- appengine/swarming/ts_mon_metrics.py
+++ appengine/swarming/ts_mon_metrics.py
@@ -50,13 +50,13 @@ _TARGET_FIELDS = {
 _bucketer = gae_ts_mon.GeometricBucketer(growth_factor=10**0.05,
                                          num_finite_buckets=100)
 
-# Custom bucketer with 2% resolution in the range of 100ms...10s. Used for
+# Custom bucketer with 2% resolution in the range of 100ms...100s. Used for
 # pubsub latency measurements.
 # Roughly speaking measurements range between 150ms and 300ms. However timeout
 # for pubsub notification is 10s.
 _pubsub_bucketer = gae_ts_mon.GeometricBucketer(growth_factor=10**0.01,
-                                                num_finite_buckets=200,
-                                                scale=100)
+                                                num_finite_buckets=220,
+                                                scale=300)
 
 # Custom bucketer with 2% resolution in the range of 100ms...100000s. Used for
 # task scheduling latency measurements.
