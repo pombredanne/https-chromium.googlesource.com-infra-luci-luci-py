28fac3fc01852f1d9e21963a2eefeccef30ff4d1
Julio Hurtado <juahurta@google.com>
2022-03-24 16:03:32 +0000

    BigQuery: send bot_events, task_results_(summary/run) to pub/sub
    BUG=b:207421681
    TEST=CQ
    
    Change-Id: Ic2d0bea8e489587fdc6ca023490413948661a60d

---
+++ appengine/components/test_support/pubsub_mock.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env vpython
+# coding: utf-8
+# Copyright 2022 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+from components import net
+from google.appengine.ext import ndb
+
+
+def mock_pubsub_requests(assert_equal_func, mock_func):
+  def mocked_request(scopes, **kwargs):  # pylint: disable=unused-argument
+    assert_equal_func(['https://www.googleapis.com/auth/pubsub'], scopes)
+    future = ndb.Future()
+    future.set_result(None)
+    return future
+
+  mock_func(net, 'json_request_async', mocked_request)
+  return None
