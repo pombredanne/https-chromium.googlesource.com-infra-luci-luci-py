0fbcc2ddf640b76f2dc92be47819e81dc16b48ed
Meera Srinivasan <msriniv@google.com>
2022-05-26 10:25:13 -0700

    made the path_handler function work with flask
    Bug: monorail:10924
    Change-Id: Ifc99a279391df34081296fcf9ff0f5f49634724d

--- appengine/components/components/endpoints_flask/adapter.py
+++ appengine/components/components/endpoints_flask/adapter.py
@@ -4,7 +4,7 @@
 
 import collections
 import endpoints
-import httplib
+from six.moves import http_client
 import json
 import logging
 import os
@@ -14,6 +14,7 @@ from endpoints import protojson
 from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
+import flask
 import webapp2
 
 from components import template
@@ -40,7 +41,7 @@ def decode_field(field, value):
 
 
 def decode_message(remote_method_info, request):
-  """Decodes a protorpc message from an webapp2 request.
+  """Decodes a protorpc message from a Flask request.
 
   If method accepts a resource container, parses field values from URL too.
   """
@@ -70,10 +71,10 @@ def decode_message(remote_method_info, request):
     else:
       param_fields = res_container.parameters_message_class.all_fields()
     for f in param_fields:
-      if f.name in request.route_kwargs:
-        values = [request.route_kwargs[f.name]]
+      if f.name in request.args:
+        values = [request.args[f.name]]
       else:
-        values = request.get_all(f.name)
+        values = request.values.get_list(f.name)
       if values:
         values = [decode_field(f, v) for v in values]
         if f.repeated:
@@ -91,6 +92,7 @@ def add_cors_headers(headers):
 
 
 class CorsHandler(webapp2.RequestHandler):
+  # TODO: make this work with a flask handler function, remove self references
   def options(self, *_args, **_kwargs):
     add_cors_headers(self.response.headers)
 
@@ -101,67 +103,72 @@ def path_handler(api_class, api_method, service_path):
   # Why return a class? Because webapp2 explicitly checks if handler that we
   # passed to Route is a class.
 
-  class Handler(webapp2.RequestHandler):
-    def dispatch(self):
-      add_cors_headers(self.response.headers)
+  # flask.request stuff
 
-      api = api_class()
-      api.initialize_request_state(
-          remote.HttpRequestState(remote_host=None,
-                                  remote_address=self.request.remote_addr,
-                                  server_host=self.request.host,
-                                  server_port=self.request.server_port,
-                                  http_method=self.request.method,
-                                  service_path=service_path,
-                                  headers=self.request.headers.items()))
+  response = flask.make_response()
+  status = 200
+  content_type = ''
+  #TODO: set status and content_type in the actual response
 
-      try:
-        req = decode_message(api_method.remote, self.request)
-        # Check that required fields are populated.
-        req.check_initialized()
-      except (messages.DecodeError, messages.ValidationError, ValueError) as ex:
-        response_body = json.dumps({'error': {'message': ex.message}})
-        self.response.set_status(httplib.BAD_REQUEST)
+  add_cors_headers(response.headers)
+
+  api = api_class()
+  api.initialize_request_state(
+      remote.HttpRequestState(remote_host=None,
+                              remote_address=flask.request.remote_addr,
+                              server_host=flask.request.host,
+                              server_port=flask.request.server_port,
+                              http_method=flask.request.method,
+                              service_path=service_path,
+                              headers=flask.request.headers.items()))
+
+  try:
+    req = decode_message(api_method.remote, flask.request)
+    # Check that required fields are populated.
+    req.check_initialized()
+  except (messages.DecodeError, messages.ValidationError, ValueError) as ex:
+    response_body = json.dumps({'error': {'message': ex.message}})
+    status = http_client.BAD_REQUEST
+  else:
+    try:
+      res = api_method(api, req)
+    except endpoints.ServiceException as ex:
+      response_body = json.dumps({'error': {'message': ex.message}})
+      status = ex.http_status
+    else:
+      if isinstance(res, message_types.VoidMessage):
+        status = 204
+        response_body = None
       else:
-        try:
-          res = api_method(api, req)
-        except endpoints.ServiceException as ex:
-          response_body = json.dumps({'error': {'message': ex.message}})
-          self.response.set_status(ex.http_status)
-        else:
-          if isinstance(res, message_types.VoidMessage):
-            self.response.set_status(204)
-            response_body = None
-          else:
-            response_body = PROTOCOL.encode_message(res)
-            if self.request.get('fields'):
-              try:
-                # PROTOCOL.encode_message checks that the message is initialized
-                # before dumping it directly to JSON string. Therefore we can't
-                # mask the protocol buffer (if masking removes a required field
-                # then encode_message will fail). Instead, call encode_message
-                # first, then load the JSON string into a dict, mask the dict,
-                # and dump it back to JSON.
-                response_body = json.dumps(
-                    partial.mask(json.loads(response_body),
-                                 self.request.get('fields')))
-              except (partial.ParsingError, ValueError) as e:
-                # Log the error but return the full response.
-                logging.warning('Ignoring erroneous field mask %r: %s',
-                                self.request.get('fields'), e)
+        response_body = PROTOCOL.encode_message(res)
+        if flask.request.get('fields'):
+          try:
+            # PROTOCOL.encode_message checks that the message is initialized
+            # before dumping it directly to JSON string. Therefore we can't
+            # mask the protocol buffer (if masking removes a required field
+            # then encode_message will fail). Instead, call encode_message
+            # first, then load the JSON string into a dict, mask the dict,
+            # and dump it back to JSON.
+            response_body = json.dumps(
+                partial.mask(json.loads(response_body),
+                             flask.request.get('fields')))
+          except (partial.ParsingError, ValueError) as e:
+            # Log the error but return the full response.
+            logging.warning('Ignoring erroneous field mask %r: %s',
+                            flask.request.get('fields'), e)
 
-      if self.response.status_int != 204:
-        self.response.content_type = 'application/json; charset=utf-8'
-        self.response.out.write(response_body)
-      else:
-        # webob sets content_type to text/html by default.
-        self.response.content_type = ''
-
-  return Handler
+  if status != 204:
+    content_type = 'application/json; charset=utf-8'
+    response.out.write(response_body)
+  else:
+    # webob sets content_type to text/html by default.
+    content_type = ''
+  response = flask.make_response(response_body, status)
+  return response
 
 
 def api_routes(api_classes, base_path='/_ah/api', regex='[^/]+'):
-  """Creates webapp2 routes for the given Endpoints v1 services.
+  """Creates routes for the given Endpoints v1 services.
 
   Args:
     api_classes: A list of protorpc.remote.Service classes to create routes for.
@@ -170,8 +177,11 @@ def api_routes(api_classes, base_path='/_ah/api', regex='[^/]+'):
     regex: Regular expression to allow in path parameters.
 
   Returns:
-    A list of webapp2.Routes.
+    A list of tuples, each consisting of three parts: a URL rule string,
+    a function that handles the given endpoint, and an optional list of strings
+    of acceptable HTTP methods.
   """
+
   routes = []
 
   # Add routes for each class.
@@ -188,12 +198,12 @@ def api_routes(api_classes, base_path='/_ah/api', regex='[^/]+'):
       t = posixpath.join(api_base_path, method_path)
       http_method = info.http_method.upper() or 'POST'
       handler = path_handler(api_class, method, api_base_path)
-      routes.append(webapp2.Route(t, handler, methods=[http_method]))
+      routes.append((t, handler, [http_method]))
       templates.add(t)
 
     # Add routes for HTTP OPTIONS (to add CORS headers) for each method.
     for t in sorted(templates):
-      routes.append(webapp2.Route(t, CorsHandler, methods=['OPTIONS']))
+      routes.append((t, CorsHandler, ['OPTIONS']))
 
   # Add generic routes.
   routes.extend([
@@ -268,10 +278,10 @@ def discovery_service_route(api_classes, base_path):
     base_path: The base path under which all service paths exist.
 
   Returns:
-    A webapp2.Route.
+    A tuple containing a URL string and a path.
   """
-  return webapp2.Route('%s/discovery/v1/apis/<name>/<version>/rest' % base_path,
-                       discovery_handler_factory(api_classes, base_path))
+  return ('%s/discovery/v1/apis/<name>/<version>/rest' % base_path,
+          discovery_handler_factory(api_classes, base_path))
 
 
 def directory_handler_factory(api_classes, base_path):
@@ -310,10 +320,10 @@ def directory_service_route(api_classes, base_path):
     base_path: The base path under which all service paths exist.
 
   Returns:
-    A webapp2.Route.
+    A tuple containing a URL string and a path.
   """
-  return webapp2.Route('%s/discovery/v1/apis' % base_path,
-                       directory_handler_factory(api_classes, base_path))
+  return ('%s/discovery/v1/apis' % base_path,
+          directory_handler_factory(api_classes, base_path))
 
 
 def explorer_proxy_route(base_path):
@@ -323,7 +333,7 @@ def explorer_proxy_route(base_path):
     base_path: The base path under which all service paths exist.
 
   Returns:
-    A webapp2.Route.
+    A tuple containing a URL string and a path.
   """
 
   class ProxyHandler(webapp2.RequestHandler):
@@ -337,7 +347,7 @@ def explorer_proxy_route(base_path):
   template.bootstrap({
       'adapter': os.path.join(THIS_DIR, 'templates'),
   })
-  return webapp2.Route('%s/static/proxy.html' % base_path, ProxyHandler)
+  return ('%s/static/proxy.html' % base_path, ProxyHandler)
 
 
 def explorer_redirect_route(base_path):
@@ -358,4 +368,4 @@ def explorer_redirect_route(base_path):
       self.redirect('https://apis-explorer.appspot.com/apis-explorer'
                     '/?base=https://%s%s' % (host, base_path))
 
-  return webapp2.Route('%s/explorer' % base_path, RedirectHandler)
+  return ('%s/explorer' % base_path, RedirectHandler)
