65a92b6c0d26db02afd9690a860e24d3cdca01d0
Meera Srinivasan <msriniv@google.com>
2022-05-27 14:47:27 -0700

    made discovery_handler work with flask
    Bug: monorail:10924
    Change-Id: I2d7932f2d80bd2f760982fc48f245a55e1303c94

--- appengine/components/components/endpoints_flask/adapter.py
+++ appengine/components/components/endpoints_flask/adapter.py
@@ -231,7 +231,7 @@ def api_server(api_classes, base_path='/_ah/api', regex='[^/]+'):
   return webapp2.WSGIApplication(api_routes(api_classes, base_path, regex))
 
 
-def discovery_handler_factory(api_classes, base_path):
+def discovery_handler(api_classes, base_path, name, version):
   """Returns a discovery request handler which knows about the given services.
 
   Args:
@@ -248,23 +248,21 @@ def discovery_handler_factory(api_classes, base_path):
     service_map[(api_class.api_info.name,
                  api_class.api_info.version)].append(api_class)
 
-  class DiscoveryHandler(webapp2.RequestHandler):
-    """Returns a discovery document for known services."""
+    host = flask.request.headers['Host']
+    services = service_map.get((name, version))
+    if not services:
+      flask.abort(404, 'Not Found')
 
-    def get(self, name, version):
-      host = self.request.headers['Host']
-      services = service_map.get((name, version))
-      if not services:
-        self.abort(404, 'Not Found')
-
-      self.response.headers['Content-Type'] = 'application/json'
-      json.dump(discovery.generate(services, host, base_path),
-                self.response,
-                indent=2,
-                sort_keys=True,
-                separators=(',', ':'))
-
-  return DiscoveryHandler
+    headers = []
+    headers['Content-Type'] = 'application/json'
+    response_body = ''
+    json.dump(
+        discovery.generate(services, host, base_path),
+        response_body,  #TODO(msriniv): make this flask
+        indent=2,
+        sort_keys=True,
+        separators=(',', ':'))
+  return flask.Response(response_body, headers=headers)
 
 
 def discovery_service_route(api_classes, base_path):
