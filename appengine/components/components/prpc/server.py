c5b3798109bcf32a80f33cd3c4f6d535e5d0488d
Yuxuan Chen <yuxuanch@google.com>
2022-06-10 13:25:08 -0700

    fix
    fix format
    
    
    [flask] add the function in pRPC to handle flask request
    
    Change-Id: I077a4cb122decb3aef9e3ecd5ca30aa4195cad60
    bug: monorail:10936

--- appengine/components/components/prpc/server.py
+++ appengine/components/components/prpc/server.py
@@ -16,6 +16,7 @@ import collections
 import httplib
 import logging
 import webapp2
+import flask
 
 from google.protobuf import symbol_database
 
@@ -155,6 +156,10 @@ class Server(object):
             handler=self._handler(),
             methods=['POST', 'OPTIONS'])
     ]
+
+  def get_flask_routes(self, prefix=''):
+    return [('%s/prpc/<service>/<method>' % prefix, self._flaskhandler(),
+             ['POST', 'OPTIONS'])]
 
   def _handler(self):
     """Returns a RequestHandler class with access to this server's data."""
@@ -309,6 +314,174 @@ class Server(object):
           self.response.headers['Access-Control-Max-Age'] = '600'
 
     return Handler
+
+  def _flaskhandler(self):
+    # Returns a FlaskRequestHandlerFunction with
+    # access to this server's data.
+
+    # Alias self as server here for readability.
+    server = self
+
+    class FlaskHandler():
+      def __init__(self):
+        self.request = flask.request
+        self.response = None
+
+      def pRcpHandler(self, service, method):
+        self.response = flask.make_response()
+
+        if self.request.method == 'POST':
+          self.post(service, method)
+        elif self.request.method == 'OPTIONS':
+          self.options(service, method)
+
+        return self.response
+
+      def post(self, service, method):
+        """Writes body and headers of flask.Response.
+
+        Args:
+          service: the service being targeted by this pRPC call.
+          method: the method being invoked by this pRPC call.
+
+        Returns:
+          response: a flask.Response.
+        """
+        context = ServicerContext()
+        content = self._handle(context, service, method)
+        origin = self.request.headers.get('Origin')
+        if origin:
+          self.response.headers['Access-Control-Allow-Origin'] = origin
+          self.response.headers['Vary'] = 'Origin'
+          self.response.headers['Access-Control-Allow-Credentials'] = 'true'
+        self.response.status_code = _PRPC_TO_HTTP_STATUS[context._code]
+        self.response.headers['X-Prpc-Grpc-Code'] = str(context._code.value)
+        self.response.headers['Access-Control-Expose-Headers'] = (
+            'X-Prpc-Grpc-Code')
+        self.response.headers['X-Content-Type-Options'] = 'nosniff'
+        if content is not None:
+          self.response.headers['Content-Type'] = encoding.Encoding.media_type(
+              context._response_encoding)
+          self.response.response = content
+        elif context._details is not None:
+          # webapp2 will automatically encode strings as utf-8.
+          # http://webapp2.readthedocs.io/en/latest/guide/response.html
+          #
+          # TODO(nodir,mknyszek): Come up with an actual test for this.
+          self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+          self.response.response = context._details
+        return self.response
+
+      def _handle(self, context, service, method):
+        """Generates the response content and sets the context appropriately.
+
+        Sets context._request_encoding and context._response_encoding.
+
+        Args:
+          context: a context.ServicerContext.
+          service: the service being targeted by this pRPC call.
+          method: the method being invoked by this pRPC call.
+
+        Returns:
+          content: the binary or textual content of the RPC response. Note
+            that this may be None in the event that an error occurs.
+        """
+        try:
+          parsed_headers = headers.parse_headers(self.request.headers)
+          context._request_encoding = parsed_headers.content_type
+          context._response_encoding = parsed_headers.accept
+        except ValueError as e:
+          logging.warning('Error parsing headers: %s', e)
+          context.set_code(StatusCode.INVALID_ARGUMENT)
+          context.set_details(e.message)
+          return None
+
+        if service not in server._services:
+          context.set_code(StatusCode.UNIMPLEMENTED)
+          context.set_details('Service %s does not exist' % service)
+          return None
+        rpc_handler = server._services[service].methods.get(method)
+        if rpc_handler is None:
+          context.set_code(StatusCode.UNIMPLEMENTED)
+          context.set_details('Method %s does not exist' % method)
+          return None
+        request_message, response_message, handler = rpc_handler
+
+        request = request_message()
+        try:
+          decoder = encoding.get_decoder(parsed_headers.content_type)
+          decoder(self.request.values, request)
+        except Exception as e:
+          logging.warning('Failed to decode request: %s', e, exc_info=True)
+          context.set_code(StatusCode.INVALID_ARGUMENT)
+          context.set_details('Error parsing request: %s' % e.message)
+          return None
+
+        context._timeout = parsed_headers.timeout
+        context._invocation_metadata = parsed_headers.invocation_metadata
+
+        # Only ipv6 addresses have ':' in them. Assume everything else is ipv4.
+        if ':' in self.request.remote_addr:
+          context._peer = 'ipv6:[%s]' % self.request.remote_addr
+        else:
+          context._peer = 'ipv4:%s' % self.request.remote_addr
+
+        call_details = HandlerCallDetails(
+            method='%s.%s' % (service, method),
+            invocation_metadata=context.invocation_metadata())
+
+        try:
+          # TODO(nodir,mknyszek): Poll for context to hit timeout or be
+          # canceled.
+          response = server._run_interceptors(request, context, call_details,
+                                              handler, 0)
+        except Exception:
+          logging.exception('Service implementation threw an exception')
+          context.set_code(StatusCode.INTERNAL)
+          context.set_details('Service implementation threw an exception')
+          return None
+
+        if response is None:
+          if context._code == StatusCode.OK:
+            context.set_code(StatusCode.INTERNAL)
+            context.set_details(
+                'Service implementation didn\'t return a response')
+          return None
+
+        if not isinstance(response, response_message):
+          logging.error('Service implementation response has incorrect type')
+          context.set_code(StatusCode.INTERNAL)
+          context.set_details('Service implementation returned invalid value')
+          return None
+
+        try:
+          encoder = encoding.get_encoder(parsed_headers.accept)
+          content = encoder(response)
+        except Exception:
+          logging.exception('Failed to encode response')
+          context.set_code(StatusCode.INTERNAL)
+          context.set_details('Error serializing response')
+          return None
+
+        return content
+
+      # pylint: disable=unused-argument
+      def options(self, service, method):
+        """Sends an empty response with CORS headers for origins, if allowed."""
+        origin = self.request.headers.get('Origin')
+
+        if origin and server.allow_cors and (not server.allowed_origins or
+                                             origin in server.allowed_origins):
+          self.response.headers['Access-Control-Allow-Origin'] = origin
+          self.response.headers['Vary'] = 'Origin'
+          self.response.headers['Access-Control-Allow-Credentials'] = 'true'
+          self.response.headers['Access-Control-Allow-Headers'] = \
+              'Origin, Content-Type, Accept, Authorization'
+          self.response.headers['Access-Control-Allow-Methods'] = \
+              'OPTIONS, POST'
+          self.response.headers['Access-Control-Max-Age'] = '600'
+
+    return FlaskHandler().pRcpHandler
 
   def _run_interceptors(self, request, context, call_details, handler, idx):
     """Runs the request via interceptors chain starting from given index.
