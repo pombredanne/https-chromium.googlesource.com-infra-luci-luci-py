2790d1e8b6101d80f85a27820c5b3a27c9ee1a86
Yuxuan Chen <yuxuanch@google.com>
2022-06-19 14:02:20 -0700

    [flask] add the function in pRPC to handle flask request
    Change-Id: I077a4cb122decb3aef9e3ecd5ca30aa4195cad60
    bug: monorail:10936

--- appengine/components/components/prpc/__init__.py
+++ appengine/components/components/prpc/__init__.py
@@ -2,5 +2,6 @@
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
-from .server import HandlerCallDetails, Server, StatusCode
+from .server_base import HandlerCallDetails, ServerBase, StatusCode
+from .server import Server
 from .encoding import Encoding
