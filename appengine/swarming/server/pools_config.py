5c71213fa083b83573eeb3477b8059c32ef8b382
Justin Luong <justinluong@google.com>
2022-07-20 11:36:02 +0000

    Upgrade to pylint 2.7 and fix the many linting errors.
    pylint 1.5 was due to be removed on 2022-07-11, but has now been
    extended until Q4-2022. We need to make Swarming compliant with pylint 2.7 before that time.
    
    Bug: 1344829
    Change-Id: I71f05fb6f697548ce4b8f563cd460453268a6199

--- appengine/swarming/server/pools_config.py
+++ appengine/swarming/server/pools_config.py
@@ -673,14 +673,14 @@ def _validate_pools_cfg(cfg, ctx):
               ctx.error('peer "%s" was specified twice', d.peer_id)
             elif peer_id:
               seen_peers.add(peer_id)
-          for i, tag in enumerate(d.require_any_of.tag):
+          for k, tag in enumerate(d.require_any_of.tag):
             if ':' not in tag:
-              ctx.error('bad tag #%d "%s" - must be <key>:<value>', i, tag)
+              ctx.error('bad tag #%d "%s" - must be <key>:<value>', k, tag)
 
       # Validate external schedulers.
-      for i, es in enumerate(msg.external_schedulers):
+      for k, es in enumerate(msg.external_schedulers):
         if not es.address:
-          ctx.error('%sth external scheduler config had no address', i)
+          ctx.error('%sth external scheduler config had no address', k)
 
       _resolve_deployment(ctx, msg, template_map, deployment_map)
 
