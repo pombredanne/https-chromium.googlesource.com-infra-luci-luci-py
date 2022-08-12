a724e2dabdbf0528d2ce5688189c62c2c4f97d84
Justin Luong <justinluong@google.com>
2022-08-12 03:50:05 +0000

    [swarming] Propagate task_runner internal failure to the bot_main on_after_task hook
    This resolves an inconsistency observed by the Swarming server and the
    bot_hook which would report an 'internal_failure=True' but the on_after_task bot hook would instead report 'internal_failure=False'.
    
    Bug: 1344913
    Change-Id: I594083f602a346452b4cf718ee112d2c570aaec5

--- appengine/swarming/swarming_bot/bot_code/bot_main.py
+++ appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -895,6 +895,7 @@ def _run_manifest(botobj, manifest, start):
 
   failure = False
   internal_failure = False
+  task_runner_internal_failure = False
   msg = None
   auth_params_dumper = None
   must_reboot_reason = None
@@ -1004,7 +1005,7 @@ def _run_manifest(botobj, manifest, start):
     if fs.exists(task_result_file):
       with fs.open(task_result_file, 'rb') as fd:
         task_result = json.load(fd)
-
+      task_runner_internal_failure = task_result['task_runner_internal_failure']
     if proc.returncode:
       # STATUS_DLL_INIT_FAILED generally means that something bad happened, and
       # a reboot magically clears things out. :(
@@ -1036,12 +1037,13 @@ def _run_manifest(botobj, manifest, start):
       auth_params_dumper.stop()
     if internal_failure:
       _post_error_task(botobj, msg, task_id)
-    logging.info('calling on_after_task: failure=%s, internal_failure=%s, '
-                 'task_dimensions=%s, task_result=%s',
-                 failure, internal_failure, task_dimensions, task_result)
-    _call_hook_safe(
-        True, botobj, 'on_after_task', failure, internal_failure,
+    any_internal_failure = internal_failure or task_runner_internal_failure
+    logging.info(
+        'calling on_after_task: failure=%s, internal_failure=%s, '
+        'task_dimensions=%s, task_result=%s', failure, any_internal_failure,
         task_dimensions, task_result)
+    _call_hook_safe(True, botobj, 'on_after_task', failure,
+                    any_internal_failure, task_dimensions, task_result)
     if fs.isdir(work_dir):
       try:
         file_path.rmtree(work_dir)
