a32d899146c51fb154bac8f86771d475c5d04cd3
Justin Luong <justinluong@google.com>
2022-09-02 05:48:43 +0000

    [swarming] Propagate task_runner internal failure to the bot_main on_after_task hook
    This resolves an inconsistency observed by the Swarming server which would report an 'internal_failure=True' but the on_after_task bot hook would instead report 'internal_failure=False'.
    
    Bug: 1344913
    Change-Id: I594083f602a346452b4cf718ee112d2c570aaec5

--- appengine/swarming/swarming_bot/bot_code/bot_main_test.py
+++ appengine/swarming/swarming_bot/bot_code/bot_main_test.py
@@ -812,10 +812,12 @@ class TestBotMain(TestBotBase):
                   returncode=0,
                   exit_code=0,
                   url='https://localhost:1',
-                  expected_auth_params_json=None):
+                  expected_auth_params_json=None,
+                  internal_error_reported=False):
     result = {
         'exit_code': exit_code,
-        'must_signal_internal_failure': None,
+        'internal_error': None,
+        'internal_error_reported': internal_error_reported,
         'version': 3,
     }
     # Method should have "self" as first argument - pylint: disable=E0213
@@ -873,7 +875,8 @@ class TestBotMain(TestBotBase):
 
   def test_run_manifest(self):
     self.mock(bot_main, '_post_error_task', self.print_err_and_fail)
-    def call_hook(botobj, name, *args):
+
+    def call_hook(_chained, botobj, name, *args):
       if name == 'on_after_task':
         failure, internal_failure, dimensions, summary = args
         self.assertEqual(self.attributes['dimensions'], botobj.dimensions)
@@ -905,7 +908,7 @@ class TestBotMain(TestBotBase):
 
     self.mock(bot_main, '_post_error_task', self.print_err_and_fail)
 
-    def call_hook(botobj, name, *args):
+    def call_hook(_chained, botobj, name, *args):
       if name == 'on_after_task':
         failure, internal_failure, dimensions, summary = args
         self.assertEqual(self.attributes['dimensions'], botobj.dimensions)
@@ -955,7 +958,7 @@ class TestBotMain(TestBotBase):
   def test_run_manifest_task_failure(self):
     self.mock(bot_main, '_post_error_task', self.print_err_and_fail)
 
-    def call_hook(_botobj, name, *args):
+    def call_hook(_chained, _botobj, name, *args):
       if name == 'on_after_task':
         failure, internal_failure, dimensions, summary = args
         self.assertEqual(True, failure)
@@ -981,7 +984,8 @@ class TestBotMain(TestBotBase):
   def test_run_manifest_internal_failure(self):
     posted = []
     self.mock(bot_main, '_post_error_task', lambda *args: posted.append(args))
-    def call_hook(_botobj, name, *args):
+
+    def call_hook(_chained, _botobj, name, *args):
       if name == 'on_after_task':
         failure, internal_failure, dimensions, summary = args
         self.assertEqual(False, failure)
@@ -1006,6 +1010,33 @@ class TestBotMain(TestBotBase):
     expected = [(self.bot, 'Execution failed: internal error (1).', '24')]
     self.assertEqual(expected, posted)
 
+  def test_run_manifest_internal_error_reported(self):
+    posted = []
+    self.mock(bot_main, '_post_error_task', lambda *args: posted.append(args))
+
+    def call_hook(_chained, _botobj, name, *args):
+      if name == 'on_after_task':
+        failure, internal_failure, dimensions, summary = args
+        self.assertEqual(False, failure)
+        self.assertEqual(True, internal_failure)
+        self.assertEqual({'pool': 'default'}, dimensions)
+        self.assertEqual(result, summary)
+
+    self.mock(bot_main, '_call_hook', call_hook)
+    result = self._mock_popen(returncode=0, internal_error_reported=True)
+
+    manifest = {
+        'command': ['echo', 'hi'],
+        'dimensions': {
+            'pool': 'default'
+        },
+        'grace_period': 30,
+        'hard_timeout': 60,
+        'io_timeout': 60,
+        'task_id': '24',
+    }
+    bot_main._run_manifest(self.bot, manifest, time.time())
+
   def test_run_manifest_exception(self):
     posted = []
 
@@ -1014,7 +1045,7 @@ class TestBotMain(TestBotBase):
 
     self.mock(bot_main, '_post_error_task', post_error_task)
 
-    def call_hook(_botobj, name, *args):
+    def call_hook(_chained, _botobj, name, *args):
       if name == 'on_after_task':
         failure, internal_failure, dimensions, summary = args
         self.assertEqual(False, failure)
