1c0ebefbe5f137185504dfd1a7b01628e8c471f5
Jonah Hooper <jonahhooper@google.com>
2022-11-29 22:06:54 +0000

    [swarming] Ported over listing tasks by bot_id to PRPC
    Change-Id: I928184dc98f047fe8b23e41ae835f26ea42fcb16

--- appengine/swarming/handlers_prpc_test.py
+++ appengine/swarming/handlers_prpc_test.py
@@ -84,6 +84,60 @@ class PrpcTest(test_env_handlers.AppTestBase):
   no_run = 1
   service = None
 
+  def apply_defaults_for_run_result(self, result):
+    """Returns a serialized swarming_rpcs.TaskResult initialized from a
+    TaskRunResult.
+
+    To be used for expectations.
+    """
+    result.bot_dimensions.extend([
+        swarming_api_pb2.StringListPair(key='id', value=['bot1']),
+        swarming_api_pb2.StringListPair(key='os', value=['Amiga']),
+        swarming_api_pb2.StringListPair(key='pool', value=['default'])
+    ])
+    result.bot_id = 'bot1'
+    result.bot_version = self.bot_version
+    result.costs_usd.extend([0.0])
+    result.current_task_slice = 0
+    result.failure = False
+    result.internal_failure = False
+    result.name = 'job1'
+    result.run_id = '5cee488008811'
+    result.server_versions.extend(['v1a'])
+    result.state = swarming_api_pb2.TaskState.RUNNING
+    result.task_id = '5cee488008811'
+    return result
+
+  def gen_perf_stats_prpc(self, stats):
+    """Returns a serialized swarming_rpcs.PerformanceStats.
+
+    To be used for expectations.
+    """
+    stats.bot_overhead = 0.1
+    stats.cache_trim.duration = 0.1
+    stats.isolated_download.duration = 1.0
+    stats.isolated_download.initial_size = 100000
+    stats.isolated_download.initial_number_items = 10
+    stats.isolated_download.items_cold = b'x\234\023\001\000\000\025\000\025'
+    stats.isolated_download.items_hot = b'x\234\223\343\002\000\000H\000)'
+    stats.isolated_download.num_items_cold = 1
+    stats.isolated_download.total_bytes_items_cold = 20
+    stats.isolated_download.num_items_hot = 2
+    stats.isolated_download.total_bytes_items_hot = 70
+
+    stats.isolated_upload.duration = 2.0
+    stats.isolated_upload.items_cold = b'x\234cdT\003\000\000.\000)'
+    stats.isolated_upload.items_hot = b'x\234cdd\324\007\000\000<\0003'
+    stats.isolated_upload.num_items_cold = 3
+    stats.isolated_upload.total_bytes_items_cold = 43
+    stats.isolated_upload.num_items_hot = 4
+    stats.isolated_upload.total_bytes_items_hot = 56
+
+    stats.cleanup.duration = 0.1
+    stats.package_installation.duration = 3.0
+    stats.named_caches_install.duration = 0.1
+    stats.named_caches_uninstall.duration = 0.1
+
   def post_prpc(self, rpc, request, expect_errors=False):
     assert self.service, "Child classes must define service"
 
@@ -426,6 +480,74 @@ class BotServicePrpcTest(PrpcTest):
     _decode(resp.body, actual)
     expected = swarming_api_pb2.TerminateResponse(task_id='5cee488008810')
     self.assertEqual(expected, actual)
+
+  def test_tasks_ok(self):
+    """Asserts that tasks produces bot information."""
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+
+    self.set_as_bot()
+    self.bot_poll()
+    self.set_as_user()
+    self.client_create_task_raw()
+    self.set_as_bot()
+    res = self.bot_poll()
+    response = self.bot_complete_task(task_id=res['manifest']['task_id'])
+    self.assertEqual({u'must_stop': False, u'ok': True}, response)
+
+    now_1 = self.mock_now(self.now, 1)
+    self.mock(random, 'getrandbits', lambda _: 0x55)
+    self.set_as_user()
+    self.client_create_task_raw(name='philbert')
+    self.set_as_bot()
+    res = self.bot_poll()
+    response = self.bot_complete_task(exit_code=1,
+                                      task_id=res['manifest']['task_id'])
+    self.assertEqual({u'must_stop': False, u'ok': True}, response)
+
+    start = self.now + datetime.timedelta(seconds=0.5)
+    end = now_1 + datetime.timedelta(seconds=0.5)
+
+    self.set_as_privileged_user()
+    request = swarming_api_pb2.BotTasksRequest()
+    request.bot_id = 'bot1'
+    request.start.FromDatetime(start)
+    request.end.FromDatetime(end)
+    request.sort = swarming_api_pb2.TaskQuery.Sort.CREATED_TS
+    request.state = swarming_api_pb2.TaskQuery.State.ALL
+    request.include_performance_stats = True
+    request.limit = 100
+    response = self.post_prpc('ListBotTasks', request)
+    actual = swarming_api_pb2.TaskListResponse()
+    _decode(response.body, actual)
+
+    expected = swarming_api_pb2.TaskResultResponse()
+    self.apply_defaults_for_run_result(expected)
+    expected.bot_idle_since_ts.FromDatetime(now_1)
+    expected.completed_ts.FromDatetime(now_1)
+    expected.modified_ts.FromDatetime(now_1)
+    expected.started_ts.FromDatetime(now_1)
+    expected.created_ts.FromDatetime(now_1)
+    expected.duration = 0.1
+    expected.name = 'philbert'
+    expected.exit_code = 1
+    expected.failure = True
+    expected.run_id = '5cee870005511'
+    expected.state = swarming_api_pb2.TaskState.COMPLETED
+    expected.task_id = '5cee870005511'
+    expected.costs_usd[:] = [0.1]
+    self.gen_perf_stats_prpc(expected.performance_stats)
+    self.assertEqual(expected, actual.items[0])
+
+    for sort in (swarming_api_pb2.TaskQuery.Sort.COMPLETED_TS,
+                 swarming_api_pb2.TaskQuery.Sort.STARTED_TS):
+      request = swarming_api_pb2.BotTasksRequest()
+      request.bot_id = 'bot1'
+      request.sort = sort
+      request.state = swarming_api_pb2.TaskQuery.State.ALL
+      request.include_performance_stats = True
+      request.limit = 100
+      response = self.post_prpc('ListBotTasks', request)
+      self.assertEqual(response.status, '200 OK')
 
 
 if __name__ == '__main__':
--- appengine/swarming/message_conversion_prpc.py
+++ appengine/swarming/message_conversion_prpc.py
@@ -8,6 +8,10 @@ from datetime import datetime
 
 import proto.api.swarming_api_pb2 as swarming
 
+from server import task_pack
+from server import task_result
+from server import task_request
+
 
 def _string_pairs_from_dict(dictionary):
   # For key: value items like env.
@@ -91,3 +95,123 @@ def bot_events_response(items, cursor):
       now=date(datetime.utcnow()),
       items=[_bot_event_response(event) for event in items],
       cursor=cursor)
+
+
+def _cas_op_stats(stat):
+  if stat is None:
+    return None
+  return swarming.CASOperationStats(
+      duration=stat.duration,
+      initial_number_items=stat.initial_number_items,
+      initial_size=stat.initial_size,
+      items_cold=stat.items_cold,
+      items_hot=stat.items_hot,
+      num_items_cold=stat.num_items_cold,
+      num_items_hot=stat.num_items_hot,
+      total_bytes_items_hot=stat.total_bytes_items_hot,
+      total_bytes_items_cold=stat.total_bytes_items_cold,
+  )
+
+
+def _op_stats(stat):
+  if stat is None:
+    return None
+  return swarming.OperationStats(duration=stat.duration)
+
+
+def _perf_stats(stats):
+  if stats is None:
+    return None
+  return swarming.PerformanceStats(
+      bot_overhead=stats.bot_overhead,
+      isolated_upload=_cas_op_stats(stats.isolated_upload),
+      isolated_download=_cas_op_stats(stats.isolated_download),
+      package_installation=_op_stats(stats.package_installation),
+      cache_trim=_op_stats(stats.cache_trim),
+      named_caches_uninstall=_op_stats(stats.named_caches_uninstall),
+      named_caches_install=_op_stats(stats.named_caches_install),
+      cleanup=_op_stats(stats.cleanup),
+  )
+
+
+def _cas_reference(ref):
+  if ref is None:
+    return None
+  return swarming.CASReference(cas_instance=ref.cas_instance,
+                               digest=swarming.Digest(
+                                   hash=ref.digest.hash,
+                                   size_bytes=ref.digest.size_bytes))
+
+
+def _cipd_package(package):
+  if package is None:
+    return package
+  return swarming.CipdPackage(
+      package_name=package.package_name,
+      version=package.version,
+      path=package.path,
+  )
+
+
+def _cipd_pins(pin):
+  if pin is None:
+    return pin
+  return swarming.CipdPins(client_package=_cipd_package(pin.client_package), )
+
+
+def _result_db_info(out):
+  out.fields('hostname', 'invocation')
+
+
+def task_result_response(result, include_performance_stats=True):
+  out = swarming.TaskResultResponse(
+      bot_id=result.bot_id,
+      bot_version=result.bot_version,
+      bot_logs_cloud_project=result.bot_logs_cloud_project,
+      deduped_from=result.deduped_from,
+      duration=result.duration,
+      exit_code=result.exit_code,
+      failure=result.failure,
+      internal_failure=result.internal_failure,
+      state=result.state,
+      task_id=result.task_id,
+      name=result.name,
+      current_task_slice=result.current_task_slice,
+      completed_ts=date(result.completed_ts),
+      bot_idle_since_ts=date(result.bot_idle_since_ts),
+      abandoned_ts=date(result.abandoned_ts),
+      modified_ts=date(result.modified_ts),
+      started_ts=date(result.started_ts),
+      created_ts=date(result.created_ts),
+      bot_dimensions=_string_list_pairs_from_dict(result.bot_dimensions),
+      children_task_ids=result.children_task_ids,
+      server_versions=result.server_versions,
+      performance_stats=_perf_stats(result.performance_stats)
+      if include_performance_stats else None,
+      cas_output_root=_cas_reference(result.cas_output_root),
+      missing_cas=[_cas_reference(ref) for ref in result.missing_cas],
+      missing_cipd=[_cipd_package(package) for package in result.missing_cipd],
+  )
+
+  if result.__class__ is task_result.TaskRunResult:
+    if result.cost_usd is not None:
+      out.costs_usd.extend([result.cost_usd])
+    if result.task_id:
+      out.run_id = result.task_id
+  else:
+    assert result.__class__ is task_result.TaskResultSummary, result
+    k = result.run_result_key
+    run_id = task_pack.pack_run_result_key(k) if k else None
+    if run_id:
+      out.run_id = run_id
+    out.user = result.user
+    out.tags.extend(result.tags)
+  return out
+
+
+def bot_tasks_response(items, cursor):
+  out = swarming.TaskListResponse()
+  out.cursor = cursor or ''
+  out.items.extend([task_result_response(item) for item in items])
+  out.now.GetCurrentTime()
+  return out
