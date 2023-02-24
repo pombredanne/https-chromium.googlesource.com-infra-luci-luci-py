f39ec67d856dcee17d501db50c7c77841fd82f3a
Jonah Hooper <jonahhooper@google.com>
2023-02-24 17:19:59 +0000

    [swarming] Created GetStdout pRPC handler
    Change-Id: I05d7874f2325f67093a3e4f30cb2eaa834cf6fba

--- appengine/swarming/api_common.py
+++ appengine/swarming/api_common.py
@@ -321,3 +321,30 @@ def cancel_task(task_id, kill_running):
                                        CANCEL).get_result()
   return task_scheduler.cancel_task(request_obj, result_key, kill_running
                                     or False, None)
+
+
+def get_output(task_id, offset, length):
+  """Returns the output of the task corresponding to a task ID.
+
+  Arguments:
+    task_id: output of task_id to return.
+    offset: byte offset to start fetching.
+    length: number of bytes from offset to fetch.
+
+  Returns:
+    tuple(output, state): output is a utf-8 character stream of task output.
+      state is the current TaskState of the task.
+  """
+  _, result = get_request_and_result(task_id, VIEW, True)
+  output = result.get_output(offset or 0, length)
+  if output:
+    # The datastore entity (TaskOutputChunk) stores output as a string blob.
+    # In python2 `str` objects are basically c-strings with no character
+    # encoding required. However, protorpc `StringField` requires that
+    # a string must be either:
+    # 1. encoded in 7bit ascii
+    # 2. have the unicode type
+    # Since the output of tasks is allowed to be utf-8 encoded strings, we must
+    # decode the string to utf-8 to be a valid StringField.
+    output = output.decode('utf-8', 'replace')
+  return output, result.state
