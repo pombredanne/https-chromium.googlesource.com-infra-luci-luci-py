a03810ad36509abf15eece29b7ce82322d552e6f
Dave Tu <dtu@google.com>
2023-04-14 15:50:10 -0700

    [components] Add Python 3 flavor of endpoints to third_party/.
    There are modifications both to support Python 3 and to remove
    additional dependencies that may not be available in all LUCI
    services. For a list of modifications, see README.swarming.
    
    Since this is endpoints 4.8.0 and we previously used endpoints 1.0,
    there is one breaking change: _ApiInfo.version has been renamed
    to _ApiInfo.path_version. Updated dependent code to support both names.
    
    Also add components/third_party/ as a dependency in all tests
    that use endpoints.
    
    Change-Id: I567622935c4d2a8e5c520d18a264723a11d693db

--- appengine/auth_service/test_env.py
+++ appengine/auth_service/test_env.py
@@ -12,6 +12,7 @@ APP_DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 def setup_test_env():
   """Sets up App Engine test environment."""
   # For application modules.
+  sys.path.insert(0, os.path.join(APP_DIR, 'components', 'third_party'))
   sys.path.insert(0, APP_DIR)
 
   from test_support import test_env
