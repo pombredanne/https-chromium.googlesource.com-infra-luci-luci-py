a4205dc5ff672f53e216be852c3af23ed179c741
Jonah Hooper <jonahhooper@google.com>
2023-04-14 20:31:38 +0000

    [components] Vendored 3rd party endpoints removed with recent gcloud update
    Change-Id: I18eadc087acee1a575e205450527ee36777c286d

--- appengine/swarming/handlers_endpoints.py
+++ appengine/swarming/handlers_endpoints.py
@@ -1,6 +1,7 @@
 # Copyright 2015 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
+# TODO(jonahhooper) Added this to make sure tests get triggered.
 
 """This module defines Swarming Server endpoints handlers."""
 
