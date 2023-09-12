27caa587e80afeedb52caa1edb6f7f8f70f90435
Camilo Jacomet <cjacomet@google.com>
2023-09-12 14:42:34 -0700

    [auth-service]: Import protos from luci-go.
    Change the source of truth from luci-py's components/auth/proto to luci-go's auth/service/protocol/components/auth/proto.
    Change script to import from luci-go to luci-py instead of luci-py to luci-go.
    
    Bug:1336139
    Change-Id: I2c83307424dd92472424c60db550054383e7cec3

---
+++ appengine/components/components/auth/proto/import_luci_go.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Copyright 2020 The LUCI Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copies some components/auth protos from luci-py, assuming infra.git gclient
+# layout. Generates generate.go and *.pb.go.
+
+set -e
+
+MYPATH=$(dirname "${BASH_SOURCE[0]}")
+cd ${MYPATH}
+
+LUCI_GO=../../../../../../go/src/go.chromium.org/luci
+LUCI_GO_PROTOS_DIR=${LUCI_GO}/server/auth/service/protocol/components/auth/proto
+
+# Kill all existing files.
+rm -f realms* replication* security_config*
+rm -f Makefile
+
+# Copy fresh files.
+cp \
+  ${LUCI_GO_PROTOS_DIR}/realms.proto \
+  ${LUCI_GO_PROTOS_DIR}/replication.proto \
+  ${LUCI_GO_PROTOS_DIR}/security_config.proto \
+  \
+  .
+
+# Make proto import paths relative to the new root.
+sed -i '' 's|import "go.chromium.org/luci/server/auth/service/protocol/components/auth/proto/|import "components/auth/proto/|g' ./*.proto
+
+# Put the revision of copied files into generate.go for posterity.
+luci_go_rev=$(git -C ${LUCI_GO} rev-parse HEAD)
+
+cat <<EOF >> Makefile
+# Copyright 2023 The LUCI Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Files copied from https://chromium.googlesource.com/infra/go/src/go.chromium.org/luci:
+#   server/auth/service/protocol/realms.proto
+#   server/auth/service/protocol/replication.proto
+#   server/auth/service/protocol/security_config.proto
+#
+# Commit: ${luci_go_rev}
+# Modifications: see import_luci_go.sh
+
+compile:
+	# If we simply compile *.proto files relative to this dir, the recorded file
+	# names in the generated _pb2.py are relative to this dir too which may
+	# introduce naming collisions in the protobuf registry.
+	#
+	# Move the proto path to //appengine/components to "namespace" proto files.
+	# This is consistent with other components assuming that "components" is in
+	# the import path.
+	../../../tools/compile_proto.py --proto_path=../../../ .
+EOF
+
+# Generate *.pb2.py.
+Make
