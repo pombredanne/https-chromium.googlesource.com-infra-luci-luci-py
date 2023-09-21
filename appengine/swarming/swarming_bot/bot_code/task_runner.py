c9221d5ad6a0aeee887eb4d3b137be558745599e
randymaldonado <randymaldonado@google.com>
2023-09-21 19:46:35 +0000

    [swarming] modifying bot code to write bbagent context to file if needed
    also:
    - removed task_id from being passed as command arg.
    - added bb python proto bindings to bot zip file.
    
    
    Bug: 1482241
    Change-Id: I775cc541bd17393f5864ba45318e8feec374fe26

--- appengine/swarming/swarming_bot/bot_code/task_runner.py
+++ appengine/swarming/swarming_bot/bot_code/task_runner.py
@@ -21,18 +21,23 @@ import os
 import random
 import signal
 import sys
+import tempfile
 import time
 import traceback
 
 from api import os_utilities
 from bot_code import bot_auth
 from bot_code import remote_client
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.message import DecodeError
 from libs import luci_context
 from utils import file_path
 from utils import net
 from utils import on_error
 from utils import subprocess42
 from utils import zip_package
+
+from bb.go.chromium.org.luci.buildbucket.proto import launcher_pb2
 
 # Path to this file or the zip containing this file.
 THIS_FILE = os.path.abspath(zip_package.get_main_script_path())
@@ -271,6 +276,31 @@ class InternalError(Exception):
   """Raised on unrecoverable errors that abort task with 'internal error'."""
 
 
+def tmp_bb_agent_context_file(secret_bytes, task_id, workdir):
+  if secret_bytes is None:
+    logging.error(
+        'Failed to create BuildbucketAgentContext file. secret_bytes is None.')
+    raise InternalError("secret_bytes is None")
+
+  tf = tempfile.NamedTemporaryFile(mode='w',
+                                   prefix='bb_agent_ctx.',
+                                   suffix='.json',
+                                   delete=True,
+                                   dir=workdir)
+  logging.debug('Writing BuildbuckerAgentContext file %r', tf.name)
+  try:
+    secrets = launcher_pb2.BuildSecrets()
+    secrets.ParseFromString(secret_bytes)
+  except TypeError as ex:
+    raise InternalError("secret_bytes should be a a bytes-like object")
+  except DecodeError as ex:
+    raise InternalError("could not decode secret_bytes")
+  content = launcher_pb2.BuildbucketAgentContext(task_id=task_id,
+                                                 secrets=secrets)
+  tf.write(MessageToJson(content))
+  return tf
+
+
 def load_and_run(in_file, swarming_server, default_swarming_server,
                  cost_usd_hour, start, out_file, run_isolated_flags, bot_file,
                  auth_params_file, rbe_session_state):
@@ -329,6 +359,7 @@ def load_and_run(in_file, swarming_server, default_swarming_server,
         },
       }
 
+      # TODO (randymaldonado): remove swarming from LUCI_CONTEXT
       # Override LUCI_CONTEXT['swarming'].
       bot_dimensions = []
       for k, vs in task_details.bot_dimensions.items():
@@ -390,9 +421,27 @@ def load_and_run(in_file, swarming_server, default_swarming_server,
       # Auth environment is up, start the command. task_result is dumped to
       # disk in 'finally' block.
       with luci_context.stage(_tmpdir=work_dir, **context_edits) as ctx_file:
-        task_result = run_command(remote, rbe_session, task_details, work_dir,
-                                  cost_usd_hour, start, run_isolated_flags,
-                                  bot_file, ctx_file)
+        try:
+          # If the build is using a buildbucket agent, we must write the custom
+          # context file for the agent to use.
+          bb_ctx_tf = None
+          if '${BUILDBUCKET_AGENT_CONTEXT_FILE}' in task_details.command:
+            bb_ctx_tf = tmp_bb_agent_context_file(task_details.secret_bytes,
+                                                  task_details.task_id,
+                                                  work_dir)
+            idx = task_details.command.index(
+                '${BUILDBUCKET_AGENT_CONTEXT_FILE}')
+            task_details.command[idx] = bb_ctx_tf.name
+          task_result = run_command(remote, rbe_session, task_details, work_dir,
+                                    cost_usd_hour, start, run_isolated_flags,
+                                    bot_file, ctx_file)
+        finally:
+          if bb_ctx_tf:
+            try:
+              bb_ctx_tf.close()
+            except:
+              raise InternalError("could not close buildbucket context file.")
+
   except (ExitSignal, InternalError, remote_client.InternalError) as e:
     # This normally means run_command() didn't get the chance to run, as it
     # itself traps exceptions and will report accordingly. In this case, we want
