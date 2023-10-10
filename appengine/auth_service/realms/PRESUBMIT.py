784712580aaaff09db9695736670207c287749b5
Camilo Jacomet <cjacomet@google.com>
2023-10-09 18:25:24 -0700

    [auth-service]: Freeze permissions.py
    Check if footer contains permissions.py, if so leave a little presubmit message telling them to change the permissions.cfg file too.
    
    Bug:1336139
    Change-Id: Iff460ad7b63686cabbb0b26e7980cf09411da5c7
    Ignore-Freeze: cl/123

---
+++ appengine/auth_service/realms/PRESUBMIT.py
@@ -0,0 +1,24 @@
+# Copyright 2023 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+USE_PYTHON3 = True
+PRESUBMIT_VERSION = '2.0.0'
+
+_IGNORE_FREEZE_FOOTER = 'Ignore-Freeze'
+
+
+def CheckPermissionsFooter(input_api, output_api):
+  footers = input_api.change.GitFootersFromDescription()
+  for file in input_api.change.AffectedFiles():
+    if "permissions.py" in str(file) and _IGNORE_FREEZE_FOOTER not in footers:
+      return [
+          output_api.PresubmitError('''
+              There is a freeze in effect on permissions.py.
+
+              If changing the PermissionsDB, please go make the same change at
+              configs/chrome-infra-auth-dev/permissions.cfg and include
+              'Ignore-Freeze': cl-changing-permissions.cfg in the git footer.
+              ''')
+      ]
+  return []
