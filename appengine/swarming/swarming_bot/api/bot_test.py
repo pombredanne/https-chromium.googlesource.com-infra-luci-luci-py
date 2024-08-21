1904f83e5b26bec895c700c2ab95bc07b4588dc1
Vadim Shtayura <vadimsh@chromium.org>
2024-08-20 18:31:42 -0700

    [swarming] Stop reporting "server_version" dimension.
    Soon the bot archive will *not* be generated by the server
    and there will be no server version associated with it.
    
    R=chanli@chromium.org
    BUG=b/355013257
    
    Change-Id: I82ea1f46a38d778e3a3f1a5e7893026f6d456310

--- appengine/swarming/swarming_bot/api/bot_test.py
+++ appengine/swarming/swarming_bot/api/bot_test.py
@@ -19,7 +19,7 @@ def make_bot(remote=None):
   return bot.Bot(remote, {'dimensions': {
       'id': ['bot1'],
       'pool': ['private']
-  }}, 'https://localhost:1', '1234-1a2b3c4-tainted-joe', 'base_dir', None)
+  }}, 'https://localhost:1', 'base_dir', None)
 
 
 class TestBot(unittest.TestCase):
@@ -75,7 +75,6 @@ class TestBot(unittest.TestCase):
     self.assertEqual({'id': ['bot1'], 'pool': ['private']}, obj.dimensions)
     self.assertEqual(
         os.path.join(obj.base_dir, 'swarming_bot.zip'), obj.swarming_bot_zip)
-    self.assertEqual('1234-1a2b3c4-tainted-joe', obj.server_version)
     self.assertEqual('base_dir', obj.base_dir)
 
   def test_attribute_updates(self):
